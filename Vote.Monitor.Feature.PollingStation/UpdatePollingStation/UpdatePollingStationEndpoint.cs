a0648471cccdd7bcd65843c1cd0f29e24646271c
Bogdan Milas <bogdan.milas@smallfoot.onmicrosoft.com>
2023-09-29 15:38:17 +0300

    Add new folder

---
+++ Vote.Monitor.Feature.PollingStation/UpdatePollingStation/UpdatePollingStationEndpoint.cs
@@ -0,0 +1,12 @@
+﻿using Vote.Monitor.Feature.PollingStation.Repositories;
+
+namespace Vote.Monitor.Feature.PollingStation.UpdatePollingStation;
+internal class UpdatePollingStationEndpoint
+{
+    private readonly IPollingStationRepository _repository;
+
+    public UpdatePollingStationEndpoint(IPollingStationRepository repository)
+    {
+        _repository = repository;
+    }
+}
