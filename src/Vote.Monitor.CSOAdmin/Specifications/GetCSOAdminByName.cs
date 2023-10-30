41e7af95c7a637015929219fef932b4e8042e104
Lidia Micusan <lidia.micusan@visma.com>
2023-10-30 09:36:43 +0200

    Create and Get for CSO Admin

---
+++ src/Vote.Monitor.CSOAdmin/Specifications/GetCSOAdminByName.cs
@@ -0,0 +1,13 @@
+﻿using System.Xml.Linq;
+using Ardalis.Specification;
+
+namespace Vote.Monitor.CSOAdmin.Specifications;
+
+public class GetCSOAdminByName : Specification<Domain.Entities.ApplicationUserAggregate.CSOAdmin>
+{
+    public GetCSOAdminByName(Guid CSOId, string name)
+    {
+        Query
+            .Where(x => x.CSOId == CSOId && x.Name == name);
+    }
+}
