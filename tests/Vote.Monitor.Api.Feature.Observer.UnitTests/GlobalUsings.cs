bd1bc5123474a715df688f16088bf3b41b7a27f8
Mihai S <>
2023-11-26 15:48:56 +0200

    adding unit tests for Observer.Speciffications

---
+++ tests/Vote.Monitor.Api.Feature.Observer.UnitTests/GlobalUsings.cs
@@ -0,0 +1,6 @@
+﻿global using Xunit;
+
+global using ObserverAggregate = Vote.Monitor.Domain.Entities.ApplicationUserAggregate.Observer;
+global using FluentAssertions;
+global using Vote.Monitor.Api.Feature.Observer.UnitTests.Specifications;
+global using Vote.Monitor.Domain.Entities.ApplicationUserAggregate;
---
+++ tests/Vote.Monitor.Api.Feature.Observer.UnitTests/Specifications/ObserverAggregateFaker.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using AutoBogus;
+using Bogus;
+using Bogus.DataSets;
+using Vote.Monitor.Domain.Entities.ApplicationUserAggregate;
+
+namespace Vote.Monitor.Api.Feature.Observer.UnitTests.Specifications;
+
+public class ObserverAggregateFaker : AutoFaker<ObserverAggregate>
+{
+    UserStatus[] statuses = new UserStatus[] { UserStatus.Active, UserStatus.Deactivated };
+    public ObserverAggregateFaker(string? name=null, string? login=null, string? password=null, UserStatus? status=null)
+    {
+        RuleFor(fake => fake.Name, fake => name ?? fake.Name.FirstName());
+        RuleFor(fake => fake.Login, fake => login ?? fake.Internet.Email());
+        RuleFor(fake => fake.Password, fake => password ?? fake.Random.String2(10));
+        RuleFor(fake => fake.Status, fake => status ?? statuses[fake.Random.Int(0, 1)]);
+
+    }
+}
