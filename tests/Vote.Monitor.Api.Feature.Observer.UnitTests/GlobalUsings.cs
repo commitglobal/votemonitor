5677959672800ee761c440b7fb0227e04ef78096
mihainem <5735215+mihainem@users.noreply.github.com>
2023-11-28 14:12:56 +0200

    Fixes #53 - adding Observer.UnitTests (#74)
    * adding unit tests for Observer.Speciffications
    
    * refactored usings (moved used ones to global usings)
    
    ---------
    
    Co-authored-by: Mihai S <>

---
+++ tests/Vote.Monitor.Api.Feature.Observer.UnitTests/GlobalUsings.cs
@@ -0,0 +1,7 @@
+﻿global using Xunit;
+
+global using ObserverAggregate = Vote.Monitor.Domain.Entities.ApplicationUserAggregate.Observer;
+global using FluentAssertions;
+global using Vote.Monitor.Api.Feature.Observer.UnitTests.Specifications;
+global using Vote.Monitor.Domain.Entities.ApplicationUserAggregate;
+global using AutoBogus;
---
+++ tests/Vote.Monitor.Api.Feature.Observer.UnitTests/Specifications/ListObserversSpecificationTests.cs
@@ -0,0 +1,82 @@
+﻿
+
+namespace Vote.Monitor.Api.Feature.Observer.Specifications;
+
+public class ListObserversSpecificationTests
+{
+    private const string DefaultName = "name";
+    private readonly UserStatus DefaultStatus = UserStatus.Active;
+
+    [Fact]
+    public void ListObserversSpecification_AppliesCorrectFilters()
+    {
+        // Arrange
+        var observer1 = new ObserverAggregateFaker(status: DefaultStatus).Generate();
+        var observer2 = new ObserverAggregateFaker(status: DefaultStatus).Generate();
+
+        var testCollection = Enumerable.Range(1, 100)
+            .Select(status => new ObserverAggregateFaker(status: DefaultStatus).Generate())
+        .Union(new[] { observer1, observer2 })
+        .ToList();
+
+        var spec = new ListObserversSpecification(null, null, 100, 2);
+
+        // Act
+        var result = spec.Evaluate(testCollection).ToList();
+
+        // Assert
+        result.Should().HaveCount(2);
+        result.Should().Contain(observer1);
+        result.Should().Contain(observer2);
+    }
+
+    [Fact]
+    public void ListObserversSpecification_AppliesCorrectFilters_WhenNameFilterApplied()
+    {
+        // Arrange
+        var observer1 = new ObserverAggregateFaker(name: DefaultName, status: DefaultStatus).Generate();
+        var observer2 = new ObserverAggregateFaker(name: DefaultName, status: DefaultStatus).Generate();
+
+        var testCollection = Enumerable
+            .Range(1, 100)
+            .Select(statusArg => new ObserverAggregateFaker(name: DefaultName, status: DefaultStatus).Generate())
+            .Union(new[] { observer1, observer2 })
+            .ToList();
+
+        var spec = new ListObserversSpecification(DefaultName, null, 100, 2);
+
+        // Act
+        var result = spec.Evaluate(testCollection).ToList();
+
+        // Assert
+        result.Should().HaveCount(2);
+        result.Should().Contain(observer1);
+        result.Should().Contain(observer2);
+    }
+
+    [Theory]
+    [InlineData("name1")]
+    [InlineData("name2")]
+    public void ListObserversSpecification_AppliesCorrectFilters_WhenPartialFilterApplied(string searchString)
+    {
+        // Arrange
+        var observer1 = new ObserverAggregateFaker(name: searchString, status: DefaultStatus).Generate();
+        var observer2 = new ObserverAggregateFaker(name: searchString, status: DefaultStatus).Generate();
+
+        var testCollection = Enumerable
+            .Range(1, 100)
+            .Select(statusArg => new ObserverAggregateFaker(name: searchString, status: DefaultStatus).Generate())
+            .Union(new[] { observer1, observer2 })
+            .ToList();
+
+        var spec = new ListObserversSpecification(searchString, null, 100, 2);
+
+        // Act
+        var result = spec.Evaluate(testCollection).ToList();
+
+        // Assert
+        result.Should().HaveCount(2);
+        result.Should().Contain(observer1);
+        result.Should().Contain(observer2);
+    }
+}
