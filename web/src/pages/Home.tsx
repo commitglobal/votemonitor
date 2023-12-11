bff03ea06fbae441f6b9af4172eaa31d99ca796e
NicolaeS <nicolae.stihhi@gmail.com>
2023-12-11 17:03:28 +0200

    feat(85): add header

--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -1,11 +1,20 @@
-import type { FunctionComponent } from "../common/types";
+import type { FunctionComponent } from '../common/types';
 
 const Home = (): FunctionComponent => {
-	return (
-		<div className="bg-blue-300  font-bold w-screen h-screen flex flex-col justify-center items-center ">
-			<p className="text-white text-6xl">Hello, world!</p>
-		</div>
-	);
+  return (
+    <>
+      <header>
+        <div className='mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8'>
+          <h1 className='text-3xl font-bold tracking-tight text-gray-900'>Dashboard</h1>
+        </div>
+      </header>
+      <main>
+        <div className='bg-white shadow rounded-md mx-auto max-w-7xl py-6 sm:px-6 lg:px-8 '>
+          <p>Content of dashboard.</p>
+        </div>
+      </main>
+    </>
+  );
 };
 
 export default Home;
