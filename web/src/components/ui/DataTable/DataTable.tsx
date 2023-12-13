b1971b5e55f8d5b6044683f426ed0ef0d79d23f6
NicolaeS <nicolae.stihhi@gmail.com>
2023-12-13 15:19:41 +0200

    feat(83): connect polling stations to backend pagination

--- web/src/components/ui/DataTable/DataTable.tsx
+++ web/src/components/ui/DataTable/DataTable.tsx
@@ -1,53 +1,76 @@
-import { type ColumnDef, flexRender, getCoreRowModel, useReactTable } from '@tanstack/react-table';
+import {
+  type ColumnDef,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+  type PaginationState,
+} from '@tanstack/react-table';
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
-import type { ReactElement } from 'react';
+import { useState, type ReactElement } from 'react';
+import type { PageParameters, PageResponse } from '@/common/types';
+import { DataTablePagination } from './DataTablePagination';
+import type { UseQueryResult } from '@tanstack/react-query';
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[];
-  data: TData[];
+  pagedQuery: (p: PageParameters) => UseQueryResult<PageResponse<TData>, Error>;
 }
 
-export function DataTable<TData, TValue>({ columns, data }: DataTableProps<TData, TValue>): ReactElement {
+export function DataTable<TData, TValue>({ columns, pagedQuery }: DataTableProps<TData, TValue>): ReactElement {
+  const [pagination, setPagination] = useState<PaginationState>({ pageIndex: 0, pageSize: 5 });
+  const { data } = pagedQuery({ pageNumber: pagination.pageIndex + 1, pageSize: pagination.pageSize });
+
   const table = useReactTable({
-    data,
+    data: data?.items || [],
     columns,
+    manualPagination: true,
+    pageCount: data ? Math.ceil(data.totalCount / data.pageSize) : 0,
     getCoreRowModel: getCoreRowModel(),
+    onPaginationChange: (updater) => {
+      setPagination((previous) => (updater instanceof Function ? updater(previous) : updater));
+    },
+    state: { pagination },
   });
 
   return (
-    <div className='rounded-md border'>
-      <Table>
-        <TableHeader>
-          {table.getHeaderGroups().map((headerGroup) => (
-            <TableRow key={headerGroup.id}>
-              {headerGroup.headers.map((header) => {
-                return (
-                  <TableHead key={header.id}>
-                    {header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}
-                  </TableHead>
-                );
-              })}
-            </TableRow>
-          ))}
-        </TableHeader>
-        <TableBody>
-          {table.getRowModel().rows?.length ? (
-            table.getRowModel().rows.map((row) => (
-              <TableRow key={row.id} data-state={row.getIsSelected() && 'selected'}>
-                {row.getVisibleCells().map((cell) => (
-                  <TableCell key={cell.id}>{flexRender(cell.column.columnDef.cell, cell.getContext())}</TableCell>
-                ))}
+    <div>
+      <div className='rounded-md border'>
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}
+                    </TableHead>
+                  );
+                })}
               </TableRow>
-            ))
-          ) : (
-            <TableRow>
-              <TableCell colSpan={columns.length} className='h-24 text-center'>
-                No results.
-              </TableCell>
-            </TableRow>
-          )}
-        </TableBody>
-      </Table>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow key={row.id} data-state={row.getIsSelected() && 'selected'}>
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>{flexRender(cell.column.columnDef.cell, cell.getContext())}</TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell colSpan={columns.length} className='h-24 text-center'>
+                  No results.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+      <div className='mt-4'>
+        <DataTablePagination table={table} />
+      </div>
     </div>
   );
 }
--- web/src/features/polling-stations/components/Dashboard/Dashboard.tsx
+++ web/src/features/polling-stations/components/Dashboard/Dashboard.tsx
@@ -1,44 +1,15 @@
-import type { ColumnDef } from '@tanstack/react-table';
-import type { PollingStation } from '../../models/PollingStation';
+import { pollingStationColDefs, type PollingStation } from '../../models/PollingStation';
 import type { ReactElement } from 'react';
 import { DataTable } from '@/components/ui/DataTable/DataTable';
 import { type UseQueryResult, useQuery } from '@tanstack/react-query';
 import type { PageParameters, PageResponse } from '@/common/types';
 import { authApi } from '@/common/auth-api';
 
-const columns: ColumnDef<PollingStation>[] = [
-  {
-    header: 'Address',
-    accessorKey: 'address',
-  },
-  {
-    header: 'County',
-    accessorKey: 'tags.county',
-  },
-  {
-    header: 'Locality',
-    accessorKey: 'tags.locality',
-  },
-  {
-    header: 'Section Number',
-    accessorKey: 'tags.sectionNumber',
-  },
-  {
-    header: 'Section Name',
-    accessorKey: 'tags.sectionName',
-  },
-];
-
-const pollingStationsQuery: PageParameters = {
-  pageNumber: 1,
-  pageSize: 5,
-};
-
-function usePollingStations(): UseQueryResult<PageResponse<PollingStation>, Error> {
+function usePollingStations({pageNumber, pageSize}: PageParameters): UseQueryResult<PageResponse<PollingStation>, Error> {
   return useQuery({
-    queryKey: ['pollingStations'],
+    queryKey: ['pollingStations', pageNumber, pageSize],
     queryFn: async () => {
-      const response = await authApi.post<PageResponse<PollingStation>>('/polling-stations:list', pollingStationsQuery);
+      const response = await authApi.post<PageResponse<PollingStation>>('/polling-stations:list', { pageNumber, pageSize });
 
       if (response.status !== 200) {
         throw new Error('Failed to fetch polling stations');
@@ -50,25 +21,14 @@ function usePollingStations(): UseQueryResult<PageResponse<PollingStation>, Erro
 }
 
 export default function PollingStationsDashboard(): ReactElement {
-  // const queryClient = useQueryClient();
-  const { status, data, error } = usePollingStations();
-
   return (
     <>
       <div className='px-4 sm:px-0'>
         <h3 className='text-base font-semibold leading-7 text-gray-900'>Polling stations</h3>
       </div>
-      {status === 'loading' ? (
-        'Loading...'
-      ) : status === 'error' ? (
-        <span>Error: {error.message}</span>
-      ) : (
-        <>
-          <div className='bg-white shadow rounded-md mt-6'>
-            <DataTable columns={columns} data={data.items} />
-          </div>
-        </>
-      )}
+      <div className='mt-6'>
+        <DataTable columns={pollingStationColDefs} pagedQuery={usePollingStations} />
+      </div>
     </>
   );
 }
