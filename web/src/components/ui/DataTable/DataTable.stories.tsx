1cc12f999af8847ed1dbc152b0e5589dc62d6960
NicolaeS <nicolae.stihhi@gmail.com>
2023-12-15 15:01:32 +0200

    feat(83): implement story for DataTable

--- web/src/components/ui/DataTable/DataTable.stories.tsx
+++ web/src/components/ui/DataTable/DataTable.stories.tsx
@@ -1,3 +1,5 @@
+import type { PageParameters, PageResponse } from '@/common/types';
+import { useQuery, type UseQueryResult } from '@tanstack/react-query';
 import { DataTable } from './DataTable';
 
 export default {
@@ -6,12 +8,33 @@ export default {
   tags: ['datatable', 'table'],
 };
 
+type Task = { id: number; name: string };
+
 export const Default = {
   args: {
-    task: {
-      id: '1',
-      title: 'Test Task',
-      state: 'TASK_INBOX',
-    },
+    columns: [
+      {
+        header: 'ID',
+        accessorKey: 'id',
+      },
+      {
+        header: 'Task name',
+        accessorKey: 'name',
+      },
+    ],
+    usePagedQuery: (p: PageParameters): UseQueryResult<PageResponse<Task>, Error> =>
+      useQuery({
+        queryKey: ['fetchData', p.pageNumber, p.pageSize],
+        queryFn: () => ({
+          currentPage: p.pageNumber - 1,
+	        pageSize: 5,
+          totalCount: 12,
+          items: [
+            { id: 1, name: 'Task 1' },
+            { id: 2, name: 'Task 2' },
+            { id: 3, name: 'Task 3' },
+          ]
+        } as PageResponse<Task>),
+      }),
   },
 };
