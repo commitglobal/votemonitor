add244db8a2a37748323068a05a811e3a66e6cf5
Iulia Dormenco <iuliaolteanu12@gmail.com>
2024-03-27 15:44:35 +0200

    add list endpoint

--- src/Feature.ObserverGuide/List/Endpoint.cs
+++ src/Feature.ObserverGuide/List/Endpoint.cs
@@ -1,6 +1,17 @@
-﻿namespace Feature.ObserverGuide.List;
+﻿using Authorization.Policies;
+using Authorization.Policies.Requirements;
+using Feature.ObserverGuide.Specifications;
+using Microsoft.AspNetCore.Authorization;
+using Vote.Monitor.Core.Services.FileStorage.Contracts;
+using Vote.Monitor.Core.Services.Security;
 
-public class Endpoint(IReadRepository<ObserverGuideAggregate> repository) : Endpoint<Request, Ok<Response>>
+namespace Feature.ObserverGuide.List;
+
+public class Endpoint(IAuthorizationService authorizationService,
+    ICurrentUserProvider currentUserProvider,
+    IReadRepository<ObserverGuideAggregate> repository,
+    IFileStorageService fileStorageService)
+    : Endpoint<Request, Results<Ok<Response>, NotFound>>
 {
     public override void Configure()
     {
@@ -9,8 +20,38 @@ public class Endpoint(IReadRepository<ObserverGuideAggregate> repository) : Endp
         Options(x => x.WithTags("observer-guide"));
     }
 
-    public override async Task<Ok<Response>> ExecuteAsync(Request req, CancellationToken ct)
+    public override async Task<Results<Ok<Response>, NotFound>> ExecuteAsync(Request req, CancellationToken ct)
     {
-        throw new NotImplementedException();
+        var authorizationResult = await authorizationService.AuthorizeAsync(User, new MonitoringObserverRequirement(req.ElectionRoundId));
+        if (!authorizationResult.Succeeded)
+        {
+            return TypedResults.NotFound();
+        }
+
+        var specification = new GetObserverGuidesSpecification(currentUserProvider.GetUserId());
+        var observerGuides = await repository.ListAsync(specification, ct);
+
+        var observerGuideModels = observerGuides
+            .Select(async observerGuide =>
+            {
+                var presignedUrl = await fileStorageService.GetPresignedUrlAsync(
+                    observerGuide.FilePath,
+                    observerGuide.UploadedFileName,
+                    ct);
+
+                return new ObserverGuideModel
+                {
+                    Title = observerGuide.Title,
+                    FileName = observerGuide.FileName,
+                    PresignedUrl = (presignedUrl as GetPresignedUrlResult.Ok)?.Url ?? string.Empty,
+                    MimeType = observerGuide.MimeType,
+                    UrlValidityInSeconds = (presignedUrl as GetPresignedUrlResult.Ok)?.UrlValidityInSeconds ?? 0,
+                    Id = observerGuide.Id
+                };
+            })
+            .Select(t => t.Result)
+            .ToList();
+
+        return TypedResults.Ok(new Response { Guides = observerGuideModels });
     }
 }
---
+++ src/Feature.ObserverGuide/Specifications/GetObserverGuidesSpecification.cs
@@ -0,0 +1,15 @@
+﻿using Ardalis.Specification;
+
+namespace Feature.ObserverGuide.Specifications;
+
+public sealed class GetObserverGuidesSpecification : Specification<ObserverGuideAggregate>
+{
+    public GetObserverGuidesSpecification(Guid? observerId)
+    {
+        Query.Include(x=> x.MonitoringNgo)
+            .ThenInclude(x=> x.MonitoringObservers)
+            .Where(x => x.MonitoringNgo
+                .MonitoringObservers
+                .Any(y=> y.ObserverId == observerId));
+    }
+}
