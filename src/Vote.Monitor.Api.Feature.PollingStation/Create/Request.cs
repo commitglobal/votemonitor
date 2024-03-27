7745237f9e52178a9c98874c97d904eb49ba194a
Iulia Dormenco <iuliaolteanu12@gmail.com>
2024-03-27 10:49:32 +0200

    fix polling station create

--- src/Vote.Monitor.Api.Feature.PollingStation/Create/Request.cs
+++ src/Vote.Monitor.Api.Feature.PollingStation/Create/Request.cs
@@ -1,7 +1,7 @@
 ﻿namespace Vote.Monitor.Api.Feature.PollingStation.Create;
 public class Request
 {
-    public required Guid ElectionRoundId { get; set; }
+    public Guid ElectionRoundId { get; set; }
     public required int DisplayOrder { get; set; }
     public required string Address { get; set; }
     public required Dictionary<string, string> Tags { get; set; }
