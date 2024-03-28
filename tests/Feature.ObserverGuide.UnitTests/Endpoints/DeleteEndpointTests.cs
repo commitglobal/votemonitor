f4a5f0913b5627b6b0b0fd4090fd7c57a3503384
Iulia Dormenco <iuliaolteanu12@gmail.com>
2024-03-28 17:24:45 +0200

    add delete tests

---
+++ tests/Feature.ObserverGuide.UnitTests/Endpoints/DeleteEndpointTests.cs
@@ -0,0 +1,129 @@
+﻿using System.Security.Claims;
+using FastEndpoints;
+using Feature.ObserverGuide.Delete;
+using Feature.ObserverGuide.Specifications;
+using FluentAssertions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http.HttpResults;
+using NSubstitute;
+using NSubstitute.ReturnsExtensions;
+using Vote.Monitor.Core.Services.Security;
+using Vote.Monitor.Domain.Repository;
+using Vote.Monitor.TestUtils.Fakes.Aggregates;
+
+namespace Feature.ObserverGuide.UnitTests.Endpoints;
+
+public class DeleteEndpointTests
+{
+    private readonly IRepository<ObserverGuideAggregate> _repository;
+    private readonly IAuthorizationService _authorizationService;
+    private readonly Endpoint _endpoint;
+
+    public DeleteEndpointTests()
+    {
+        _repository = Substitute.For<IRepository<ObserverGuideAggregate>>();
+        _authorizationService = Substitute.For<IAuthorizationService>();
+        var currentUserProvider = Substitute.For<ICurrentUserProvider>();
+
+        _endpoint = Factory.Create<Endpoint>(_authorizationService,
+            currentUserProvider,
+            _repository);
+    }
+
+    [Fact]
+    public async Task ShouldReturnNoContent_WhenObserverGuideExists()
+    {
+        // Arrange
+        var observerGuideId = Guid.NewGuid();
+        var fileName = "photo.jpg";
+
+        var fakeElectionRound = new ElectionRoundAggregateFaker().Generate();
+        var fakeObserverGuide = new ObserverGuideAggregateFaker(observerGuideId, fileName).Generate();
+
+        _authorizationService.AuthorizeAsync(Arg.Any<ClaimsPrincipal>(), Arg.Any<object?>(), Arg.Any<IEnumerable<IAuthorizationRequirement>>())
+            .Returns(AuthorizationResult.Success());
+
+        _repository.FirstOrDefaultAsync(Arg.Any<GetObserverGuideSpecification>())
+            .Returns(fakeObserverGuide);
+
+        // Act
+        var request = new Request
+        {
+            ElectionRoundId = fakeElectionRound.Id,
+            Id = observerGuideId
+        };
+        var result = await _endpoint.ExecuteAsync(request, default);
+
+        // Assert
+        await _repository
+            .Received(1)
+            .UpdateAsync(Arg.Is<ObserverGuideAggregate>(x => x.Id == fakeObserverGuide.Id
+                                                             && x.IsDeleted));
+
+
+        result
+            .Should().BeOfType<Results<NoContent, NotFound>>()
+            .Which
+            .Result.Should().BeOfType<NoContent>();
+    }
+
+    [Fact]
+    public async Task ShouldReturnNotFound_WhenNotAuthorised()
+    {
+        // Arrange
+        var observerGuideId = Guid.NewGuid();
+        var fileName = "photo.jpg";
+
+        var fakeElectionRound = new ElectionRoundAggregateFaker().Generate();
+        var fakeObserverGuide = new ObserverGuideAggregateFaker(observerGuideId, fileName).Generate();
+
+        _authorizationService.AuthorizeAsync(Arg.Any<ClaimsPrincipal>(), Arg.Any<object?>(), Arg.Any<IEnumerable<IAuthorizationRequirement>>())
+            .Returns(AuthorizationResult.Failed());
+
+        _repository.FirstOrDefaultAsync(Arg.Any<GetObserverGuideSpecification>())
+            .Returns(fakeObserverGuide);
+
+        // Act
+        var request = new Request
+        {
+            ElectionRoundId = fakeElectionRound.Id,
+            Id = observerGuideId
+        };
+        var result = await _endpoint.ExecuteAsync(request, default);
+
+        // Assert
+        result
+            .Should().BeOfType<Results<NoContent, NotFound>>()
+            .Which
+            .Result.Should().BeOfType<NotFound>();
+    }
+
+    [Fact]
+    public async Task ShouldReturnNotFound_WhenNotFound()
+    {
+        // Arrange
+        var observerGuideId = Guid.NewGuid();
+
+        var fakeElectionRound = new ElectionRoundAggregateFaker().Generate();
+
+        _authorizationService.AuthorizeAsync(Arg.Any<ClaimsPrincipal>(), Arg.Any<object?>(), Arg.Any<IEnumerable<IAuthorizationRequirement>>())
+            .Returns(AuthorizationResult.Success());
+
+        _repository.FirstOrDefaultAsync(Arg.Any<GetObserverGuideSpecification>())
+            .ReturnsNull();
+
+        // Act
+        var request = new Request
+        {
+            ElectionRoundId = fakeElectionRound.Id,
+            Id = observerGuideId
+        };
+        var result = await _endpoint.ExecuteAsync(request, default);
+
+        // Assert
+        result
+            .Should().BeOfType<Results<NoContent, NotFound>>()
+            .Which
+            .Result.Should().BeOfType<NotFound>();
+    }
+}
