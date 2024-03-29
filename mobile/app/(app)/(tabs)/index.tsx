157b76b622a7dbef02bd8babd5e99a919b69fcc7
luciatugui <lucia.tugui@wearetribus.com>
2024-03-29 17:30:54 +0200

    style: header styling

--- mobile/app/(app)/(tabs)/index.tsx
+++ mobile/app/(app)/(tabs)/index.tsx
@@ -1,12 +1,14 @@
 import { View, Text } from "react-native";
 import { useAuth } from "../../../hooks/useAuth";
 import OfflinePersistComponentExample from "../../../components/OfflinePersistComponentExample";
+import { StatusBar } from "react-native";
 
 const Index = () => {
   const { signOut } = useAuth();
 
   return (
     <View style={{ gap: 20 }}>
+      <StatusBar barStyle="light-content" />
       <Text>Observation</Text>
       <OfflinePersistComponentExample></OfflinePersistComponentExample>
       <Text onPress={signOut}>Logout</Text>
--- mobile/app/(app)/_layout.tsx
+++ mobile/app/(app)/_layout.tsx
@@ -1,10 +1,18 @@
-import { Redirect } from "expo-router";
+import { Redirect, useNavigation } from "expo-router";
+import { DrawerActions } from "@react-navigation/native";
 import { useAuth } from "../../hooks/useAuth";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { Drawer } from "expo-router/drawer";
+import { useTheme } from "tamagui";
+import { TouchableOpacity } from "react-native";
+import DotsVerticalSVG from "../../assets/icons/dots-vertical.svg";
+import MenuAlt2SVG from "../../assets/icons/menu-alt-2.svg";
+import { Typography } from "../../components/Typography";
 
 const AppLayout = () => {
   const { isAuthenticated } = useAuth();
+  const theme = useTheme();
+  const navigation = useNavigation();
 
   if (!isAuthenticated) {
     // On web, static rendering will stop here as the user is not authenticated
@@ -14,8 +22,54 @@ const AppLayout = () => {
 
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <Drawer screenOptions={{ drawerType: "front" }}>
-        <Drawer.Screen name="(tabs)" options={{ headerShown: true }} />
+      <Drawer
+        screenOptions={{
+          drawerType: "front",
+        }}
+      >
+        <Drawer.Screen
+          name="(tabs)"
+          options={{
+            headerShown: true,
+            headerStyle: { backgroundColor: theme.purple5.val },
+            headerTintColor: "white",
+            headerTitleAlign: "center",
+            headerTitleStyle: {
+              fontSize: 16,
+            },
+            headerTitle: (props) => (
+              <Typography
+                preset="body1"
+                style={{
+                  fontWeight: "500",
+                  color: props.tintColor,
+                  lineHeight: 24,
+                }}
+              >
+                2024 EU Parliamentary RO
+              </Typography>
+            ),
+            // drawer icon left
+            headerLeft: () => (
+              <TouchableOpacity
+                onPress={() => navigation.dispatch(DrawerActions.openDrawer())}
+                style={{ marginLeft: 14 }}
+              >
+                <MenuAlt2SVG fill="white" />
+              </TouchableOpacity>
+            ),
+
+            //TODO: decide what this right should do, onPress=...
+            headerRight: () => (
+              <TouchableOpacity
+                style={{ marginRight: 14 }}
+                onPress={() => console.log("To be decided what to do here")}
+              >
+                <DotsVerticalSVG fill="white" />
+              </TouchableOpacity>
+            ),
+          }}
+        />
       </Drawer>
     </GestureHandlerRootView>
   );
