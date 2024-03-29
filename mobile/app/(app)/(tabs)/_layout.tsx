d9a8e231889c71b3b9a82b16b0c6d00e8bcc5543
luciatugui <lucia.tugui@wearetribus.com>
2024-03-29 13:28:52 +0200

    feat: styling navigation tab bar

--- mobile/app/(app)/(tabs)/_layout.tsx
+++ mobile/app/(app)/(tabs)/_layout.tsx
@@ -1,55 +1,78 @@
 import React from "react";
-import FontAwesome from "@expo/vector-icons/FontAwesome";
 import { Tabs } from "expo-router";
+import ObservationSVG from "../../../assets/icons/observation.svg";
+import QuickReportSVG from "../../../assets/icons/quick-report.svg";
+import GuidesSVG from "../../../assets/icons/Learning.svg";
+import InboxSVG from "../../../assets/icons/Inbox.svg";
+import MoreSVG from "../../../assets/icons/More.svg";
+import { TextStyle, ViewStyle } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { useTheme } from "tamagui";
 
 export default function TabLayout() {
+  const insets = useSafeAreaInsets();
+  const theme = useTheme();
+
   return (
-    <Tabs screenOptions={{ tabBarActiveTintColor: "blue", headerShown: false }}>
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: theme.purple5.val,
+        tabBarHideOnKeyboard: true,
+        headerShown: false,
+        tabBarStyle: [$tabBar, { height: insets.bottom + 60 }],
+        tabBarLabelStyle: $tabBarLabel,
+        tabBarItemStyle: $tabBarItem,
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
           title: "Observation",
-          tabBarIcon: ({ color }) => (
-            <FontAwesome size={28} name="home" color={color} />
-          ),
+          tabBarIcon: ({ color }) => <ObservationSVG fill={color} />,
         }}
       />
       <Tabs.Screen
         name="quick-report"
         options={{
           title: "Quick Report",
-          tabBarIcon: ({ color }) => (
-            <FontAwesome size={28} name="exclamation-triangle" color={color} />
-          ),
+          tabBarIcon: ({ color }) => <QuickReportSVG fill={color} />,
         }}
       />
       <Tabs.Screen
         name="guides"
         options={{
           title: "Guides",
-          tabBarIcon: ({ color }) => (
-            <FontAwesome size={28} name="address-book" color={color} />
-          ),
+          tabBarIcon: ({ color }) => <GuidesSVG fill={color} />,
         }}
       />
       <Tabs.Screen
         name="inbox"
         options={{
           title: "Inbox",
-          tabBarIcon: ({ color }) => (
-            <FontAwesome size={28} name="inbox" color={color} />
-          ),
+          tabBarIcon: ({ color }) => <InboxSVG fill={color} />,
         }}
       />
       <Tabs.Screen
         name="more"
         options={{
           title: "More",
-          tabBarIcon: ({ color }) => (
-            <FontAwesome size={28} name="dot-circle-o" color={color} />
-          ),
+          tabBarIcon: ({ color }) => <MoreSVG fill={color} />,
         }}
       />
     </Tabs>
   );
 }
+
+const $tabBar: ViewStyle = {
+  backgroundColor: "white",
+};
+
+const $tabBarLabel: TextStyle = {
+  marginBottom: 4,
+  fontFamily: "Roboto",
+  fontSize: 12,
+};
+
+const $tabBarItem: ViewStyle = {
+  paddingTop: 2,
+};
