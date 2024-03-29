45ceee4fa82d32ebacd4bfd2c19dcb5b0e6d997c
Ion Dormenco <idormenco@gmail.com>
2024-03-29 17:52:19 +0200

    Update Endpoint.cs

--- src/Feature.PollingStation.Information.Form/Get/Endpoint.cs
+++ src/Feature.PollingStation.Information.Form/Get/Endpoint.cs
@@ -7,7 +7,10 @@ public class Endpoint(IReadRepository<PollingStationInfoFormAggregate> repositor
         Get("/api/election-rounds/{electionRoundId}/polling-station-information-form");
         DontAutoTag();
         Options(x => x.WithTags("polling-station-information-form", "mobile"));
-        Description(x => x.Accepts<Request>());
+        Description(x => x.Accepts<Request>()); Summary(s => {
+            s.Summary = "Gets the polling station information form.";
+            s.Description = "Gets the polling station information form. Observers will fill in this form when they visit a polling station.";
+        });
     }
 
     public override async Task<Results<Ok<PollingStationInformationFormModel>, NotFound>> ExecuteAsync(Request req, CancellationToken ct)
