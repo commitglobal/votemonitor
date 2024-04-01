8b7277d6feab23ab2879e6e6e210db81af4a8e46
luciatugui <lucia.tugui@wearetribus.com>
2024-04-01 11:22:52 +0300

    feat: add custom spacing

--- mobile/app/(app)/(tabs)/quick-report.tsx
+++ mobile/app/(app)/(tabs)/quick-report.tsx
@@ -1,9 +1,18 @@
+import React from "react";
 import { View, Text } from "react-native";
+import { Typography } from "../../../components/Typography";
+import { getTokens } from "@tamagui/core";
+import { Stack } from "tamagui";
 
 const QuickReport = () => {
   return (
     <View>
       <Text>Quick Report</Text>
+
+      {/* example of using default spacing for padding*/}
+      <Stack backgroundColor={"$purple5"} padding="$sm">
+        <Typography size="xl">Hello typo</Typography>
+      </Stack>
     </View>
   );
 };
