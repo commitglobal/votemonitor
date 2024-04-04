8ec2c7d694207cf07901ae3c6e489f2ee9b52052
Birloi Florian <birloiflorian@Birlois-MacBook-Pro.local>
2024-04-04 14:37:49 +0300

    refactor: update button layouting and presets

--- mobile/components/Button.tsx
+++ mobile/components/Button.tsx
@@ -1,107 +1,107 @@
 import React from "react";
-import { StyleProp, TextStyle, ViewStyle} from "react-native";
-import { 
-  Button as TamaguiButton, 
-  ButtonProps as TamaguiButtonProps, 
-  Variable, 
-  useTheme, 
+import { StyleProp, TextStyle, ViewStyle } from "react-native";
+import {
+  Button as TamaguiButton,
+  ButtonProps as TamaguiButtonProps,
+  styled,
+  useTheme,
 } from "tamagui";
-
-import { UseThemeResult } from '@tamagui/web/src/hooks/useTheme';
 import { Typography } from "./Typography";
-import {tokens} from "../theme/tokens"
-
-
 
 type PresetType = "default" | "outlined" | "chromeless" | "red";
-
 export interface ButtonProps extends TamaguiButtonProps {
-    text: String;
-
-    style? : StyleProp<ViewStyle>;
-
-    /**
-     * One of the different types of button presets.
-     */
-    preset? : PresetType;
-
-    /**
-     * Optional styling for overriding presetType text styling
-     */
-    textStyle? : TextStyle;
+  children: string;
+  /**
+   * Style overrides
+   */
+  style?: StyleProp<ViewStyle>;
+  /**
+   * One of the different types of button presets.
+   */
+  preset?: PresetType;
+  /**
+   * Optional styling for overriding presetType text styling
+   */
+  textStyle?: TextStyle;
 }
 
 /**
  * Button components which supports 3 initial presets: filled, outlined and chromeless
  * This component is a HOC over the Tamagui Button.
- * @param {ButtonProps} props - The props for the `Buttom` component.
+ * @param {ButtonProps} props - The props for the `Button` component.
  * @returns {JSX.Element} The rendered `Button` component.
  */
-export function Button(props : ButtonProps): JSX.Element {
-    const theme = useTheme()
+const Button = (props: ButtonProps): JSX.Element => {
+  const theme = useTheme();
 
-    const {style: $styleOverride, text, textStyle, ...rest} = props;
-    const presetType : PresetType = props.preset ?? "default";
-    const preset = $presets(theme, tokens.space)[presetType];
+  const { style: $styleOverride, children, textStyle, ...rest } = props;
+  const presetType: PresetType = props.preset ?? "default";
+  const $presetTextStyles = {
+    color:
+      presetType === "default" || presetType === "red"
+        ? "white"
+        : theme.$purple5?.val,
+  };
 
-    const $buttonStyles: StyleProp<ViewStyle> = [
-      preset,
-      $styleOverride,
-    ];
+  const $textStyles: TextStyle = { ...$presetTextStyles, ...textStyle };
 
-    const $presetTextStyles = {
-        color: presetType === 'default' || presetType === 'red' ?
-          'white' :
-          theme.$purple5?.val,
-    };
-
-    const $textStyles: TextStyle = {...$presetTextStyles, ...textStyle};
-
-    return (
-      <TamaguiButton {...rest} style={$buttonStyles}>
-        <Typography
-          preset="body1"
-          style={{ ...$textStyles, fontWeight: "500" }}>
-            {text}
-        </Typography>
-      </TamaguiButton>
-    );
-} 
-
-
-
-const $presets = (
-  theme: UseThemeResult,
-  spacing: { [key: string]: Variable<number> }
-) => {
-  const $baseStyle : StyleProp<ViewStyle> = {
-    paddingHorizontal: spacing.md.val,
-    paddingVertical: spacing.xs.val,
+  const StyledButton = styled(TamaguiButton, {
+    name: "StyledButton",
+    paddingHorizontal: "$md",
+    paddingVertical: "$xs",
     borderRadius: 8,
-    backgroundColor : theme.$purple5?.val,
-    alignItems: 'center',
-  };
-
-  return {
-      default: $baseStyle, 
-
-      outlined: {
-          ...$baseStyle,
+    backgroundColor: "$purple5",
+    alignItems: "center",
+    disabledStyle: {
+      backgroundColor: "$gray3",
+    },
+    variants: {
+      presets: {
+        default: {
+          pressStyle: {
+            backgroundColor: "$purple5",
+            opacity: 0.8,
+          },
+        },
+        outlined: {
           borderWidth: 1,
-          borderColor: theme.$purple5?.val,
-          backgroundColor: 'transparent',
-      } as StyleProp<ViewStyle>,
+          borderColor: "$purple5",
+          backgroundColor: "transparent",
+          pressStyle: {
+            backgroundColor: "transparent",
+            opacity: 0.8,
+            borderColor: "$purple7",
+          },
+          disabledStyle: {
+            backgroundColor: "transparent",
+            borderColor: "$gray3",
+          },
+        },
+        chromeless: {
+          backgroundColor: "transparent",
+          pressStyle: {
+            backgroundColor: "transparent",
+            opacity: 0.8,
+          },
+        },
+        red: {
+          backgroundColor: "$red10",
+          pressStyle: {
+            backgroundColor: "$red10",
+            opacity: 0.8,
+          },
+        },
+      },
+    } as const,
+  });
 
-      chromeless: {
-          ...$baseStyle,
-          backgroundColor: 'transparent',
-      } as StyleProp<ViewStyle>,
-
-      red: {
-        ...$baseStyle,
-        backgroundColor: theme.$red10?.val,
-      } as StyleProp<ViewStyle>,
-  
-  };
-
+  return (
+    <StyledButton presets={presetType} style={$styleOverride} {...rest}>
+      <Typography preset="body1" style={{ ...$textStyles, fontWeight: "500" }}>
+        {children}
+      </Typography>
+    </StyledButton>
+  );
 };
+
+export default Button;
