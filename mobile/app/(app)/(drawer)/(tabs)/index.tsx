47dfce45f26e951c2e8068829c17ad3220d3094e
luciatugui <lucia.tugui@wearetribus.com>
2024-04-04 13:23:21 +0300

    feat: card footer component

--- mobile/app/(app)/(drawer)/(tabs)/index.tsx
+++ mobile/app/(app)/(drawer)/(tabs)/index.tsx
@@ -2,8 +2,10 @@ import { View, Text } from "react-native";
 import { useAuth } from "../../../../hooks/useAuth";
 import OfflinePersistComponentExample from "../../../../components/OfflinePersistComponentExample";
 import { StatusBar } from "react-native";
-import { Button } from "tamagui";
+import { Button, Card } from "tamagui";
 import { router } from "expo-router";
+import CardFooter from "../../../../components/CardFooter";
+import { Typography } from "../../../../components/Typography";
 
 const Index = () => {
   const { signOut } = useAuth();
@@ -21,6 +23,14 @@ const Index = () => {
       <Button onPress={() => router.push("/polling-station-questionnaire")}>
         Go To Polling Station Qustionnaire
       </Button>
+      <Card padding="$md">
+        <Typography>Card component</Typography>
+        <CardFooter
+          text="Card footer"
+          action={() => console.log("card footer")}
+          marginTop="$sm"
+        />
+      </Card>
       <Text onPress={signOut}>Logout</Text>
     </View>
   );
