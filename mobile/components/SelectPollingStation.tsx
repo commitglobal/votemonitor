84a5d14ee5f5fc063b43299a0cd31f6c2bb7c6fb
luciatugui <lucia.tugui@wearetribus.com>
2024-04-04 10:46:00 +0300

    feat: change sheet header text color and numberOfLines

--- mobile/components/SelectPollingStation.tsx
+++ mobile/components/SelectPollingStation.tsx
@@ -60,8 +60,7 @@ const SelectPollingStation = () => {
                   My polling stations
                 </Typography>
                 <View marginTop="$xxs">
-                  {/* //TODO: typography doesnt accept nrOfLines */}
-                  <Typography numberOfLines={3}>
+                  <Typography numberOfLines={3} color="$gray5">
                     You can switch between polling stations if you want to
                     revisit form answers or polling station information.
                   </Typography>
