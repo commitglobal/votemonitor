e175cc3981a175ce37955abe7a81bde5ae9fe1cf
Ion Dormenco <idormenco@gmail.com>
2024-04-08 18:15:11 +0300

    Update Installer.cs (#283)

--- src/Vote.Monitor.Core/Services/FileStorage/S3/Installer.cs
+++ src/Vote.Monitor.Core/Services/FileStorage/S3/Installer.cs
@@ -1,6 +1,6 @@
-﻿using Amazon.Extensions.NETCore.Setup;
-using Amazon.Runtime;
+﻿using Amazon.Runtime;
 using Amazon.S3;
+using Humanizer.Configuration;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Vote.Monitor.Core.Services.FileStorage.Contracts;
@@ -9,28 +9,24 @@ namespace Vote.Monitor.Core.Services.FileStorage.S3;
 
 internal static class Installer
 {
-    internal static IServiceCollection AddS3FileStorage(this IServiceCollection services, IConfiguration configurationSection)
+    internal static IServiceCollection AddS3FileStorage(this IServiceCollection services, IConfiguration configuration)
     {
-        services.Configure<S3Options>(configurationSection);
+        services.Configure<S3Options>(configuration);
 
-        services.AddAWSService<IAmazonS3>(GetAwsOptions(configurationSection));
+        services.AddSingleton<IAmazonS3>(_ =>
+        {
+            string awsRegion = configuration.GetSection("AWSRegion").Value!;
+            string awsAccessKey = configuration.GetSection("AWSAccessKey").Value!;
+            string awsSecretKey = configuration.GetSection("AWSSecretKey").Value!;
+
+            var region = Amazon.RegionEndpoint.GetBySystemName(awsRegion);
+            var credentials = new BasicAWSCredentials(awsAccessKey, awsSecretKey);
+
+            return new AmazonS3Client(credentials, region);
+
+        });
         services.AddSingleton<IFileStorageService, S3FileStorageService>();
 
         return services;
-    }
-
-    private static AWSOptions GetAwsOptions(IConfiguration configuration)
-    {
-        string region = configuration.GetSection("AWSRegion").Value;
-        string awsAccessKey = configuration.GetSection("AWSAccessKey").Value;
-        string awsSecretKey = configuration.GetSection("AWSSecretKey").Value;
-
-        var awsOptions = new AWSOptions
-        {
-            Region = Amazon.RegionEndpoint.GetBySystemName(region),
-            Credentials = new BasicAWSCredentials(awsAccessKey, awsSecretKey)
-        };
-
-        return awsOptions;
     }
 }
