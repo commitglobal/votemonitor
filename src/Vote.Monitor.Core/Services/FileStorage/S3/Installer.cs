318c5b95cdfc83ba51a3e17d216dc4aa44603f87
Ion Dormenco <idormenco@gmail.com>
2024-04-08 19:09:09 +0300

    Bugfix/downgrade s3 aws (#285)
    * downgrade aws sdk
    
    * Last hope

--- src/Vote.Monitor.Core/Services/FileStorage/S3/Installer.cs
+++ src/Vote.Monitor.Core/Services/FileStorage/S3/Installer.cs
@@ -3,6 +3,7 @@ using Amazon.S3;
 using Humanizer.Configuration;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Serilog;
 using Vote.Monitor.Core.Services.FileStorage.Contracts;
 
 namespace Vote.Monitor.Core.Services.FileStorage.S3;
@@ -12,19 +13,22 @@ internal static class Installer
     internal static IServiceCollection AddS3FileStorage(this IServiceCollection services, IConfiguration configuration)
     {
         services.Configure<S3Options>(configuration);
-
         services.AddSingleton<IAmazonS3>(_ =>
         {
             string awsRegion = configuration.GetSection("AWSRegion").Value!;
             string awsAccessKey = configuration.GetSection("AWSAccessKey").Value!;
             string awsSecretKey = configuration.GetSection("AWSSecretKey").Value!;
+            Log.Logger.Warning("starting aws sdk init {awsRegion} {awsAccessKey} {awsSecretKey}", awsRegion, awsAccessKey, awsSecretKey);
 
             var region = Amazon.RegionEndpoint.GetBySystemName(awsRegion);
             var credentials = new BasicAWSCredentials(awsAccessKey, awsSecretKey);
 
-            return new AmazonS3Client(credentials, region);
+            var client = new AmazonS3Client(credentials, region);
+            Log.Logger.Warning("done starting aws sdk init");
 
+            return client;
         });
+
         services.AddSingleton<IFileStorageService, S3FileStorageService>();
 
         return services;
