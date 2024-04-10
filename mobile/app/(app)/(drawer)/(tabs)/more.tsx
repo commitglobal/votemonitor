1482a864d8526652159697fa50cd793483d3ffc9
MadalinaZanficu <zanficumadalina@gmail.com>
2024-04-10 19:18:28 +0300

    Added all sections with proper styling and integrated translations.

--- mobile/app/(app)/(drawer)/(tabs)/more.tsx
+++ mobile/app/(app)/(drawer)/(tabs)/more.tsx
@@ -1,18 +1,63 @@
-import { View, Text, XStack } from "tamagui";
+import React, { useState } from "react";
+import { View, XStack } from "tamagui";
 import Card from "../../../../components/Card";
 import { Screen } from "../../../../components/Screen";
 import { Typography } from "../../../../components/Typography";
 import { Icon } from "../../../../components/Icon";
+import { useTranslation } from "react-i18next";
+import * as Linking from "expo-linking";
+import { tokens } from "../../../../theme/tokens";
 
 const More = () => {
+  const { t } = useTranslation("more");
+
+  // TODO: Change these consts
+  const appVersion = "2.0.4";
+  const appLanguage = "English (United States)";
+  const URL = "https://www.google.com/";
+
   return (
-    <Screen preset="auto">
-      <Text> More</Text>
+    <Screen
+      preset="auto"
+      backgroundColor="white"
+      contentContainerStyle={{
+        gap: tokens.space.md.val,
+        paddingHorizontal: tokens.space.md.val,
+        paddingVertical: tokens.space.xl.val,
+      }}
+    >
       <Section
-        headerText="Change app language"
-        subHeaderText="English (United States)"
+        headerText={t("change-language")}
+        subHeaderText={appLanguage}
         iconName="check"
+        chevronRight={true}
       ></Section>
+      <Section headerText={t("change-password")} iconName="check" chevronRight={true}></Section>
+      <Section
+        headerText={t("terms")}
+        iconName="check"
+        chevronRight={true}
+        action={() => {
+          Linking.openURL(URL);
+        }}
+      ></Section>
+      <Section
+        headerText={t("privacy_policy")}
+        iconName="check"
+        chevronRight={true}
+        action={() => {
+          Linking.openURL(URL);
+        }}
+      ></Section>
+      <Section
+        headerText={t("about")}
+        subHeaderText={t("app_version", { value: appVersion })}
+        iconName="check"
+        chevronRight={true}
+      ></Section>
+      <Section headerText={t("support")} iconName="check"></Section>
+      <Section headerText={t("feedback")} iconName="check"></Section>
+      <Section headerText={t("logout")} iconName="check"></Section>
     </Screen>
   );
 };
@@ -21,14 +66,24 @@ interface SectionProps {
   headerText: string;
   subHeaderText?: string;
   iconName: string;
+  chevronRight?: boolean;
+  action?: () => void;
 }
 
 const Section = (props: SectionProps) => {
-  const { headerText, subHeaderText, iconName } = props;
+  const { headerText, subHeaderText, iconName, chevronRight, action } = props;
   const hasSubHeader = subHeaderText ? true : false;
+  const hasChevronRight = chevronRight ? true : false;
+
+  const [isPressed, setIsPressed] = useState(false);
 
   return (
-    <Card>
+    <Card
+      onPress={action}
+      onPressIn={() => setIsPressed(true)}
+      onPressOut={() => setIsPressed(false)}
+      opacity={isPressed ? 0.5 : 1}
+    >
       <XStack alignItems="center" justifyContent="space-between">
         <XStack alignItems="center" gap="$xxs">
           <Icon size={24} icon={iconName} color="black" />
@@ -38,7 +93,7 @@ const Section = (props: SectionProps) => {
           </View>
         </XStack>
 
-        <Icon size={24} icon="chevronRight" color="$purple7" />
+        {hasChevronRight && <Icon size={24} icon="chevronRight" color="$purple7" />}
       </XStack>
     </Card>
   );
