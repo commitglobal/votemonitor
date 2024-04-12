9062ed028533c6b88295090a862c0145ffa882ed
MadalinaZanficu <zanficumadalina@gmail.com>
2024-04-12 12:06:04 +0300

    Integrated header for polling-station-wizzard screen.

--- mobile/app/(app)/_layout.tsx
+++ mobile/app/(app)/_layout.tsx
@@ -1,9 +1,17 @@
 import { Redirect, Stack } from "expo-router";
 import { useAuth } from "../../hooks/useAuth";
 import { PortalProvider } from "tamagui";
+import Header from "../../components/Header";
+import { Icon } from "../../components/Icon";
+import { getHeaderTitle } from "@react-navigation/elements";
+import { StyleProp, ViewStyle } from "react-native";
+import { useTheme } from "tamagui";
+import { useTranslation } from "react-i18next";
 
 const AppLayout = () => {
   const { isAuthenticated } = useAuth();
+  const theme = useTheme();
+  const { t } = useTranslation("polling_station_information");
 
   if (!isAuthenticated) {
     // On web, static rendering will stop here as the user is not authenticated
@@ -15,7 +23,29 @@ const AppLayout = () => {
     <PortalProvider>
       <Stack>
         <Stack.Screen name="(drawer)" options={{ headerShown: false }} />
-        <Stack.Screen name="polling-station-wizzard" />
+        <Stack.Screen
+          name="polling-station-wizzard"
+          options={{
+            header: ({ navigation, route, options }) => {
+              const title = getHeaderTitle(options, route.name);
+              return (
+                <Header
+                  title={t("header.title")}
+                  titleColor="white"
+                  backgroundColor={theme.purple5?.val}
+                  barStyle="light-content"
+                  style={options.headerStyle as StyleProp<ViewStyle>}
+                  leftIcon={<Icon icon="chevronLeft" color="white" />}
+                  onLeftPress={() => navigation.goBack()}
+                  rightIcon={<Icon icon="dotsVertical" color="white" />}
+                  onRightPress={() => {
+                    console.log("on right action press");
+                  }}
+                />
+              );
+            },
+          }}
+        />
         <Stack.Screen name="form-questionnaire" />
         <Stack.Screen name="polling-station-questionnaire" />
       </Stack>
