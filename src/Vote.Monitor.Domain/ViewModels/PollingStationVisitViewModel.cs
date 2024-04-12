b8fc3d4b596e234307ef5b03bddc8cbd86bd5302
Ion Dormenco <idormenco@gmail.com>
2024-04-12 15:13:00 +0300

    Fix form versions

--- src/Vote.Monitor.Domain/ViewModels/PollingStationVisitViewModel.cs
+++ src/Vote.Monitor.Domain/ViewModels/PollingStationVisitViewModel.cs
@@ -4,6 +4,13 @@ public class PollingStationVisitViewModel
 {
     public Guid ElectionRoundId { get; set; }
     public Guid PollingStationId { get; set; }
+    public string Level1 { get; set; }
+    public string Level2 { get; set; }
+    public string Level3 { get; set; }
+    public string Level4 { get; set; }
+    public string Level5 { get; set; }
+    public string Address { get; set; }
+    public string Number { get; set; }
     public Guid MonitoringNgoId { get; set; }
     public Guid NgoId { get; set; }
     public Guid MonitoringObserverId { get; set; }
