cee403a65c5392412c5bb6de983084e61aa39fcd
luciatugui <lucia.tugui@wearetribus.com>
2024-04-15 15:45:12 +0300

    fix: stop updating date on every onChange trigger for date input

--- mobile/components/Inputs/DateInput.tsx
+++ mobile/components/Inputs/DateInput.tsx
@@ -4,6 +4,7 @@ import RNDateTimePicker, { DateTimePickerEvent } from "@react-native-community/d
 import { Keyboard, Platform } from "react-native";
 import { Typography } from "../Typography";
 import { Icon } from "../Icon";
+import Button from "../Button";
 
 export interface DateInputProps {
   value: Date;
@@ -21,6 +22,9 @@ export const DateInput: React.FC<DateInputProps> = ({
   placeholder,
 }) => {
   const [open, setOpen] = useState(false);
+  // on ios we use a temporary date, as the onChange function gets triggered every time the user picks a new date
+  // therefore, we will update the FINAL date state (that comes from the outside), only onDonePress
+  const [tempDate, setTempDate] = useState(new Date());
 
   const handleSheetOpen = () => {
     Keyboard.dismiss();
@@ -28,13 +32,28 @@ export const DateInput: React.FC<DateInputProps> = ({
   };
 
   const onDateChange = (event: DateTimePickerEvent, selectedDate: Date | undefined) => {
-    // send selected date to the form
-    onChange(selectedDate);
-
-    // on android, close the modal
-    if (Platform.OS === "android") {
-      setOpen(false);
+    if (Platform.OS === "ios") {
+      selectedDate && setTempDate(selectedDate);
+    } else {
+      // press OK - set the time
+      if (event.type === "set") {
+        onClose();
+        onChange(selectedDate);
+      } else if (event.type === "dismissed") {
+        // press Cancel - close modal
+        onClose();
+      }
     }
+  };
+
+  const onDonePress = () => {
+    // if the onChange was not already triggered and the value wasn't updated, we set the date with the current one
+    onChange(tempDate);
+    onClose();
+  };
+
+  const onClose = () => {
+    setOpen(false);
   };
 
   return (
@@ -60,7 +79,9 @@ export const DateInput: React.FC<DateInputProps> = ({
         <Sheet modal native open={open} onOpenChange={setOpen} zIndex={100_000} snapPoints={[45]}>
           <Sheet.Overlay />
           <Sheet.Frame padding="$md">
-            <XStack gap="$sm" justifyContent="space-between" width="100%"></XStack>
+            <XStack gap="$sm" justifyContent="flex-end" width="100%">
+              <Button onPress={onDonePress}>Done</Button>
+            </XStack>
             <XStack flex={1} justifyContent="center" alignItems="center">
               <RNDateTimePicker
                 value={value || new Date()}
