c48029ce5b2b4ae0083b9b64cc443fc5f7e423bc
MadalinaZanficu <zanficumadalina@gmail.com>
2024-04-18 17:59:35 +0300

    Base work for forgot password screen.

--- mobile/app/forgot-password.tsx
+++ mobile/app/forgot-password.tsx
@@ -1,17 +1,94 @@
-import { View, Text } from "react-native";
-import { router } from "expo-router";
+import Header from "../components/Header";
+import { Screen } from "../components/Screen";
+import { useTranslation } from "react-i18next";
+import { Icon } from "../components/Icon";
+import { useNavigation } from "expo-router";
+import { YStack } from "tamagui";
+import { Typography } from "../components/Typography";
+import { useForm, Controller } from "react-hook-form";
+import FormInput from "../components/FormInputs/FormInput";
+import Card from "../components/Card";
+import Button from "../components/Button";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+
+type FormData = {
+  email: string;
+};
 
 const ForgotPassword = () => {
+  const navigation = useNavigation();
+
+  const { handleSubmit, control, formState } = useForm<FormData>({});
+
+  const { t } = useTranslation("login");
+  const { errors } = formState;
+
+  const insets = useSafeAreaInsets();
+
+  const onSubmit = (data: FormData) => {
+    console.log("Forgot password for email: ", data.email);
+  };
+
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text
-        onPress={() => {
-          router.back();
-        }}
-      >
-        Go Back
-      </Text>
-    </View>
+    <Screen
+      preset="auto"
+      ScrollViewProps={{
+        bounces: false,
+      }}
+      contentContainerStyle={{
+        flexGrow: 1,
+      }}
+    >
+      <Header
+        title={"More"}
+        titleColor="white"
+        barStyle="light-content"
+        leftIcon={<Icon icon="chevronLeft" color="white" />}
+        onLeftPress={() => navigation.goBack()}
+      />
+
+      <YStack paddingHorizontal="$md" gap="$md" paddingTop={10 + insets.top}>
+        <Typography preset="heading" fontWeight="700">
+          Forgot your password?{" "}
+        </Typography>
+
+        <Typography>
+          Please enter the email address associated with your account and we'll send you a link to
+          reset your password.
+        </Typography>
+
+        <Controller
+          key="email"
+          name="email"
+          control={control}
+          rules={{
+            required: {
+              value: true,
+              message: t("form.email.required"),
+            },
+          }}
+          render={({ field: { onChange, value } }) => (
+            <YStack>
+              <FormInput
+                type="text"
+                title={t("form.email.label")}
+                placeholder={t("form.email.placeholder")}
+                value={value}
+                onChangeText={onChange}
+              ></FormInput>
+
+              <Typography color="$red5" marginTop="$xs">
+                {errors?.email?.message?.toString() ?? ""}
+              </Typography>
+            </YStack>
+          )}
+        />
+      </YStack>
+
+      <Card width="100%" paddingBottom={16 + insets.bottom} marginTop="auto">
+        <Button onPress={handleSubmit(onSubmit)}>Log in</Button>
+      </Card>
+    </Screen>
   );
 };
 
