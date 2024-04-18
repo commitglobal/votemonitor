9361662efc6a3688f7f6180293add0f9d37a95aa
Ion Dormenco <idormenco@gmail.com>
2024-04-18 15:41:52 +0300

    Add extra claim in token

--- api/src/Vote.Monitor.Core/Security/ApplicationClaimTypes.cs
+++ api/src/Vote.Monitor.Core/Security/ApplicationClaimTypes.cs
@@ -4,5 +4,5 @@ public static class ApplicationClaimTypes
 {
     public const string UserId = "user-id";
     public const string NgoId = "ngoId";
-    public const string Role = "role";
+    public const string Role = "user-role";
 }
