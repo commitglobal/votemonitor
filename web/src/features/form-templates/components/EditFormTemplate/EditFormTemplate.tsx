85ea02d01231712f41b2f3efabfc5c6041a17fd1
Ion Dormenco <idormenco@gmail.com>
2024-04-18 15:39:00 +0300

    more styling

--- web/src/features/form-templates/components/EditFormTemplate/EditFormTemplate.tsx
+++ web/src/features/form-templates/components/EditFormTemplate/EditFormTemplate.tsx
@@ -1,6 +1,6 @@
 import { useLoaderData, useNavigate } from '@tanstack/react-router';
 import Layout from '@/components/layout/Layout';
-import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Separator } from '@/components/ui/separator';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
@@ -8,14 +8,13 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { authApi } from '@/common/auth-api';
 import { useMutation } from '@tanstack/react-query';
 import { useToast } from '@/components/ui/use-toast';
-import { FormTemplateFull, FormTemplateType } from '../../models/formTemplate';
+import { FormTemplateFull, FormTemplateType, mapFormTemplateType } from '../../models/formTemplate';
 import { Route as FormTemplatesListRoute } from '@/routes/form-templates/'
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+import { Form, FormControl, FormField, FormMessage } from '@/components/ui/form';
 import { useTranslation } from 'react-i18next';
 import LanguageSelect from '@/containers/LanguageSelect';
-import { Description, Field, FieldGroup, Fieldset, Label, Legend } from '@/components/ui/fieldset'
+import { ErrorMessage, Field, FieldGroup, Fieldset, Label } from '@/components/ui/fieldset'
 import { Input } from '@/components/ui/input';
-import { Text } from '@/components/ui/text';
 import { Textarea } from '@/components/ui/textarea';
 
 import {
@@ -25,9 +24,7 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-import { PencilIcon } from '@heroicons/react/24/solid';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
-import { Button } from '@/components/ui/button';
 import EditFormTemplateFooter from './EditFormTemplateFooter';
 
 
@@ -38,10 +35,10 @@ export default function EditFormTemplate() {
   const { toast } = useToast();
 
   const editFormTemplateFormSchema = z.object({
-    code: z.string(),
-    name: z.string(),
-    description: z.string(),
-    defaultLanguage: z.string(),
+    code: z.string().nonempty(),
+    name: z.string().nonempty(),
+    description: z.string().optional(),
+    defaultLanguage: z.string().nonempty(),
     formTemplateType: z.enum([FormTemplateType.Opening, FormTemplateType.Voting, FormTemplateType.ClosingAndCounting]).catch(FormTemplateType.Opening)
   });
 
@@ -58,25 +55,17 @@ export default function EditFormTemplate() {
 
   function onSubmit(values: z.infer<typeof editFormTemplateFormSchema>) {
     const updatedFormTemplate: FormTemplateFull = {
-      ...formTemplate
+      ...formTemplate,
+    
     };
 
     editMutation.mutate(updatedFormTemplate);
   }
 
-  const deleteMutation = useMutation({
-    mutationFn: (formTemplateId: string) => {
-      return authApi.delete<void>(`/form-templates/${formTemplateId}`);
-    },
-    onSuccess: () => {
-      navigate({ search: (prev) => (prev) });
-    },
-  });
-
   const editMutation = useMutation({
     mutationFn: (obj: FormTemplateFull) => {
 
-      return authApi.post<void>(
+      return authApi.put<void>(
         `/form-templates/${formTemplate.id}`,
         obj
       );
@@ -85,7 +74,7 @@ export default function EditFormTemplate() {
     onSuccess: () => {
       toast({
         title: 'Success',
-        description: 'Observer successfully updated',
+        description: 'Form template updated successfully updated',
       });
     },
   });
@@ -94,33 +83,34 @@ export default function EditFormTemplate() {
 
   return (
     <Layout title={`${formTemplate.code} - ${formTemplate.name[formTemplate.defaultLanguage]}`}>
-      <Tabs defaultValue='form-details'>
-        <TabsList className='grid grid-cols-2 bg-gray-200 w-[400px] mb-4'>
-          <TabsTrigger value='form-details'>Template form details</TabsTrigger>
-          <TabsTrigger value='questions'>Questions</TabsTrigger>
-        </TabsList>
-        <TabsContent value='form-details'>
-          <Card className='pt-0'>
-            <CardHeader className='flex flex-column gap-2'>
-              <div className='flex flex-row justify-between items-center'>
-                <CardTitle className='text-xl'>Template form details</CardTitle>
-
-              </div>
-              <Separator />
-            </CardHeader>
-            <CardContent className='flex flex-col gap-6 items-baseline'>
-              <Form {...form}>
-                <form onSubmit={form.handleSubmit(onSubmit)}>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <Tabs defaultValue='form-details'>
+            <TabsList className='grid grid-cols-2 bg-gray-200 w-[400px] mb-4'>
+              <TabsTrigger value='form-details'>Template form details</TabsTrigger>
+              <TabsTrigger value='questions'>Questions</TabsTrigger>
+            </TabsList>
+            <TabsContent value='form-details'>
+              <Card className='pt-0'>
+                <CardHeader className='flex flex-column gap-2'>
+                  <div className='flex flex-row justify-between items-center'>
+                    <CardTitle className='text-xl'>Template form details</CardTitle>
+                  </div>
+                  <Separator />
+                </CardHeader>
+                <CardContent className='flex flex-col gap-6 items-baseline'>
                   <Fieldset className='grid grid-cols-2 gap-12'>
                     <FieldGroup className='!mt-0'>
                       <FormField
                         control={form.control}
                         name='code'
-                        render={({ field }) => (
+                        render={({ field, fieldState }) => (
                           <Field>
                             <Label>{t('form-template.field.code')}</Label>
-                            <Input placeholder={t('form-template.placeholder.code')} {...field} />
-                          </Field>)} />
+                            <Input placeholder={t('form-template.placeholder.code')} {...field}  {...fieldState} />
+                            {fieldState.invalid && <ErrorMessage>{fieldState?.error?.message}</ErrorMessage>}
+                          </Field>
+                        )} />
 
                       <FormField
                         control={form.control}
@@ -135,9 +125,9 @@ export default function EditFormTemplate() {
                                 </SelectTrigger>
                               </FormControl>
                               <SelectContent>
-                                <SelectItem value="m@example.com">m@example.com</SelectItem>
-                                <SelectItem value="m@google.com">m@google.com</SelectItem>
-                                <SelectItem value="m@support.com">m@support.com</SelectItem>
+                                <SelectItem value={FormTemplateType.Opening}>{mapFormTemplateType(FormTemplateType.Opening)}</SelectItem>
+                                <SelectItem value={FormTemplateType.Voting}>{mapFormTemplateType(FormTemplateType.Voting)}</SelectItem>
+                                <SelectItem value={FormTemplateType.ClosingAndCounting}>{mapFormTemplateType(FormTemplateType.ClosingAndCounting)}</SelectItem>
                               </SelectContent>
                             </Select>
                             <FormMessage />
@@ -148,10 +138,11 @@ export default function EditFormTemplate() {
                       <FormField
                         control={form.control}
                         name='name'
-                        render={({ field }) => (
+                        render={({ field, fieldState }) => (
                           <Field>
                             <Label>{t('form-template.field.name')}</Label>
-                            <Input placeholder={t('form-template.placeholder.name')} {...field} />
+                            <Input placeholder={t('form-template.placeholder.name')} {...field}  {...fieldState} />
+                            {fieldState.invalid && <ErrorMessage>{fieldState?.error?.message}</ErrorMessage>}
                           </Field>)} />
 
                       <FormField
@@ -181,27 +172,27 @@ export default function EditFormTemplate() {
                       />
                     </FieldGroup>
                   </Fieldset>
-                </form>
-              </Form>
-            </CardContent>
-            <CardFooter className='flex justify-between'></CardFooter>
-          </Card>
-        </TabsContent>
-        <TabsContent value='questions'>
-          <Card className='pt-0'>
-            <CardHeader className='flex flex-column gap-2'>
-              <div className='flex flex-row justify-between items-center'>
-                <CardTitle className='text-xl'>Template form questions</CardTitle>
-              </div>
-              <Separator />
-            </CardHeader>
-            <CardContent className='-mx-6 flex items-start justify-left px-6 sm:mx-0 sm:px-8'>
-              editorr goes here
-            </CardContent>
-          </Card>
-        </TabsContent>
-      </Tabs>
-      <EditFormTemplateFooter/>
+                </CardContent>
+              </Card>
+            </TabsContent>
+            <TabsContent value='questions'>
+              <Card className='pt-0'>
+                <CardHeader className='flex flex-column gap-2'>
+                  <div className='flex flex-row justify-between items-center'>
+                    <CardTitle className='text-xl'>Template form questions</CardTitle>
+                  </div>onSubmit
+
+                  <Separator />
+                </CardHeader>
+                <CardContent className='-mx-6 flex items-start justify-left px-6 sm:mx-0 sm:px-8'>
+                  editorr goes here
+                </CardContent>
+              </Card>
+            </TabsContent>
+          </Tabs>
+          <EditFormTemplateFooter />
+        </form>
+      </Form>
     </Layout>
   );
 }
--- web/src/features/form-templates/components/EditFormTemplate/EditFormTemplateFooter.tsx
+++ web/src/features/form-templates/components/EditFormTemplate/EditFormTemplateFooter.tsx
@@ -2,14 +2,13 @@ import { Button } from '@/components/ui/button'
 import React from 'react'
 
 export interface EditFormTemplateFooterProps {
-
 }
 
-function EditFormTemplateFooter(props: EditFormTemplateFooterProps) {
+function EditFormTemplateFooter({ }: EditFormTemplateFooterProps) {
   return (
     <footer className="fixed left-0 bottom-0 h-[100px] w-full bg-white">
       <div className='flex justify-end items-center h-full container'>
-        <Button type='button' variant='default'>Save</Button>
+        <Button type='submit' variant='default'>Save</Button>
       </div>
     </footer>
   )
