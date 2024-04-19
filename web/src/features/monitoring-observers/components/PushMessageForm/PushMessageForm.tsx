2a7a0a03bf729cd43cdadd64b115cab59c0e3b3b
Ion Dormenco <idormenco@gmail.com>
2024-04-19 17:33:10 +0300

    Rework notifications (#341)

--- web/src/features/monitoring-observers/components/PushMessageForm/PushMessageForm.tsx
+++ web/src/features/monitoring-observers/components/PushMessageForm/PushMessageForm.tsx
@@ -8,6 +8,11 @@ import { Textarea } from '@/components/ui/textarea';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
+import { authApi } from '@/common/auth-api';
+import { useQuery } from '@tanstack/react-query';
+import { LevelNode } from '@/common/types';
+import { useEffect, useState } from 'react';
+import { useTags } from '../../queries';
 
 function PushMessageForm() {
   const createPushMessageSchema = z.object({
@@ -16,16 +21,113 @@ function PushMessageForm() {
       .string()
       .min(1, { message: 'Your message must have a detailed description before sending.' })
       .max(1000),
-    recipients: z.string(),
-    location: z.string(),
-    location1: z.string(),
-    location2: z.string(),
-    location3: z.string(),
+    recipientsSelection: z.enum(['subgroup', 'individual']).catch('subgroup'),
+    subgroupFilter: z.object({
+      filterObserversBy: z.enum(['tags', 'location']).catch('location'),
+      tagsFilter: z.object({
+        tags: z.array(z.string()).optional(),
+      }),
+      locationFilter: z.object({
+        location1: z.string().optional(),
+        location2: z.string().optional(),
+        location3: z.string().optional(),
+        location4: z.string().optional(),
+        location5: z.string().optional()
+      }).optional()
+
+    }),
+    individualFilter: z.object({
+      monitoringObserverIds: z.array(z.string()).optional()
+    })
   });
 
   const form = useForm<z.infer<typeof createPushMessageSchema>>({
     resolver: zodResolver(createPushMessageSchema),
   });
+
+  const watchRecipientSelection = form.watch('recipientsSelection');
+  const watchFilterObserversBy = form.watch('subgroupFilter.filterObserversBy');
+
+  useEffect(() => {
+    if (watchRecipientSelection === 'individual') {
+      form.register('individualFilter');
+      form.unregister('subgroupFilter');
+    } else {
+      form.unregister('individualFilter');
+      form.register('subgroupFilter');
+    }
+  }, [form.register, form.unregister, watchRecipientSelection]);
+
+  useEffect(() => {
+    if (watchFilterObserversBy === 'tags') {
+      form.register('subgroupFilter.tagsFilter');
+      form.unregister('subgroupFilter.locationFilter');
+    } else {
+      form.unregister('subgroupFilter.tagsFilter');
+      form.register('subgroupFilter.locationFilter');
+    }
+  }, [form.register, form.unregister, watchFilterObserversBy]);
+
+
+  const { data: nodes } = useQuery({
+    queryKey: ['levels'],
+    queryFn: async () => {
+      const electionRoundId: string | null = localStorage.getItem('electionRoundId');
+
+      const response = await authApi.get<{ nodes: LevelNode[] }>(
+        `/election-rounds/${electionRoundId}/polling-stations:fetchLevels`
+      );
+
+      if (response.status !== 200) {
+        throw new Error('Failed to fetch levels');
+      }
+
+      return response.data.nodes;
+    }
+  });
+
+  const { data:tags } = useTags();
+
+
+  const [level1Options, setLevel1Options] = useState<LevelNode[]>([]);
+  const [level2Options, setLevel2Options] = useState<LevelNode[]>([]);
+  const [level3Options, setLevel3Options] = useState<LevelNode[]>([]);
+  const [level4Options, setLevel4Options] = useState<LevelNode[]>([]);
+  const [level5Options, setLevel5Options] = useState<LevelNode[]>([]);
+
+  const [level1Map, setLevel1Map] = useState<{[name:string]: number}>({});
+  const [level2Map, setLevel2Map] = useState<{[name:string]: number}>({});
+  const [level3Map, setLevel3Map] = useState<{[name:string]: number}>({});
+  const [level4Map, setLevel4Map] = useState<{[name:string]: number}>({});
+
+  useEffect(() => {
+    const level1Nodes = nodes?.filter(x => x.depth === 1) ?? [];
+    const level2Nodes = nodes?.filter(x => x.depth === 2) ?? [];
+    const level3Nodes = nodes?.filter(x => x.depth === 3) ?? [];
+    const level4Nodes = nodes?.filter(x => x.depth === 4) ?? [];
+
+    setLevel1Map(level1Nodes.reduce((acc: { [name: string]: number }, node) => {
+      acc[node.name] = node.id;
+      return acc;
+    }, {}));
+
+    setLevel2Map(level2Nodes.reduce((acc: { [name: string]: number }, node) => {
+      acc[node.name] = node.id;
+      return acc;
+    }, {}));
+
+    setLevel3Map(level3Nodes.reduce((acc: { [name: string]: number }, node) => {
+      acc[node.name] = node.id;
+      return acc;
+    }, {}));
+
+    setLevel4Map(level4Nodes.reduce((acc: { [name: string]: number }, node) => {
+      acc[node.name] = node.id;
+      return acc;
+    }, {}));
+
+    setLevel1Options(level1Nodes);
+  }, [nodes]);
 
   function onSubmit(values: z.infer<typeof createPushMessageSchema>) {
     console.log(values);
@@ -75,7 +177,7 @@ function PushMessageForm() {
 
               <FormField
                 control={form.control}
-                name='recipients'
+                name='recipientsSelection'
                 render={({ field }) => (
                   <FormItem className='w-[540px]'>
                     <FormLabel>
@@ -89,6 +191,7 @@ function PushMessageForm() {
                       </FormControl>
                       <SelectContent>
                         <SelectItem value='subgroup'>A subgroup of observers</SelectItem>
+                        <SelectItem value='individual'>Individual observers</SelectItem>
                       </SelectContent>
                     </Select>
 
@@ -97,97 +200,173 @@ function PushMessageForm() {
                 )}
               />
 
-              <FormField
-                control={form.control}
-                name='location'
-                render={({ field }) => (
-                  <FormItem className='w-[540px]'>
-                    <FormLabel>
-                      Filter recipients by <span className='text-red-500'>*</span>
-                    </FormLabel>
-                    <Select onValueChange={field.onChange} defaultValue={field.value}>
-                      <FormControl>
-                        <SelectTrigger>
-                          <SelectValue placeholder='Location' />
-                        </SelectTrigger>
-                      </FormControl>
-                      <SelectContent>
-                        <SelectItem value='subgroup'>Location</SelectItem>
-                      </SelectContent>
-                    </Select>
 
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
+              {watchRecipientSelection === 'individual' ?
+                <>
+                </> : <>
+                  <FormField
+                    control={form.control}
+                    name='subgroupFilter.filterObserversBy'
+                    render={({ field }) => (
+                      <FormItem className='w-[540px]'>
+                        <FormLabel>
+                          Filter recipients by <span className='text-red-500'>*</span>
+                        </FormLabel>
+                        <Select onValueChange={field.onChange} defaultValue={field.value}>
+                          <FormControl>
+                            <SelectTrigger>
+                              <SelectValue placeholder='Filter observers by' />
+                            </SelectTrigger>
+                          </FormControl>
+                          <SelectContent>
+                            <SelectItem value='tags'>Tags</SelectItem>
+                            <SelectItem value='location'>Location</SelectItem>
+                          </SelectContent>
+                        </Select>
 
-              <FormField
-                control={form.control}
-                name='location1'
-                render={({ field }) => (
-                  <FormItem className='w-[540px]'>
-                    <FormLabel>
-                      Location (Level 1) <span className='text-red-500'>*</span>
-                    </FormLabel>
-                    <Select onValueChange={field.onChange} defaultValue={field.value}>
-                      <FormControl>
-                        <SelectTrigger>
-                          <SelectValue placeholder='Location' />
-                        </SelectTrigger>
-                      </FormControl>
-                      <SelectContent>
-                        <SelectItem value='subgroup'>Location</SelectItem>
-                      </SelectContent>
-                    </Select>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  {watchFilterObserversBy === 'tags' ? <>
+                    <div>TBD</div>
+                  </> :
+                    <>
+                      {level1Options.length > 0 && <FormField
+                        control={form.control}
+                        name='subgroupFilter.locationFilter.location1'
+                        render={({ field }) => (
+                          <FormItem className='w-[540px]'>
+                            <FormLabel>
+                              Location (Level 1) <span className='text-red-500'>*</span>
+                            </FormLabel>
+                            <Select onValueChange={(value) => {
+                              field.onChange(value);
+                              setLevel2Options(nodes?.filter(n => n.parentId === level1Map[value]) ?? []);
+                              setLevel3Options([]);
+                              setLevel4Options([]);
+                              setLevel5Options([]);
+                            }} defaultValue={field.value}>
+                              <FormControl>
+                                <SelectTrigger>
+                                  <SelectValue placeholder='Location' />
+                                </SelectTrigger>
+                              </FormControl>
+                              <SelectContent>
+                                {level1Options.map(level => <SelectItem value={level.name}>{level.name}</SelectItem>)}
+                              </SelectContent>
+                            </Select>
 
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                      />}
 
-              <FormField
-                control={form.control}
-                name='location2'
-                render={({ field }) => (
-                  <FormItem className='w-[540px]'>
-                    <FormLabel>Location (Level 2)</FormLabel>
-                    <Select onValueChange={field.onChange} defaultValue={field.value}>
-                      <FormControl>
-                        <SelectTrigger>
-                          <SelectValue placeholder='Location' />
-                        </SelectTrigger>
-                      </FormControl>
-                      <SelectContent>
-                        <SelectItem value='subgroup'>Location</SelectItem>
-                      </SelectContent>
-                    </Select>
+                      {level2Options.length > 0 && <FormField
+                        control={form.control}
+                        name='subgroupFilter.locationFilter.location2'
+                        render={({ field }) => (
+                          <FormItem className='w-[540px]'>
+                            <FormLabel>Location (Level 2)</FormLabel>
+                            <Select onValueChange={(value) => {
+                              field.onChange(value);
+                              setLevel3Options(nodes?.filter(n => n.parentId === level2Map[value]) ?? []);
+                              setLevel4Options([]);
+                              setLevel5Options([]);
+                            }} defaultValue={field.value}>
+                              <FormControl>
+                                <SelectTrigger>
+                                  <SelectValue placeholder='Location' />
+                                </SelectTrigger>
+                              </FormControl>
+                              <SelectContent>
+                                {level2Options.map(level => <SelectItem value={level.name}>{level.name}</SelectItem>)}
+                              </SelectContent>
+                            </Select>
 
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                      />}
 
-              <FormField
-                control={form.control}
-                name='location3'
-                render={({ field }) => (
-                  <FormItem className='w-[540px]'>
-                    <FormLabel>Location (Level 3)</FormLabel>
-                    <Select onValueChange={field.onChange} defaultValue={field.value}>
-                      <FormControl>
-                        <SelectTrigger>
-                          <SelectValue placeholder='Location' />
-                        </SelectTrigger>
-                      </FormControl>
-                      <SelectContent>
-                        <SelectItem value='subgroup'>Location</SelectItem>
-                      </SelectContent>
-                    </Select>
+                      {level3Options.length > 0 && <FormField
+                        control={form.control}
+                        name='subgroupFilter.locationFilter.location3'
+                        render={({ field }) => (
+                          <FormItem className='w-[540px]'>
+                            <FormLabel>Location (Level 3)</FormLabel>
+                            <Select onValueChange={(value) => {
+                              field.onChange(value);
+                              setLevel4Options(nodes?.filter(n => n.parentId === level3Map[value]) ?? []);
+                              setLevel5Options([]);
+                            }} defaultValue={field.value}>
+                              <FormControl>
+                                <SelectTrigger>
+                                  <SelectValue placeholder='Location' />
+                                </SelectTrigger>
+                              </FormControl>
+                              <SelectContent>
+                                {level3Options.map(level => <SelectItem value={level.name}>{level.name}</SelectItem>)}
+                              </SelectContent>
+                            </Select>
 
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                      />}
+
+                      {level4Options.length > 0 && <FormField
+                        control={form.control}
+                        name='subgroupFilter.locationFilter.location4'
+                        render={({ field }) => (
+                          <FormItem className='w-[540px]'>
+                            <FormLabel>Location (Level 4)</FormLabel>
+                            <Select onValueChange={(value) => {
+                              field.onChange(value);
+                              setLevel5Options(nodes?.filter(n => n.parentId === level4Map[value]) ?? []);
+                            }} defaultValue={field.value}>
+                              <FormControl>
+                                <SelectTrigger>
+                                  <SelectValue placeholder='Location' />
+                                </SelectTrigger>
+                              </FormControl>
+                              <SelectContent>
+                                {level4Options.map(level => <SelectItem value={level.name}>{level.name}</SelectItem>)}
+                              </SelectContent>
+                            </Select>
+
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                      />}
+
+                      {level5Options.length > 0 && <FormField
+                        control={form.control}
+                        name='subgroupFilter.locationFilter.location5'
+                        render={({ field }) => (
+                          <FormItem className='w-[540px]'>
+                            <FormLabel>Location (Level 5)</FormLabel>
+                            <Select onValueChange={field.onChange} defaultValue={field.value}>
+                              <FormControl>
+                                <SelectTrigger>
+                                  <SelectValue placeholder='Location' />
+                                </SelectTrigger>
+                              </FormControl>
+                              <SelectContent>
+                                {level5Options.map(level => (<SelectItem value={level.name}>{level.name}</SelectItem>))}
+                              </SelectContent>
+                            </Select>
+
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                      />}
+                    </>
+                  }
+                </>
+              }
+
+
               <div className='fixed bottom-0 left-0 bg-white py-4 px-12 flex justify-end w-screen'>
                 <Button>Send message to 152 observers</Button>
               </div>
