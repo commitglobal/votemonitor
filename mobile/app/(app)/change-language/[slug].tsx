453736c8e433a035461af13810e82aa30d2748d0
MadalinaZanficu <zanficumadalina@gmail.com>
2024-04-22 15:14:58 +0300

    Integrated Screen and Header components in change-language.

--- mobile/app/(app)/change-language/[slug].tsx
+++ mobile/app/(app)/change-language/[slug].tsx
@@ -1,10 +1,16 @@
 import React, { useContext } from "react";
-import { View } from "react-native";
 import { LanguageContext } from "../../../contexts/language/LanguageContext.provider";
 import Button from "../../../components/Button";
+import { Screen } from "../../../components/Screen";
+import Header from "../../../components/Header";
+import { Icon } from "../../../components/Icon";
+import { router } from "expo-router";
+import { YStack } from "tamagui";
+import { useTranslation } from "react-i18next";
 
 const ChangeLanguage = () => {
   const { changeLanguage } = useContext(LanguageContext);
+  const { t } = useTranslation("more");
 
   const switchToEnglish = () => {
     changeLanguage("en");
@@ -15,17 +21,35 @@ const ChangeLanguage = () => {
   };
 
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        gap: 20,
+    <Screen
+      preset="auto"
+      backgroundColor="white"
+      ScrollViewProps={{
+        stickyHeaderIndices: [0],
+        bounces: false,
+        showsVerticalScrollIndicator: false,
+      }}
+      contentContainerStyle={{
+        flexGrow: 1,
       }}
     >
-      <Button onPress={switchToEnglish}>English</Button>
-      <Button onPress={switchToRomanian}>Romanian</Button>
-    </View>
+      <Header
+        title={t("change-language")}
+        titleColor="white"
+        barStyle="light-content"
+        leftIcon={<Icon icon="chevronLeft" color="white" />}
+        onLeftPress={() => router.back()}
+        rightIcon={<Icon icon="dotsVertical" color="white" />}
+        onRightPress={() => {
+          console.log("Right icon pressed");
+        }}
+      />
+
+      <YStack flex={1} alignItems="center" justifyContent="center" gap={10}>
+        <Button onPress={switchToEnglish}>English</Button>
+        <Button onPress={switchToRomanian}>Romanian</Button>
+      </YStack>
+    </Screen>
   );
 };
 
