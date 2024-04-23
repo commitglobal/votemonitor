09f006054ecefe8ffc73635f959d3556def472b4
MadalinaZanficu <zanficumadalina@gmail.com>
2024-04-23 14:55:43 +0300

    Added react hook form.

--- mobile/app/(app)/change-password/index.tsx
+++ mobile/app/(app)/change-password/index.tsx
@@ -3,11 +3,31 @@ import { Screen } from "../../../components/Screen";
 import Header from "../../../components/Header";
 import { Icon } from "../../../components/Icon";
 import { router } from "expo-router";
+import { YStack } from "tamagui";
+import Card from "../../../components/Card";
+import Button from "../../../components/Button";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { Control, Controller, FieldErrors, FieldValues, useForm } from "react-hook-form";
+import React from "react";
+import { Typography } from "../../../components/Typography";
+import FormInput from "../../../components/FormInputs/FormInput";
+
+interface FormData {
+  currentPassword: string;
+  newPassword: string;
+  confirmPassword: string;
+}
 
 const ChangePassowrd = () => {
+  const insets = useSafeAreaInsets();
+
+  const { handleSubmit, control, formState } = useForm<FormData>({});
+  const { errors } = formState;
+
   return (
     <Screen
       preset="auto"
+      backgroundColor="white"
       ScrollViewProps={{
         bounces: false,
       }}
@@ -20,7 +40,94 @@ const ChangePassowrd = () => {
         leftIcon={<Icon icon="chevronLeft" color="white" />}
         onLeftPress={() => router.back()}
       />
+
+      <Form control={control} errors={errors}></Form>
+
+      <Card width="100%" paddingBottom={16 + insets.bottom} marginTop="auto">
+        <Button onPress={() => console.log("Save new password")}>Save new pass</Button>
+      </Card>
     </Screen>
+  );
+};
+
+const Form = ({
+  control,
+  errors,
+}: {
+  control: Control<FormData, any>;
+  errors: FieldErrors<FieldValues>;
+}) => {
+  const [secureTextEntry, setSecureTextEntry] = React.useState(false);
+  const passIcon = secureTextEntry === false ? "eye" : "eyeOff";
+
+  return (
+    <YStack gap={32} paddingHorizontal={16} paddingVertical={32}>
+      <Controller
+        key="currentPassword"
+        name="currentPassword"
+        control={control}
+        render={({ field: { onChange, value } }) => (
+          <YStack>
+            <FormInput
+              type="password"
+              secureTextEntry={secureTextEntry}
+              title={"Password"}
+              placeholder={"Password"}
+              value={value}
+              onChangeText={onChange}
+              iconRight={<Icon icon={passIcon} size={20} color="$gray11" />}
+              onIconRightPress={() => {
+                setSecureTextEntry(!secureTextEntry);
+              }}
+            ></FormInput>
+          </YStack>
+        )}
+      />
+
+      <Controller
+        key="newPassword"
+        name="newPassword"
+        control={control}
+        render={({ field: { onChange, value } }) => (
+          <YStack>
+            <FormInput
+              type="password"
+              secureTextEntry={secureTextEntry}
+              title={"Password"}
+              placeholder={"Password"}
+              value={value}
+              onChangeText={onChange}
+              iconRight={<Icon icon={passIcon} size={20} color="$gray11" />}
+              onIconRightPress={() => {
+                setSecureTextEntry(!secureTextEntry);
+              }}
+            ></FormInput>
+          </YStack>
+        )}
+      />
+
+      <Controller
+        key="confirmPassword"
+        name="confirmPassword"
+        control={control}
+        render={({ field: { onChange, value } }) => (
+          <YStack>
+            <FormInput
+              type="password"
+              secureTextEntry={secureTextEntry}
+              title={"Password"}
+              placeholder={"Password"}
+              value={value}
+              onChangeText={onChange}
+              iconRight={<Icon icon={passIcon} size={20} color="$gray11" />}
+              onIconRightPress={() => {
+                setSecureTextEntry(!secureTextEntry);
+              }}
+            ></FormInput>
+          </YStack>
+        )}
+      />
+    </YStack>
   );
 };
 
