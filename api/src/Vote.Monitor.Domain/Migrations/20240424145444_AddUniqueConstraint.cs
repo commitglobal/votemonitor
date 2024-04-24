b869b63f21b128e0e5ec07653921e55a898c6be4
Ion Dormenco <idormenco@gmail.com>
2024-04-24 18:01:56 +0300

    Add unique constraint (#356)

---
+++ api/src/Vote.Monitor.Domain/Migrations/20240424145444_AddUniqueConstraint.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Vote.Monitor.Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUniqueConstraint : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_PollingStationInformation_ElectionRoundId",
+                table: "PollingStationInformation");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PollingStationInformation_ElectionRoundId_PollingStationId_~",
+                table: "PollingStationInformation",
+                columns: new[] { "ElectionRoundId", "PollingStationId", "MonitoringObserverId", "PollingStationInformationFormId" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_PollingStationInformation_ElectionRoundId_PollingStationId_~",
+                table: "PollingStationInformation");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PollingStationInformation_ElectionRoundId",
+                table: "PollingStationInformation",
+                column: "ElectionRoundId");
+        }
+    }
+}
