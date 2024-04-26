a63e3509a1a42aa7f5be34f1e7f1f60b93457c09
luciatugui <lucia.tugui@wearetribus.com>
2024-04-26 12:35:53 +0300

    feat: edit question note

--- mobile/services/mutations/edit-note.mutation.ts
+++ mobile/services/mutations/edit-note.mutation.ts
@@ -1,27 +1,28 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { updateNote, UpdateNotePayload } from "../definitions.api";
 import { useMemo } from "react";
 import { pollingStationsKeys } from "../queries.service";
-import { addNote, NotePayload } from "../definitions.api";
 
-export const useAddNoteMutation = (
-  electionRoundId: string | undefined,
-  pollingStationId: string | undefined,
-  formId: string | undefined,
+export const useUpdateNote = (
+  electionRoundId: string,
+  pollingStationId: string,
+  formId: string,
+  id: string,
 ) => {
   const queryClient = useQueryClient();
 
-  // this is the GET notes key - we need it in order to invalidate that query after adding the new note
+  // this is the GET notes key - we need it in order to invalidate that query after updating the note
   const getNotesQK = useMemo(
     () => pollingStationsKeys.notes(electionRoundId, pollingStationId, formId),
     [electionRoundId],
   );
 
   return useMutation({
-    mutationKey: ["addNote"],
-    mutationFn: async (payload: NotePayload) => {
-      return addNote(payload);
+    mutationKey: [`updateNote_${id}`],
+    mutationFn: async (payload: UpdateNotePayload) => {
+      return updateNote(payload);
     },
-    onMutate: async (_payload: NotePayload) => {
+    onMutate: async (_payload: UpdateNotePayload) => {
       await queryClient.cancelQueries({ queryKey: getNotesQK });
       // TODO: optimistic updates
     },
