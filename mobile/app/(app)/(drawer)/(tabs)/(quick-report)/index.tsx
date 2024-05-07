ad7c8541245cbcfb9c9d004923180c468676312f
MadalinaZanficu <zanficumadalina@gmail.com>
2024-05-07 15:23:14 +0300

    UI chnages

--- mobile/app/(app)/(drawer)/(tabs)/(quick-report)/index.tsx
+++ mobile/app/(app)/(drawer)/(tabs)/(quick-report)/index.tsx
@@ -10,17 +10,82 @@ import Button from "../../../../../components/Button";
 import OptionsSheet from "../../../../../components/OptionsSheet";
 import { useQuickReports } from "../../../../../services/queries/quick-reports.query";
 import { useUserData } from "../../../../../contexts/user/UserContext.provider";
-import { QuickReportsAPIResponse } from "../../../../../services/definitions.api";
 import ReportCard from "../../../../../components/ReportCard";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 const QuickReport = () => {
   const navigation = useNavigation();
   const [openContextualMenu, setOpenContextualMenu] = useState(false);
   const { activeElectionRound } = useUserData();
-
   const { data: quickReports } = useQuickReports(activeElectionRound?.id);
-
   console.log(quickReports);
+  const insets = useSafeAreaInsets();
+
+  if (quickReports === undefined || (quickReports && quickReports.length === 0)) {
+    return <NoQuickReportsExist />;
+  }
+
+  return (
+    <Screen
+      preset="scroll"
+      ScrollViewProps={{
+        showsVerticalScrollIndicator: false,
+        stickyHeaderIndices: [0],
+        bounces: false,
+      }}
+      contentContainerStyle={{
+        flexGrow: 1,
+      }}
+      backgroundColor="white"
+    >
+      <Header
+        title={"Quick Report"}
+        titleColor="white"
+        barStyle="light-content"
+        leftIcon={<Icon icon="menuAlt2" color="white" />}
+        onLeftPress={() => navigation.dispatch(DrawerActions.openDrawer)}
+        rightIcon={<Icon icon="dotsVertical" color="white" />}
+        onRightPress={() => {
+          setOpenContextualMenu(true);
+        }}
+      />
+
+      <YStack paddingHorizontal={16} gap={8} justifyContent="center" paddingVertical={32}>
+        <Typography preset="body1" textAlign="left" color="$gray7" fontWeight="700">
+          {" "}
+          My reported issues{" "}
+        </Typography>
+
+        {quickReports.map((report) => (
+          <ReportCard
+            key={report.id}
+            title={report.title}
+            description={report.description}
+            onPress={() => router.push(`/report-details/${report.id}?reportTitle=${report.title}`)}
+          />
+        ))}
+      </YStack>
+
+      <View width="100%" paddingBottom={8 + insets.bottom} marginTop="auto" paddingHorizontal={32}>
+        <Button preset="outlined" onPress={router.push.bind(null, "/report-issue")}>
+          Report new issue
+        </Button>
+      </View>
+
+      <OptionsSheet open={openContextualMenu} setOpen={setOpenContextualMenu}>
+        {/* //TODO: what do we need to add here? */}
+        <OptionsSheetContent />
+      </OptionsSheet>
+    </Screen>
+  );
+};
+
+/**
+ * This component is displayed when there are no quick reports available.
+ */
+const NoQuickReportsExist = () => {
+  const navigation = useNavigation();
+  const [openContextualMenu, setOpenContextualMenu] = useState(false);
 
   return (
     <Screen
@@ -44,61 +109,28 @@ const QuickReport = () => {
         }}
       />
 
-      {quickReports === null && <NoQuickReportsExist />}
-      {quickReports && quickReports.length === 0 && <NoQuickReportsExist />}
-      {quickReports && quickReports.length > 0 && <QuickReportsList quickReports={quickReports} />}
+      <YStack flex={1} alignItems="center" justifyContent="center" gap="$md">
+        <Icon icon="undrawFlag" />
 
+        <YStack gap="$md" paddingHorizontal="$xl">
+          <Typography preset="body1" textAlign="center" color="$gray12" lineHeight={24}>
+            Start sending quick reports to the organization if you notice irregularities inside,
+            outside the polling station or whenever needed.
+          </Typography>
+          <Button
+            preset="outlined"
+            onPress={router.push.bind(null, "/report-issue")}
+            backgroundColor="white"
+          >
+            Report new issue
+          </Button>
+        </YStack>
+      </YStack>
       <OptionsSheet open={openContextualMenu} setOpen={setOpenContextualMenu}>
         {/* //TODO: what do we need to add here? */}
         <OptionsSheetContent />
       </OptionsSheet>
     </Screen>
-  );
-};
-
-interface listProps {
-  quickReports: Array<QuickReportsAPIResponse>;
-}
-
-const QuickReportsList = (props: listProps) => {
-  const { quickReports } = props;
-
-  return (
-    <YStack flex={1} alignItems="center" justifyContent="center" gap="$xs">
-      {quickReports.map((report) => (
-        <ReportCard
-          key={report.id}
-          title={report.title}
-          description={report.description}
-          onPress={() => router.push(`/report-details/${report.id}?reportTitle=${report.title}`)}
-        />
-      ))}
-    </YStack>
-  );
-};
-
-/**
- * This component is displayed when there are no quick reports available.
- */
-const NoQuickReportsExist = () => {
-  return (
-    <YStack flex={1} alignItems="center" justifyContent="center" gap="$md">
-      <Icon icon="undrawFlag" />
-
-      <YStack gap="$md" paddingHorizontal="$xl">
-        <Typography preset="body1" textAlign="center" color="$gray12" lineHeight={24}>
-          Start sending quick reports to the organization if you notice irregularities inside,
-          outside the polling station or whenever needed.
-        </Typography>
-        <Button
-          preset="outlined"
-          onPress={router.push.bind(null, "/report-issue")}
-          backgroundColor="white"
-        >
-          Report new issue
-        </Button>
-      </YStack>
-    </YStack>
   );
 };
 
