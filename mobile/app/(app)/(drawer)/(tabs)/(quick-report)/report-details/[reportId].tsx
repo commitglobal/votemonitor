cc39100b3f3b0e21b799ac31fbd5be582a6d348e
MadalinaZanficu <zanficumadalina@gmail.com>
2024-05-08 12:59:51 +0300

    Added attachments list.

--- mobile/app/(app)/(drawer)/(tabs)/(quick-report)/report-details/[reportId].tsx
+++ mobile/app/(app)/(drawer)/(tabs)/(quick-report)/report-details/[reportId].tsx
@@ -6,11 +6,34 @@ import { Typography } from "../../../../../../components/Typography";
 import { YStack } from "tamagui";
 import { useQuickReportById } from "../../../../../../services/queries/quick-reports.query";
 import { useUserData } from "../../../../../../contexts/user/UserContext.provider";
+import { QuickReportAttachmentAPIResponse } from "../../../../../../services/definitions.api";
+import Card from "../../../../../../components/Card";
 
 type SearchParamsType = {
   reportId: string;
   reportTitle: string;
 };
+
+const dummyAttachments: QuickReportAttachmentAPIResponse[] = [
+  {
+    id: "1",
+    quickReportId: "1",
+    electionRoundId: "1",
+    fileName: "Attachment 1.jpg",
+    mimeType: "image/jpeg",
+    presignedUrl: "https://via.placeholder.com/150",
+    urlValidityInSeconds: 3600,
+  },
+  {
+    id: "2",
+    quickReportId: "1",
+    electionRoundId: "1",
+    fileName: "Attachment 1.jpg",
+    mimeType: "image/jpeg",
+    presignedUrl: "https://via.placeholder.com/150",
+    urlValidityInSeconds: 3600,
+  },
+];
 
 const ReportDetails = () => {
   const { reportTitle, reportId } = useLocalSearchParams<SearchParamsType>();
@@ -27,6 +50,8 @@ const ReportDetails = () => {
     isLoading: isLoadingCurrentReport,
     error: currentReportError,
   } = useQuickReportById(activeElectionRound?.id, reportId);
+
+  const attachments = dummyAttachments;
 
   if (isLoadingCurrentReport) {
     return <Typography>Loading</Typography>;
@@ -58,16 +83,31 @@ const ReportDetails = () => {
         leftIcon={<Icon icon="chevronLeft" color="white" />}
         onLeftPress={() => router.back()}
       />
-      <YStack gap={16} paddingHorizontal={16} paddingTop={32} justifyContent="center">
-        <Typography preset="subheading" fontWeight="500">
-          {quickReport?.title}
-        </Typography>
-        <Typography preset="body1" lineHeight={24} color="$gray8">
-          {quickReport?.description}
-        </Typography>
+      <YStack gap={32} paddingHorizontal={16} paddingTop={32} justifyContent="center">
+        <YStack gap={16}>
+          <Typography preset="subheading" fontWeight="500">
+            {quickReport?.title}
+          </Typography>
+          <Typography preset="body1" lineHeight={24} color="$gray8">
+            {quickReport?.description}
+          </Typography>
+        </YStack>
 
-        {quickReport?.attachments?.length == 0 && (
-          <Typography fontWeight="500"> No attached files </Typography>
+        {attachments.length === 0 ? (
+          <Typography fontWeight="500">No attached files</Typography>
+        ) : (
+          <YStack gap={16}>
+            <Typography fontWeight="500" color="$gray10">
+              Uploaded media
+            </Typography>
+            {attachments.map((attachment) => (
+              <Card>
+                <Typography preset="body1" fontWeight="700" key={attachment.id}>
+                  {attachment.fileName}
+                </Typography>
+              </Card>
+            ))}
+          </YStack>
         )}
       </YStack>
     </Screen>
