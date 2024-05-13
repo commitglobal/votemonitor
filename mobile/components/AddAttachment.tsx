a93393a401779b9a82471301b17babf7e309e405
MadalinaZanficu <zanficumadalina@gmail.com>
2024-05-13 19:25:55 +0300

    Fixed B125: changed label for AddAttachment. Part of B126: fixed pressable area for AddAttachment.

--- mobile/components/AddAttachment.tsx
+++ mobile/components/AddAttachment.tsx
@@ -2,13 +2,15 @@ import React from "react";
 import { XStack, XStackProps } from "tamagui";
 import { Icon } from "./Icon";
 import { Typography } from "./Typography";
+import { useTranslation } from "react-i18next";
 
 const AddAttachment = ({ ...rest }: XStackProps) => {
+  const { t } = useTranslation("question_page");
   return (
-    <XStack alignItems="center" pressStyle={{ opacity: 0.5 }} {...rest}>
+    <XStack pressStyle={{ opacity: 0.5 }} {...rest} alignSelf="flex-start">
       <Icon icon="attachment" />
       <Typography color="$purple5" marginLeft="$xs">
-        Add Note, Photo or Video
+        {t("actions.add_media")}
       </Typography>
     </XStack>
   );
