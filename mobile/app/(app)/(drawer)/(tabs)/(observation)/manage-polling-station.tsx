6665c8a18394f3fb5b0de419f283ee4ba45615cd
MadalinaZanficu <zanficumadalina@gmail.com>
2024-05-17 16:35:02 +0300

    Added trigger dialog to manage polling station.

--- mobile/app/(app)/(drawer)/(tabs)/(observation)/manage-polling-station.tsx
+++ mobile/app/(app)/(drawer)/(tabs)/(observation)/manage-polling-station.tsx
@@ -1,23 +1,18 @@
 import { Screen } from "../../../../../components/Screen";
 import Header from "../../../../../components/Header";
 import { Icon } from "../../../../../components/Icon";
-import { YStack, XStack, View } from "tamagui";
+import { YStack, XStack, View, AlertDialog } from "tamagui";
 import { Typography } from "../../../../../components/Typography";
 import { router } from "expo-router";
 import Card from "../../../../../components/Card";
 import { useUserData } from "../../../../../contexts/user/UserContext.provider";
 import { Dialog } from "../../../../../components/Dialog";
 import Button from "../../../../../components/Button";
-import { useState } from "react";
 import { PollingStationVisitVM } from "../../../../../common/models/polling-station.model";
 import { useTranslation } from "react-i18next";
 
 const ManagePollingStation = () => {
   const { t } = useTranslation("manage_polling_stations");
-
-  const [deleteDialogOpen, setDeleteDialogOpen] = useState(false);
-  const [selectedPollingStation, setSelectedPollingStation] = useState("");
-
   const { visits } = useUserData();
   if (visits === undefined || visits.length === 0) {
     return <Typography>No visits</Typography>;
@@ -43,46 +38,78 @@ const ManagePollingStation = () => {
 
       <YStack gap="$lg" paddingTop="$lg" paddingHorizontal="$md">
         {visits.map((visit) => (
-          <Card
-            key={visit.pollingStationId}
-            onPress={() => {
-              setDeleteDialogOpen(true);
-              setSelectedPollingStation(visit.number);
-            }}
-          >
-            <PollingStationCard visit={visit} />
-          </Card>
+          <PollingStationCard visit={visit} key={visit.pollingStationId} />
         ))}
       </YStack>
-
-      {/* TODO: API call for delete polling-station */}
-      {deleteDialogOpen && (
-        <DeletePollingStationDialog
-          pollingStationNumber={selectedPollingStation}
-          onCancel={() => setDeleteDialogOpen(false)}
-          onDelete={() => {
-            console.log("DELETED");
-            setDeleteDialogOpen(false);
-          }}
-        />
-      )}
     </Screen>
+  );
+};
+
+interface PollingStationCardProps {
+  visit: PollingStationVisitVM;
+}
+
+const PollingStationCard = (props: PollingStationCardProps) => {
+  const { t } = useTranslation("manage_polling_stations");
+  const { visit } = props;
+
+  return (
+    <Card pressStyle={{ opacity: 1 }}>
+      <YStack gap="$md">
+        <XStack justifyContent="space-between" alignItems="center">
+          <Typography preset="body1" fontWeight="700">
+            {t("station_card.title", { value: visit.number })}
+          </Typography>
+
+          <DeletePollingStationDialog
+            pollingStationNumber={visit.number}
+            onDelete={() => {
+              console.log("TODO: API call for delete polling station!");
+            }}
+          />
+        </XStack>
+
+        {visit.level1 !== "" && (
+          <Typography>
+            {t("station_card.level_1")} <Typography fontWeight="500">{visit.level1}</Typography>{" "}
+          </Typography>
+        )}
+
+        {visit.level2 !== "" && (
+          <Typography>
+            {t("station_card.level_2")} <Typography fontWeight="500"> {visit.level2} </Typography>{" "}
+          </Typography>
+        )}
+
+        {visit.level3 !== "" && (
+          <Typography>
+            {t("station_card.level_3")} <Typography fontWeight="500">{visit.level3}</Typography>
+          </Typography>
+        )}
+
+        <Typography>
+          {t("station_card.street")} <Typography fontWeight="500">{visit.address}</Typography>
+        </Typography>
+        <Typography>
+          {t("station_card.number")} <Typography fontWeight="500">{visit.number}</Typography>
+        </Typography>
+      </YStack>
+    </Card>
   );
 };
 
 interface DeletePollingStationDialogProps {
   pollingStationNumber: string;
-  onCancel: () => void;
   onDelete: () => void;
 }
 
 const DeletePollingStationDialog = (props: DeletePollingStationDialogProps) => {
   const { t } = useTranslation("modals");
-  const { onCancel, onDelete, pollingStationNumber } = props;
+  const { onDelete, pollingStationNumber } = props;
 
   return (
     <Dialog
-      open
+      trigger={<Icon icon="bin" color="white" />}
       header={
         <Typography preset="heading">
           {t("delete_station.title", { value: pollingStationNumber })}
@@ -100,61 +127,20 @@ const DeletePollingStationDialog = (props: DeletePollingStationDialogProps) => {
         </View>
       }
       footer={
-        <XStack gap="$md">
-          <Button preset="chromeless" textStyle={{ color: "black" }} onPress={onCancel}>
-            {t("delete_station.actions.cancel")}
-          </Button>
-          <Button preset="red" onPress={onDelete} flex={1}>
-            {t("delete_station.actions.delete")}
-          </Button>
+        <XStack gap="$sm" justifyContent="center">
+          <AlertDialog.Cancel asChild>
+            <Button preset="chromeless" textStyle={{ color: "black" }}>
+              {t("delete_station.actions.cancel")}
+            </Button>
+          </AlertDialog.Cancel>
+          <AlertDialog.Cancel asChild>
+            <Button preset="red" onPress={onDelete} flex={1}>
+              {t("delete_station.actions.delete")}
+            </Button>
+          </AlertDialog.Cancel>
         </XStack>
       }
     />
-  );
-};
-
-interface PollingStationCardProps {
-  visit: PollingStationVisitVM;
-}
-
-const PollingStationCard = (props: PollingStationCardProps) => {
-  const { t } = useTranslation("manage_polling_stations");
-  const { visit } = props;
-
-  return (
-    <YStack gap="$md">
-      <XStack justifyContent="space-between" alignItems="center">
-        <Typography preset="body1" fontWeight="700">
-          {t("station_card.title", { value: visit.number })}
-        </Typography>
-        <Icon icon="bin" color="white"></Icon>
-      </XStack>
-
-      {visit.level1 !== "" && (
-        <Typography>
-          {t("station_card.level_1")} <Typography fontWeight="500">{visit.level1}</Typography>{" "}
-        </Typography>
-      )}
-
-      {visit.level2 !== "" && (
-        <Typography>
-          {t("station_card.level_2")} <Typography fontWeight="500"> {visit.level2} </Typography>{" "}
-        </Typography>
-      )}
-
-      {visit.level3 !== "" && (
-        <Typography>
-          {t("station_card.level_3")} <Typography fontWeight="500">{visit.level3}</Typography>
-        </Typography>
-      )}
-
-      <Typography>
-        {t("station_card.street")} <Typography fontWeight="500">{visit.address}</Typography>
-      </Typography>
-      <Typography>
-        {t("station_card.number")} <Typography fontWeight="500">{visit.number}</Typography>
-      </Typography>
-    </YStack>
   );
 };
 
