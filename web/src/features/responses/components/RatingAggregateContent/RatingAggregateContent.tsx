39c61e0c575e99cc2172d75a337d40da61c82e41
Ion Dormenco <idormenco@gmail.com>
2024-05-22 16:01:57 +0300

    fix typos

--- web/src/features/responses/components/RatingAggregateContent/RatingAggregateContent.tsx
+++ web/src/features/responses/components/RatingAggregateContent/RatingAggregateContent.tsx
@@ -29,7 +29,7 @@ const RatingAggregateContent = forwardRef<ChartJSOrUndefined<'bar', number[]>, R
       <>
         <div className='flex flex-col gap-1'>
           <div><span className='font-bold mr-1'>Min:</span><span>{aggregate.min}</span></div>
-          <div><span className='font-bold mr-1'>Min:</span><span> {aggregate.max}</span></div>
+          <div><span className='font-bold mr-1'>Max:</span><span> {aggregate.max}</span></div>
           <div><span className='font-bold mr-1'>Average:</span><span>{round(aggregate.average, 2)}</span></div>
         </div>
 
