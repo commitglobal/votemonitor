c3e6db1d923ff5f56effa908048a05d9c7dcfcaf
MadalinaZanficu <zanficumadalina@gmail.com>
2024-05-23 10:51:52 +0300

    Added fix height for MenuItem inside more screen.

--- mobile/app/(app)/(drawer)/(tabs)/more.tsx
+++ mobile/app/(app)/(drawer)/(tabs)/more.tsx
@@ -27,7 +27,7 @@ interface MenuItemProps {
 }
 
 const MenuItem = ({ label, helper, icon, chevronRight, onClick }: MenuItemProps) => (
-  <Card onPress={onClick}>
+  <Card onPress={onClick} style={{ height: 64, justifyContent: "center" }}>
     <XStack alignItems="center" justifyContent="space-between" gap="$xxxs">
       <XStack alignItems="center" gap="$xxs" maxWidth="80%">
         <Icon size={24} icon={icon} color="black" />
