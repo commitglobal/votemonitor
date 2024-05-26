12a80ee95896eb2c343b4fd5a1aa8f73deeaad1c
Ion Dormenco <idormenco@gmail.com>
2024-05-26 16:49:36 +0300

    Update SerializerService.cs (#490)
    * Update SerializerService.cs
    
    * Update AuditTrailInterceptorTests.cs

--- api/src/Vote.Monitor.Core/Services/Serialization/SerializerService.cs
+++ api/src/Vote.Monitor.Core/Services/Serialization/SerializerService.cs
@@ -1,14 +1,28 @@
 ﻿using System.Text.Json;
+using Microsoft.Extensions.Logging;
 
 namespace Vote.Monitor.Core.Services.Serialization;
 
-public class SerializerService : ISerializerService
+public class SerializerService(ILogger<SerializerService> logger) : ISerializerService
 {
     private readonly JsonSerializerOptions _serializerOptions = new(JsonSerializerDefaults.Web)
     {
         WriteIndented = true,
-        MaxDepth = 3
+        ReferenceHandler = ReferenceHandler.IgnoreCycles,
     };
 
-    public string Serialize<T>(T obj) => JsonSerializer.Serialize(obj, _serializerOptions);
+    public string Serialize<T>(T obj)
+    {
+        try
+        {
+            var json = JsonSerializer.Serialize(obj, _serializerOptions);
+
+            return json;
+        }
+        catch (Exception e)
+        {
+            logger.LogError(e, "Error serializing the object");
+            throw;
+        }
+    }
 }
