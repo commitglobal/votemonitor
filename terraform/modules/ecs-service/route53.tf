ac61f70cb911f96ab8bc3d8f36f7d82ca01f42d0
Andrei Ioniță <hi@andrei.io>
2024-05-27 13:20:44 +0300

    lb dns records

--- terraform/modules/ecs-service/route53.tf
+++ terraform/modules/ecs-service/route53.tf
@@ -1,29 +1,29 @@
-# # A record
-# resource "aws_route53_record" "ipv4" {
-#   count = length(var.lb_hosts)
+# A record
+resource "aws_route53_record" "ipv4" {
+  count = length(var.lb_hosts)
 
-#   zone_id = var.lb_domain_zone_id
-#   name    = var.lb_hosts[count.index]
-#   type    = "A"
+  zone_id = var.lb_domain_zone_id
+  name    = var.lb_hosts[count.index]
+  type    = "A"
 
-#   alias {
-#     name                   = var.lb_dns_name
-#     zone_id                = var.lb_zone_id
-#     evaluate_target_health = true
-#   }
-# }
+  alias {
+    name                   = var.lb_dns_name
+    zone_id                = var.lb_zone_id
+    evaluate_target_health = true
+  }
+}
 
-# # AAAA record
-# resource "aws_route53_record" "ipv6" {
-#   count = length(var.lb_hosts)
+# AAAA record
+resource "aws_route53_record" "ipv6" {
+  count = length(var.lb_hosts)
 
-#   zone_id = var.lb_domain_zone_id
-#   name    = var.lb_hosts[count.index]
-#   type    = "AAAA"
+  zone_id = var.lb_domain_zone_id
+  name    = var.lb_hosts[count.index]
+  type    = "AAAA"
 
-#   alias {
-#     name                   = var.lb_dns_name
-#     zone_id                = var.lb_zone_id
-#     evaluate_target_health = true
-#   }
-# }
+  alias {
+    name                   = var.lb_dns_name
+    zone_id                = var.lb_zone_id
+    evaluate_target_health = true
+  }
+}
