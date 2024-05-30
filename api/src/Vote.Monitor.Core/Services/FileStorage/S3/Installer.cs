c73f55eda6aea6f8caf13e2f58b1172b6d451e0f
Andrei Ioniță <hi@andrei.io>
2024-05-30 21:54:29 +0300

    Revert "Log s3 credentials (#530)" (#532)
    This reverts commit 19e80b766ec67a345761cc15ce32f93f179c85f5.

--- api/src/Vote.Monitor.Core/Services/FileStorage/S3/Installer.cs
+++ api/src/Vote.Monitor.Core/Services/FileStorage/S3/Installer.cs
@@ -2,7 +2,6 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Vote.Monitor.Core.Services.FileStorage.Contracts;
-using Serilog;
 
 namespace Vote.Monitor.Core.Services.FileStorage.S3;
 
@@ -16,9 +15,6 @@ internal static class Installer
         string awsSecretKey = configuration.GetSection("AWSSecretKey").Value!;
 
         var region = Amazon.RegionEndpoint.GetBySystemName(awsRegion);
-
-        Log.Warning($"amazon config: {awsRegion} - {awsAccessKey} - {awsSecretKey}");
-        Console.WriteLine($"amazon config: {awsRegion} - {awsAccessKey} - {awsSecretKey}");
 
         services.AddSingleton<IAmazonS3>(new AmazonS3Client(awsAccessKey, awsSecretKey, region));
         services.AddSingleton<IFileStorageService, S3FileStorageService>();
