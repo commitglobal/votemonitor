8465d75956b83312ff8d01d135bfc89024faa19e
Lupu Gheorghe <46172059+gheorghelupu17@users.noreply.github.com>
2024-05-30 20:44:02 +0300

    infra: fix s3 access (#528)
    * add access
    
    * fix s3
    
    * separate statements
    
    ---------
    
    Co-authored-by: Andrei Ioniță <hi@andrei.io>

--- terraform/service_hangfire.tf
+++ terraform/service_hangfire.tf
@@ -151,6 +151,14 @@ module "ecs_hangfire" {
       name      = "Mailing__SES__AWSSecretKey"
       valueFrom = "${aws_secretsmanager_secret.ses_hotfix.arn}:secret_key::"
     },
+        {
+      name      = "FileStorage__S3__AWSSecretKey"
+      valueFrom = "${aws_secretsmanager_secret.ses_hotfix.arn}:access_key::"
+    },
+    {
+      name      = "FileStorage__S3__AWSAccessKey"
+      valueFrom = "${aws_secretsmanager_secret.ses_hotfix.arn}:secret_key::"
+    },
   ]
 
   allowed_secrets = [
