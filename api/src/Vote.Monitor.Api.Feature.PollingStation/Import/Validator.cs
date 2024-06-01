d3c2fe6ac55e42bd0b53a3cd425c31b286f4c254
Ion Dormenco <idormenco@gmail.com>
2024-06-01 11:05:32 +0300

    remove upload limits

--- api/src/Vote.Monitor.Api.Feature.PollingStation/Import/Validator.cs
+++ api/src/Vote.Monitor.Api.Feature.PollingStation/Import/Validator.cs
@@ -11,11 +11,6 @@ public class Validator : Validator<Request>
             .NotEmpty();
 
         RuleFor(x => x.File)
-            .FileSmallerThan(25 * 1024 * 1024)// 25 MB
-            .When(x => x.File != null)
-            .WithMessage("The selected file exceeds 25 MB limit.");
-
-        RuleFor(x => x.File)
             .Must(file => Path.GetExtension(file.FileName).ToLower() == ".csv")
             .When(x => x.File != null)
             .WithMessage("Only CSV files are accepted.");
