15c9a25c2109152b35703d7e16308d634ca20d43
Ion Dormenco <idormenco@gmail.com>
2024-06-03 15:03:47 +0300

    Bugfix/fix charts (#554)
    * Fix charts
    
    * Fix issueas
    
    * fix min password length req

---
+++ utils/SubmissionsFaker/Clients/MonitoringObserver/Models/PSISubmissionRequest.cs
@@ -0,0 +1,25 @@
+﻿namespace SubmissionsFaker.Clients.MonitoringObserver.Models;
+
+public class PSISubmissionRequest
+{
+
+    public DateTime ArrivalTime { get; set; }
+    public DateTime DepartureTime { get; set; }
+
+    public string ObserverToken { get; set; }
+    public string PollingStationId { get; set; }
+
+    public List<BaseAnswerRequest> Answers { get; set; } = [];
+
+
+    public override bool Equals(object obj)
+    {
+        PSISubmissionRequest other = (PSISubmissionRequest)obj;
+        return PollingStationId == other.PollingStationId;
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(PollingStationId);
+    }
+}
