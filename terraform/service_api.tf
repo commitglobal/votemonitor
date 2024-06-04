4b651a772a32dc910c9a5b7baed70a0d55c0711f
Andrei Ioniță <hi@andrei.io>
2024-06-04 23:32:37 +0100

    scale up

--- terraform/service_api.tf
+++ terraform/service_api.tf
@@ -7,8 +7,8 @@ module "ecs_api" {
 
   name         = "api-${var.env}"
   cluster_name = module.ecs_cluster.cluster_name
-  min_capacity = 2
-  max_capacity = 8
+  min_capacity = 3
+  max_capacity = 9
 
   image_repo = local.images.api.image
   image_tag  = local.images.api.tag
