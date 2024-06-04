61b27ed7dff53525cad13a1bf2bbc9ae048db027
luciatugui <lucia.tugui@wearetribus.com>
2024-06-04 10:38:56 +0300

    fix: change translation keys used for password confirmation screen

--- mobile/components/PasswordConfirmationScreen.tsx
+++ mobile/components/PasswordConfirmationScreen.tsx
@@ -38,7 +38,7 @@ const PasswordConfirmationScreen = ({ translationKey, icon }: PasswordConfirmati
       >
         <Icon icon={icon} size={126} marginBottom="$md" color="yellow" />
         <Typography preset="heading" fontWeight="700" color="$purple5">
-          {t("confirmation.title")}
+          {t("confirmation.heading")}
         </Typography>
         <Typography preset="body1" color="black" textAlign="center">
           {t("confirmation.paragraph")}
@@ -46,7 +46,7 @@ const PasswordConfirmationScreen = ({ translationKey, icon }: PasswordConfirmati
       </YStack>
 
       <Card width="100%" paddingBottom={16 + insets.bottom} marginTop="auto">
-        <Button onPress={() => router.back()}>{t("confirmation.back")}</Button>
+        <Button onPress={() => router.back()}>{t("confirmation.confirm")}</Button>
       </Card>
     </Screen>
   );
