b9843c76407c9ad4d07b4eaa39fd6c547b4e7c37
Andrew Radulescu <radulescuandrew@gmail.com>
2024-06-06 13:29:54 +0300

    Fix bottom sheets not opening due to 2 PortalProviders

--- mobile/app/(app)/_layout.tsx
+++ mobile/app/(app)/_layout.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Redirect, Stack } from "expo-router";
-import { PortalProvider } from "tamagui";
 import { useAuth } from "../../hooks/useAuth";
 import UserContextProvider from "../../contexts/user/UserContext.provider";
 import NotificationContextProvider from "../../contexts/notification/NotificationContextProvider";
@@ -13,22 +12,20 @@ const AppLayout = () => {
   }
 
   return (
-    <PortalProvider>
-      <UserContextProvider>
-        <NotificationContextProvider>
-          <Stack>
-            <Stack.Screen name="(drawer)" options={{ headerShown: false }} />
-            <Stack.Screen name="polling-station-wizzard" options={{ headerShown: false }} />
-            <Stack.Screen name="manage-polling-stations" options={{ headerShown: false }} />
-            <Stack.Screen name="form-questionnaire" options={{ headerShown: false }} />
-            <Stack.Screen name="polling-station-questionnaire" options={{ headerShown: false }} />
-            <Stack.Screen name="report-issue" options={{ headerShown: false }} />
-            <Stack.Screen name="change-password" options={{ headerShown: false }} />
-            <Stack.Screen name="about-votemonitor" options={{ headerShown: false }} />
-          </Stack>
-        </NotificationContextProvider>
-      </UserContextProvider>
-    </PortalProvider>
+    <UserContextProvider>
+      <NotificationContextProvider>
+        <Stack>
+          <Stack.Screen name="(drawer)" options={{ headerShown: false }} />
+          <Stack.Screen name="polling-station-wizzard" options={{ headerShown: false }} />
+          <Stack.Screen name="manage-polling-stations" options={{ headerShown: false }} />
+          <Stack.Screen name="form-questionnaire" options={{ headerShown: false }} />
+          <Stack.Screen name="polling-station-questionnaire" options={{ headerShown: false }} />
+          <Stack.Screen name="report-issue" options={{ headerShown: false }} />
+          <Stack.Screen name="change-password" options={{ headerShown: false }} />
+          <Stack.Screen name="about-votemonitor" options={{ headerShown: false }} />
+        </Stack>
+      </NotificationContextProvider>
+    </UserContextProvider>
   );
 };
 
