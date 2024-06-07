28b797110bf12cdfffe5d78b042d730fcb89cff0
Ion Dormenco <idormenco@gmail.com>
2024-06-07 23:54:08 +0300

    Add usefull endpoints (#576)
    * resend invitation emails
    * platform admin change password
    * fix excell sheet name

--- api/src/Vote.Monitor.Api.Feature.Auth/ChangePassword/Endpoint.cs
+++ api/src/Vote.Monitor.Api.Feature.Auth/ChangePassword/Endpoint.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Logging;
-using Vote.Monitor.Core.Extensions;
 
 namespace Vote.Monitor.Api.Feature.Auth.ChangePassword;
 
--- api/src/Vote.Monitor.Api.Feature.Auth/NgoAdminsOnly/Request.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Vote.Monitor.Api.Feature.Auth.NgoAdminsOnly;
-
-public class Request
-{
-    public Guid NgoId { get; set; }
-}
