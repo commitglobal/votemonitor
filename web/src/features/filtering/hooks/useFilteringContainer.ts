b6d92676b90f9266b61ac2474097376ce3cb0ef3
Iulian Deaconu <imdeaconu@gmail.com>
2024-09-17 10:16:56 +0300

    Fix observer list losing filtering (#663)
    * fix: make dropdown menus scrollable
    
    * fix: truncate overflowing table columns
    
    * create reusable filtering containers
    
    * add ObserverTags component
    
    * fix ObserversTagSelect
    
    * remove old filtering system from observers list
    
    * feature: remove just one item from a filter array
    
    * fix back button not keeping filtering in ObserversList
    
    * update active filter badges
    
    * add titles to back buttons
    
    * rename components and use enums instead of magic strings
    
    * misc: various changes

---
+++ web/src/features/filtering/hooks/useFilteringContainer.ts
@@ -0,0 +1,34 @@
+import { useSetPrevSearch } from '@/common/prev-search-store';
+import { useNavigate, useSearch } from '@tanstack/react-router';
+import { useCallback } from 'react';
+
+export function useFilteringContainer() {
+  const navigate = useNavigate();
+  const queryParams = useSearch({ strict: false });
+  const setPrevSearch = useSetPrevSearch();
+  const filteringIsActive = Object.keys(queryParams).some((key) => key !== 'tab' && key !== 'viewBy');
+
+  const navigateHandler = useCallback(
+    (search: Record<string, any | undefined>) => {
+      void navigate({
+        // @ts-ignore
+        search: (prev) => {
+          const newSearch: Record<string, string | undefined | string[] | number> = {
+            ...prev,
+            ...search,
+          };
+          setPrevSearch(newSearch);
+          return newSearch;
+        },
+      });
+    },
+    [navigate, setPrevSearch]
+  );
+
+  const resetFilters = () => {
+    navigate({});
+    setPrevSearch({});
+  };
+
+  return { queryParams, filteringIsActive, navigate, navigateHandler, resetFilters };
+}
---
+++ web/src/features/monitoring-observers/filtering/MonitoringObserverStatusSelect.tsx
@@ -0,0 +1,39 @@
+import { FILTER_KEY } from '@/features/filtering/filtering-enums';
+import { MonitoringObserverStatus } from '@/features/monitoring-observers/models/monitoring-observer';
+import { FC } from 'react';
+import { SelectFilter, SelectFilterOption } from '../../filtering/components/SelectFilter';
+import { useFilteringContainer } from '../../filtering/hooks/useFilteringContainer';
+
+const monitoringObserverStausOptions: SelectFilterOption[] = [
+  {
+    value: MonitoringObserverStatus.Active,
+    label: MonitoringObserverStatus.Active,
+  },
+
+  {
+    value: MonitoringObserverStatus.Pending,
+    label: MonitoringObserverStatus.Pending,
+  },
+
+  {
+    value: MonitoringObserverStatus.Suspended,
+    label: MonitoringObserverStatus.Suspended,
+  },
+];
+
+export const MonitoringObserverStatusSelect: FC = () => {
+  const { queryParams, navigateHandler } = useFilteringContainer();
+
+  const onStatusChange = (value: string) => {
+    navigateHandler({ [FILTER_KEY.MonitoringObserverStatus]: value });
+  };
+
+  return (
+    <SelectFilter
+      value={(queryParams as any)[FILTER_KEY.MonitoringObserverStatus]}
+      onChange={onStatusChange}
+      placeholder='Observer status'
+      options={monitoringObserverStausOptions}
+    />
+  );
+};
