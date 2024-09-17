f7d60f826706e325ae959f0b2deba48b7b0770bd
Iulian Deaconu <imdeaconu@gmail.com>
2024-09-17 10:16:56 +0300

    Fix observer list losing filtering (#663)
    * fix: make dropdown menus scrollable
    
    * fix: truncate overflowing table columns
    
    * create reusable filtering containers
    
    * add ObserverTags component
    
    * fix ObserversTagSelect
    
    * remove old filtering system from observers list
    
    * feature: remove just one item from a filter array
    
    * fix back button not keeping filtering in ObserversList
    
    * update active filter badges
    
    * add titles to back buttons
    
    * rename components and use enums instead of magic strings
    
    * misc: various changes

---
+++ web/src/features/monitoring-observers/components/MonitoringObserverBackButton.tsx
@@ -0,0 +1,18 @@
+import { usePrevSearch } from '@/common/prev-search-store';
+import { BackButtonIcon } from '@/components/layout/Breadcrumbs/BackButton';
+import { Link } from '@tanstack/react-router';
+import { FC } from 'react';
+
+export const MonitorObserverBackButton: FC = () => {
+  const prevSearch = usePrevSearch();
+  return (
+    <Link
+      title='Go back'
+      search={prevSearch}
+      to='/monitoring-observers/$tab/'
+      params={{ tab: 'list' }}
+      preload='intent'>
+      <BackButtonIcon />
+    </Link>
+  );
+};
