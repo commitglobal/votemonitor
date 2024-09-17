d8f5703c35c0bc4de763b189d76b205d9fcac200
Iulian Deaconu <imdeaconu@gmail.com>
2024-09-17 10:16:56 +0300

    Fix observer list losing filtering (#663)
    * fix: make dropdown menus scrollable
    
    * fix: truncate overflowing table columns
    
    * create reusable filtering containers
    
    * add ObserverTags component
    
    * fix ObserversTagSelect
    
    * remove old filtering system from observers list
    
    * feature: remove just one item from a filter array
    
    * fix back button not keeping filtering in ObserversList
    
    * update active filter badges
    
    * add titles to back buttons
    
    * rename components and use enums instead of magic strings
    
    * misc: various changes

---
+++ web/src/features/filtering/components/SelectFilter.tsx
@@ -0,0 +1,39 @@
+import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
+import { FC } from 'react';
+
+export type SelectFilterOption = {
+  value: string;
+  label: string;
+};
+
+interface SelectFilterProps {
+  placeholder: string;
+  value: string;
+  options: SelectFilterOption[];
+  onChange: (value: string) => void;
+}
+
+export const SelectFilter: FC<SelectFilterProps> = (props) => {
+  const { placeholder, value, options, onChange } = props;
+
+  const selectId = crypto.randomUUID();
+
+  return (
+    <Select value={value ?? ''} onValueChange={onChange}>
+      <SelectTrigger className='w-[180px]'>
+        <SelectValue placeholder={placeholder} />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectGroup>
+          {options.map((option) => {
+            return (
+              <SelectItem key={`select-${selectId}-${option.value}`} value={option.value}>
+                {option.label}
+              </SelectItem>
+            );
+          })}
+        </SelectGroup>
+      </SelectContent>
+    </Select>
+  );
+};
