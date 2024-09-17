204a3ba05a5361ffb1161412071c6c9ebd744780
Iulian Deaconu <imdeaconu@gmail.com>
2024-09-17 10:16:56 +0300

    Fix observer list losing filtering (#663)
    * fix: make dropdown menus scrollable
    
    * fix: truncate overflowing table columns
    
    * create reusable filtering containers
    
    * add ObserverTags component
    
    * fix ObserversTagSelect
    
    * remove old filtering system from observers list
    
    * feature: remove just one item from a filter array
    
    * fix back button not keeping filtering in ObserversList
    
    * update active filter badges
    
    * add titles to back buttons
    
    * rename components and use enums instead of magic strings
    
    * misc: various changes

---
+++ web/src/features/filtering/filtering-enums.ts
@@ -0,0 +1,11 @@
+export const enum FILTER_KEY {
+  PageSize = 'pageSize',
+  PageNumber = 'pageNumber',
+  MonitoringObserverStatus = 'monitoringObserverStatus',
+  MonitoringObserverTags = 'tags',
+}
+
+export const enum FILTER_LABEL {
+  MonitoringObserverStatus = 'Observer status',
+  MonitoringObserverTags = 'Tags',
+}
