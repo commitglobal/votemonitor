fbb67a346ea211b88a5057adcd083a5c2ba33b28
Ion Dormenco <idormenco@gmail.com>
2024-09-25 16:07:20 +0300

    fix build, and tag selector

--- web/src/features/monitoring-observers/filtering/MonitoringObserverTagsSelect.tsx
+++ web/src/features/monitoring-observers/filtering/MonitoringObserverTagsSelect.tsx
@@ -1,66 +1,38 @@
-import { Button } from '@/components/ui/button';
-import {
-  DropdownMenu,
-  DropdownMenuCheckboxItem,
-  DropdownMenuContent,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu';
-import { Input } from '@/components/ui/input';
+import { MultiSelectDropdown } from '@/components/ui/multiple-select-dropdown';
 import { useCurrentElectionRoundStore } from '@/context/election-round.store';
 import { FILTER_KEY } from '@/features/filtering/filtering-enums';
 import { useFilteringContainer } from '@/features/filtering/hooks/useFilteringContainer';
 import { useMonitoringObserversTags } from '@/hooks/tags-queries';
-import { Check, ChevronDown, ChevronUp } from 'lucide-react';
 
-import { FC, useState } from 'react';
+import { FC, useEffect } from 'react';
 
 export const MonitoringObserverTagsSelect: FC = () => {
   const currentElectionRoundId = useCurrentElectionRoundStore((s) => s.currentElectionRoundId);
   const { data: tags } = useMonitoringObserversTags(currentElectionRoundId);
   const { queryParams, navigateHandler } = useFilteringContainer();
   const currentTags = (queryParams as any)?.[FILTER_KEY.MonitoringObserverTags] ?? [];
-  const [query, setQuery] = useState<string>('');
 
-  const toggleTagsFilter = (tag: string) => {
-    if (!currentTags.includes(tag))
-      return navigateHandler({ [FILTER_KEY.MonitoringObserverTags]: [...currentTags, tag] });
-
-    const filteredTags = currentTags.filter((tagText: string) => tagText !== tag);
-
-    return navigateHandler({ [FILTER_KEY.MonitoringObserverTags]: filteredTags });
+  const toggleTagsFilter = (tags: string[]) => {
+    return navigateHandler({ [FILTER_KEY.MonitoringObserverTags]: tags });
   };
 
+  useEffect(() => {}, [currentTags]);
+
   return (
-    <DropdownMenu onOpenChange={() => setQuery('')}>
-      <DropdownMenuTrigger asChild>
-        <Button className='border-gray-200 gap-1 hover:bg-white w-[180px]' variant='outline'>
+    <MultiSelectDropdown
+      options={tags?.map((tag) => ({ label: tag, value: tag })) ?? []}
+      onValueChange={toggleTagsFilter}
+      placeholder='Observer tags'
+      defaultValue={currentTags}
+      className='text-slate-700'
+      selectionDisplay={
+        <div>
           <span className='text-sm font-normal text-slate-700'>Observer tags</span>
           {currentTags && currentTags.length && (
-            <span className='inline-block px-2 text-purple-600 rounded-full bg-purple-50'>{currentTags.length}</span>
+            <span className='px-2 text-purple-600 rounded-full bg-purple-50'>{currentTags.length}</span>
           )}
-          <ChevronDown className='w-4 h-4 opacity-50' />
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent className='w-56'>
-        <div className='sticky top-0 p-2 bg-background'>
-          <Input
-            placeholder='Search...'
-            value={query}
-            onChange={(e) => setQuery(e.target.value)}
-            onKeyDown={(e: React.KeyboardEvent<HTMLInputElement>) => e.stopPropagation()}
-          />
         </div>
-        <ScrollArea className='h-[300px]'>
-          {tags?.map((tag) => (
-            <DropdownMenuCheckboxItem
-              checked={currentTags.includes(tag)}
-              onCheckedChange={() => toggleTagsFilter(tag)}
-              key={tag}>
-              {tag}
-            </DropdownMenuCheckboxItem>
-          ))}
-        </ScrollArea>
-      </DropdownMenuContent>
-    </DropdownMenu>
+      }
+    />
   );
 };
