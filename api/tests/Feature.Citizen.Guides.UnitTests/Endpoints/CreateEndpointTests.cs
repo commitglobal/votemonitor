42dbb42e45d56d678c802aca636604f428810969
Ion Dormenco <idormenco@gmail.com>
2024-10-07 17:25:15 +0300

    fix failing tests

--- api/tests/Feature.Citizen.Guides.UnitTests/Endpoints/CreateEndpointTests.cs
+++ api/tests/Feature.Citizen.Guides.UnitTests/Endpoints/CreateEndpointTests.cs
@@ -1,90 +1,74 @@
 ﻿using System.Net;
 using System.Security.Claims;
-using System.Text;
 using FastEndpoints;
-using Feature.ObserverGuide.Create;
-using Feature.ObserverGuide.Specifications;
 using FluentAssertions;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.HttpResults;
 using NSubstitute;
-using NSubstitute.ReturnsExtensions;
 using Vote.Monitor.Core.Services.FileStorage.Contracts;
 using Vote.Monitor.Core.Services.Security;
-using Vote.Monitor.Domain.Entities.MonitoringNgoAggregate;
-using Vote.Monitor.Domain.Entities.ObserverGuideAggregate;
 using Vote.Monitor.Domain.Repository;
-using Vote.Monitor.TestUtils.Fakes;
 using Vote.Monitor.TestUtils.Fakes.Aggregates;
 
-namespace Feature.ObserverGuide.UnitTests.Endpoints;
+namespace Feature.Citizen.Guides.UnitTests.Endpoints;
 
 public class CreateEndpointTests
 {
-    private readonly IRepository<ObserverGuideAggregate> _repository;
+    private readonly IRepository<CitizenGuide> _repository;
     private readonly IFileStorageService _fileStorageService;
     private readonly IAuthorizationService _authorizationService;
-    private readonly IReadRepository<MonitoringNgo> _monitoringNgoRepository;
-    private readonly Create.Endpoint _endpoint;
     private readonly IHtmlStringSanitizer _htmlStringSanitizer;
+    private readonly Create.Endpoint _endpoint;
 
     public CreateEndpointTests()
     {
-        _repository = Substitute.For<IRepository<ObserverGuideAggregate>>();
+        _repository = Substitute.For<IRepository<CitizenGuide>>();
         _fileStorageService = Substitute.For<IFileStorageService>();
         _authorizationService = Substitute.For<IAuthorizationService>();
-        _monitoringNgoRepository = Substitute.For<IReadRepository<MonitoringNgo>>();
         _htmlStringSanitizer = Substitute.For<IHtmlStringSanitizer>();
 
         _endpoint = Factory.Create<Create.Endpoint>(_authorizationService,
             _repository,
-            _monitoringNgoRepository,
             _fileStorageService,
             _htmlStringSanitizer);
     }
 
     [Fact]
-    public async Task ShouldReturnOkWithObserverGuideModel_WhenUploadSucceeds()
+    public async Task ShouldReturnOkWithCitizenGuideModel_WhenUploadSucceeds()
     {
         // Arrange
         var fakeElectionRound = new ElectionRoundAggregateFaker().Generate();
-        var fakeMonitoringNgo = new MonitoringNgoAggregateFaker().Generate();
 
         _authorizationService.AuthorizeAsync(Arg.Any<ClaimsPrincipal>(), Arg.Any<object?>(),
                 Arg.Any<IEnumerable<IAuthorizationRequirement>>())
             .Returns(AuthorizationResult.Success());
 
-        _monitoringNgoRepository
-            .FirstOrDefaultAsync(Arg.Any<GetMonitoringNgoSpecification>())
-            .Returns(fakeMonitoringNgo);
-
         var fileName = "file.txt";
         var url = "url";
         var urlValidityInSeconds = 60;
+
         _fileStorageService.UploadFileAsync(Arg.Any<string>(), Arg.Any<string>(), Arg.Any<Stream>(),
                 Arg.Any<CancellationToken>())
             .Returns(new UploadFileResult.Ok(url, fileName, urlValidityInSeconds));
 
         // Act
-        var observerGuideTitle = "my observer guide";
+        var guideTitle = "my citizen guide";
 
-        var request = new Request
+        var request = new Create.Request
         {
             ElectionRoundId = fakeElectionRound.Id,
-            Title = observerGuideTitle,
+            Title = guideTitle,
             Attachment = FakeFormFile.New().HavingFileName(fileName).Please(),
-            GuideType = ObserverGuideType.Document
+            GuideType = CitizenGuideType.Document
         };
         var result = await _endpoint.ExecuteAsync(request, default);
 
         // Assert
         await _repository
             .Received(1)
-            .AddAsync(Arg.Is<ObserverGuideAggregate>(x => x.Title == observerGuideTitle
-                                                          && x.MonitoringNgoId == fakeMonitoringNgo.Id));
+            .AddAsync(Arg.Is<CitizenGuide>(x => x.Title == guideTitle));
 
-        var model = result.Result.As<Ok<ObserverGuideModel>>();
+        var model = result.Result.As<Ok<CitizenGuideModel>>();
         model.Value!.PresignedUrl.Should().Be(url);
         model.Value.UrlValidityInSeconds.Should().Be(urlValidityInSeconds);
         model.Value.FileName.Should().Be(fileName);
@@ -101,10 +85,10 @@ public class CreateEndpointTests
             .Returns(AuthorizationResult.Failed());
 
         // Act
-        var request = new Request
+        var request = new Create.Request
         {
             ElectionRoundId = fakeElectionRound.Id,
-            Title = "my observer guide",
+            Title = "my citizen guide",
             Attachment = FakeFormFile.New().Please()
         };
         var result = await _endpoint.ExecuteAsync(request, default);
@@ -112,54 +96,10 @@ public class CreateEndpointTests
         // Assert
         await _repository
             .DidNotReceive()
-            .AddAsync(Arg.Any<ObserverGuideAggregate>());
+            .AddAsync(Arg.Any<CitizenGuide>());
 
         result
-            .Should().BeOfType<Results<Ok<ObserverGuideModel>, NotFound, StatusCodeHttpResult>>()
-            .Which
-            .Result.Should().BeOfType<NotFound>();
-    }
-
-    [Fact]
-    public async Task ShouldReturnNotFound_WhenMonitoringNgoNotFound()
-    {
-        // Arrange
-        var fakeElectionRound = new ElectionRoundAggregateFaker().Generate();
-        var fakeMonitoringNgo = new MonitoringNgoAggregateFaker().Generate();
-
-        _authorizationService.AuthorizeAsync(Arg.Any<ClaimsPrincipal>(), Arg.Any<object?>(),
-                Arg.Any<IEnumerable<IAuthorizationRequirement>>())
-            .Returns(AuthorizationResult.Success());
-
-        _monitoringNgoRepository
-            .FirstOrDefaultAsync(Arg.Any<GetMonitoringNgoSpecification>())
-            .ReturnsNull();
-
-        var fileName = "file.txt";
-        var url = "url";
-        var urlValidityInSeconds = 60;
-        _fileStorageService.UploadFileAsync(Arg.Any<string>(), Arg.Any<string>(), Arg.Any<Stream>(),
-                Arg.Any<CancellationToken>())
-            .Returns(new UploadFileResult.Ok(url, fileName, urlValidityInSeconds));
-
-        // Act
-        var observerGuideTitle = "my observer guide";
-
-        var request = new Request
-        {
-            ElectionRoundId = fakeElectionRound.Id,
-            Title = observerGuideTitle,
-            Attachment = FakeFormFile.New().Please()
-        };
-        var result = await _endpoint.ExecuteAsync(request, default);
-
-        // Assert
-        await _repository
-            .DidNotReceive()
-            .AddAsync(Arg.Any<ObserverGuideAggregate>());
-
-        result
-            .Should().BeOfType<Results<Ok<ObserverGuideModel>, NotFound, StatusCodeHttpResult>>()
+            .Should().BeOfType<Results<Ok<CitizenGuideModel>, NotFound, StatusCodeHttpResult>>()
             .Which
             .Result.Should().BeOfType<NotFound>();
     }
@@ -169,37 +109,33 @@ public class CreateEndpointTests
     {
         // Arrange
         var fakeElectionRound = new ElectionRoundAggregateFaker().Generate();
-        var fakeMonitoringNgo = new MonitoringNgoAggregateFaker().Generate();
 
         _authorizationService.AuthorizeAsync(Arg.Any<ClaimsPrincipal>(), Arg.Any<object?>(),
                 Arg.Any<IEnumerable<IAuthorizationRequirement>>())
             .Returns(AuthorizationResult.Success());
-
-        _monitoringNgoRepository
-            .FirstOrDefaultAsync(Arg.Any<GetMonitoringNgoSpecification>())
-            .Returns(fakeMonitoringNgo);
 
         _fileStorageService.UploadFileAsync(Arg.Any<string>(), Arg.Any<string>(), Arg.Any<Stream>(),
                 Arg.Any<CancellationToken>())
             .Returns(new UploadFileResult.Failed("error message"));
 
         // Act
-        var request = new Request
+        var request = new Create.Request
         {
             ElectionRoundId = fakeElectionRound.Id,
-            Title = "my observer guide",
+            Title = "my citizen guide",
             Attachment = FakeFormFile.New().Please(),
-            GuideType = ObserverGuideType.Document
+            GuideType = CitizenGuideType.Document
         };
+
         var result = await _endpoint.ExecuteAsync(request, default);
 
         // Assert
         await _repository
             .DidNotReceive()
-            .AddAsync(Arg.Any<ObserverGuideAggregate>());
+            .AddAsync(Arg.Any<CitizenGuide>());
 
         result
-            .Should().BeOfType<Results<Ok<ObserverGuideModel>, NotFound, StatusCodeHttpResult>>()
+            .Should().BeOfType<Results<Ok<CitizenGuideModel>, NotFound, StatusCodeHttpResult>>()
             .Which
             .Result.Should().BeOfType<StatusCodeHttpResult>();
 
@@ -212,15 +148,10 @@ public class CreateEndpointTests
     {
         // Arrange
         var fakeElectionRound = new ElectionRoundAggregateFaker().Generate();
-        var fakeMonitoringNgo = new MonitoringNgoAggregateFaker().Generate();
 
         _authorizationService.AuthorizeAsync(Arg.Any<ClaimsPrincipal>(), Arg.Any<object?>(),
                 Arg.Any<IEnumerable<IAuthorizationRequirement>>())
             .Returns(AuthorizationResult.Success());
-
-        _monitoringNgoRepository
-            .FirstOrDefaultAsync(Arg.Any<GetMonitoringNgoSpecification>())
-            .Returns(fakeMonitoringNgo);
 
         // Act
         var guideText = "<p>some html</p>";
@@ -229,7 +160,7 @@ public class CreateEndpointTests
             ElectionRoundId = fakeElectionRound.Id,
             Title = "my citizen guide",
             Text = guideText,
-            GuideType = ObserverGuideType.Text
+            GuideType = CitizenGuideType.Text
         };
         var result = await _endpoint.ExecuteAsync(request, default);
 
