4a4c09769f726ab0c4c363172438d615e0503b32
Ion Dormenco <idormenco@gmail.com>
2024-10-09 15:47:05 +0300

    Fix psi form submissions

--- api/src/Feature.FormTemplates/Create/Request.cs
+++ api/src/Feature.FormTemplates/Create/Request.cs
@@ -12,6 +12,6 @@ public class Request
     public TranslatedString Description { get; set; } = new();
     public FormTemplateType FormTemplateType { get; set; }
     public List<string> Languages { get; set; } = [];
-    public List<BaseQuestionRequest> Questions { get; set; } = new();
+    public List<BaseQuestionRequest> Questions { get; set; } = [];
 
 }
