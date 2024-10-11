6724f841c48d948d76bc300206a4f6f4601aee8c
Ion Dormenco <idormenco@gmail.com>
2024-10-11 13:27:39 +0300

    Fix update complete flag (#745)

--- api/src/Feature.Form.Submissions/SetCompletion/Validator.cs
+++ api/src/Feature.Form.Submissions/SetCompletion/Validator.cs
@@ -6,6 +6,7 @@ public class Validator : Validator<Request>
     {
         RuleFor(x => x.ElectionRoundId).NotEmpty();
         RuleFor(x => x.ObserverId).NotEmpty();
-        RuleFor(x => x.Id).NotEmpty();
+        RuleFor(x => x.PollingStationId).NotEmpty();
+        RuleFor(x => x.FormId).NotEmpty();
     }
 }
