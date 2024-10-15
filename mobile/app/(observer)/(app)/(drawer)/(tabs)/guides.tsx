f4bd18b0d56ce3b6fbdf7948376eaddb98a13306
luciatugui <lucia.tugui@wearetribus.com>
2024-10-15 13:28:09 +0300

    feat: info modal on quick-report and guides pages

--- mobile/app/(observer)/(app)/(drawer)/(tabs)/guides.tsx
+++ mobile/app/(observer)/(app)/(drawer)/(tabs)/guides.tsx
@@ -12,14 +12,17 @@ import SearchInput from "../../../../../components/SearchInput";
 import { useState, useMemo } from "react";
 import { filterResources } from "../../../../../helpers/resources";
 import { Guide } from "../../../../../services/api/get-guides.api";
+import InfoModal from "../../../../../components/InfoModal";
 
 const Guides = () => {
   const { t } = useTranslation("guides");
   const router = useRouter();
   const navigation = useNavigation();
-  const [search, setSearch] = useState("");
-  const { activeElectionRound } = useUserData();
 
+  const [search, setSearch] = useState("");
+  const [isOpenInfoModal, setIsOpenInfoModal] = useState(false);
+
+  const { activeElectionRound } = useUserData();
   const {
     data: guides,
     isLoading: isLoadingGuides,
@@ -39,6 +42,14 @@ const Guides = () => {
     router.push(`/guide/${resource.id}`);
   };
 
+  const handleOpenInfoModal = () => {
+    setIsOpenInfoModal(true);
+  };
+
+  const handleCloseInfoModal = () => {
+    setIsOpenInfoModal(false);
+  };
+
   return (
     <Screen
       preset="fixed"
@@ -53,6 +64,8 @@ const Guides = () => {
           barStyle="light-content"
           leftIcon={<Icon icon="menuAlt2" color="white" />}
           onLeftPress={() => navigation.dispatch(DrawerActions.openDrawer)}
+          rightIcon={<Icon icon="infoCircle" color="white" />}
+          onRightPress={handleOpenInfoModal}
         />
         <YStack backgroundColor="$purple5" padding="$md">
           <SearchInput onSearch={handleSearch} />
@@ -66,6 +79,10 @@ const Guides = () => {
         emptyContainerMarginTop="30%"
         onResourcePress={handleResourcePress}
       />
+
+      {isOpenInfoModal && (
+        <InfoModal paragraphs={[t("info_modal")]} handleCloseInfoModal={handleCloseInfoModal} />
+      )}
     </Screen>
   );
 };
