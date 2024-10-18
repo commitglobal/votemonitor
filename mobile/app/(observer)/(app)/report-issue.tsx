533dc108e933864d685ede0c0cd198192bff20f1
Ion Dormenco <idormenco@gmail.com>
2024-10-18 18:08:56 +0300

    Update report-issue.tsx

--- mobile/app/(observer)/(app)/report-issue.tsx
+++ mobile/app/(observer)/(app)/report-issue.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from "react";
+import React, { useMemo, useRef, useState } from "react";
 import { XStack, YStack } from "tamagui";
 import { Screen } from "../../../components/Screen";
 import { Icon } from "../../../components/Icon";
@@ -19,35 +19,30 @@ import { Typography } from "../../../components/Typography";
 import { useAddQuickReport } from "../../../services/mutations/quick-report/add-quick-report.mutation";
 import * as Crypto from "expo-crypto";
 import { FileMetadata, useCamera } from "../../../hooks/useCamera";
-import { useUploadAttachmentQuickReportMutation } from "../../../services/mutations/quick-report/add-attachment-quick-report.mutation";
-import {
-  IncidentCategory,
-  QuickReportLocationType,
-} from "../../../services/api/quick-report/post-quick-report.api";
+import { useUploadAttachmentQuickReportAbortMutation, useUploadAttachmentQuickReportCompleteMutation, useUploadAttachmentQuickReportMutation } from "../../../services/mutations/quick-report/add-attachment-quick-report.mutation";
+import { IncidentCategory, QuickReportLocationType } from "../../../services/api/quick-report/post-quick-report.api";
 import * as DocumentPicker from "expo-document-picker";
 import { onlineManager, useMutationState, useQueryClient } from "@tanstack/react-query";
 import Card from "../../../components/Card";
 import { QuickReportKeys } from "../../../services/queries/quick-reports.query";
 import * as Sentry from "@sentry/react-native";
 import {
-  addAttachmentQuickReportMultipartAbort,
-  addAttachmentQuickReportMultipartComplete,
   AddAttachmentQuickReportStartAPIPayload,
 } from "../../../services/api/quick-report/add-attachment-quick-report.api";
 import { useTranslation } from "react-i18next";
-import i18n from "../../../common/config/i18n";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 import Toast from "react-native-toast-message";
-import { uploadS3Chunk } from "../../../services/api/add-attachment.api";
 // import { t } from "i18next";
-import { MULTIPART_FILE_UPLOAD_SIZE } from "../../../common/constants";
+import { MULTIPART_FILE_UPLOAD_SIZE, MUTATION_SCOPE_DO_NOT_HYDRATE } from "../../../common/constants";
 import * as FileSystem from "expo-file-system";
 import { Buffer } from "buffer";
 import MediaLoading from "../../../components/MediaLoading";
 import { useNetInfoContext } from "../../../contexts/net-info-banner/NetInfoContext";
+import { removeMutationByScopeId, useUploadS3ChunkMutation } from "../../../services/mutations/attachments/add-attachment.mutation";
+import { TFunction } from "i18next";
 import { localizeIncidentCategory } from "../../../helpers/translationHelper";
 
-const mapVisitsToSelectPollingStations = (visits: PollingStationVisitVM[] = []) => {
+const mapVisitsToSelectPollingStations = (visits: PollingStationVisitVM[] = [], t: TFunction) => {
   const pollingStationsForSelect = visits.map((visit) => {
     return {
       id: visit.pollingStationId,
@@ -61,12 +56,12 @@ const mapVisitsToSelectPollingStations = (visits: PollingStationVisitVM[] = []) 
     {
       id: "other",
       value: QuickReportLocationType.OtherPollingStation,
-      label: i18n.t("form.polling_station_id.options.other", { ns: "report_new_issue" }),
+      label: t("form.polling_station_id.options.other", { ns: "report_new_issue" }),
     },
     {
       id: "not_related_to_polling_station",
       value: QuickReportLocationType.NotRelatedToAPollingStation,
-      label: i18n.t("form.polling_station_id.options.not_related", { ns: "report_new_issue" }),
+      label: t("form.polling_station_id.options.not_related", { ns: "report_new_issue" }),
     },
   );
   return pollingStationsForSelect;
@@ -107,6 +102,7 @@ const mapIncidentCategoriesToSelectList = () => {
   }));
 };
 
+
 type ReportIssueFormType = {
   polling_station_id: string;
   polling_station_details: string;
@@ -116,15 +112,18 @@ type ReportIssueFormType = {
 };
 
 const ReportIssue = () => {
+  const cancelRef = useRef<boolean>(false);
+  const { t } = useTranslation("report_new_issue");
   const insets = useSafeAreaInsets();
   const queryClient = useQueryClient();
   const { visits, activeElectionRound } = useUserData();
-  const pollingStations = useMemo(() => mapVisitsToSelectPollingStations(visits), [visits]);
-  const incidentCategories = useMemo(() => mapIncidentCategoriesToSelectList(), [i18n]);
+  const pollingStations = useMemo(() => mapVisitsToSelectPollingStations(visits, t), [visits, t]);
+  const incidentCategories = useMemo(() => mapIncidentCategoriesToSelectList(), [t]);
+
   const [optionsSheetOpen, setOptionsSheetOpen] = useState(false);
-  const { t } = useTranslation("report_new_issue");
   const [isLoadingAttachment, setIsLoadingAttachment] = useState(false);
   const [isPreparingFile, setIsPreparingFile] = useState(false);
+  const [isUploading, setIsUploading] = useState(false);
   const [uploadProgress, setUploadProgress] = useState("");
   const { isOnline } = useNetInfoContext();
 
@@ -138,9 +137,10 @@ const ReportIssue = () => {
     isPaused: isPausedAddQuickReport,
   } = useAddQuickReport();
 
-  const { mutateAsync: addAttachmentQReport } = useUploadAttachmentQuickReportMutation(
-    `Quick_Report_${activeElectionRound?.id}}`,
-  );
+  const { mutateAsync: addAttachmentQReport } = useUploadAttachmentQuickReportMutation();
+  const { mutateAsync: addAttachmentQReportComplete } = useUploadAttachmentQuickReportCompleteMutation();
+  const { mutateAsync: addAttachmentQReportAbort } = useUploadAttachmentQuickReportAbortMutation();
+  const { mutateAsync: uploadS3Chunk } = useUploadS3ChunkMutation();
 
   const addAttachmentsMutationState = useMutationState({
     filters: { mutationKey: QuickReportKeys.addAttachment() },
@@ -167,7 +167,7 @@ const ReportIssue = () => {
       polling_station_details: "",
       issue_title: "",
       issue_description: "",
-      incident_category: IncidentCategory.Other,
+      incident_category: IncidentCategory.Other
     },
   });
 
@@ -187,6 +187,7 @@ const ReportIssue = () => {
 
     if (!cameraResult || !activeElectionRound) {
       setUploadProgress("");
+      setIsPreparingFile(false);
       return;
     }
 
@@ -239,13 +240,17 @@ const ReportIssue = () => {
       let etags: Record<number, string> = {};
       const urls = Object.values(uploadUrls);
       for (const [index, url] of urls.entries()) {
+        if (cancelRef.current) {
+          throw new Error('Upload aborted');
+        }
+
         const chunk = await FileSystem.readAsStringAsync(filePath, {
           length: MULTIPART_FILE_UPLOAD_SIZE,
           position: index * MULTIPART_FILE_UPLOAD_SIZE,
           encoding: FileSystem.EncodingType.Base64,
         });
         const buffer = Buffer.from(chunk, "base64");
-        const data = await uploadS3Chunk(url, buffer);
+        const data = await uploadS3Chunk({ url, chunk: buffer });
         setUploadProgress(
           `${t("upload.progress")} ${Math.ceil(((uploadedPartsNo + index) / totalParts) * 100)}%`,
         );
@@ -253,7 +258,7 @@ const ReportIssue = () => {
       }
 
       if (activeElectionRound?.id) {
-        await addAttachmentQuickReportMultipartComplete({
+        await addAttachmentQReportComplete({
           uploadId,
           etags,
           electionRoundId: activeElectionRound?.id,
@@ -265,7 +270,7 @@ const ReportIssue = () => {
       Sentry.captureException(err, { data: activeElectionRound });
       if (activeElectionRound?.id) {
         setUploadProgress(t("upload.aborted"));
-        await addAttachmentQuickReportMultipartAbort({
+        await addAttachmentQReportAbort({
           id: attachmentId,
           uploadId,
           electionRoundId: activeElectionRound.id,
@@ -305,8 +310,10 @@ const ReportIssue = () => {
     const optimisticAttachments: AddAttachmentQuickReportStartAPIPayload[] = [];
 
     if (attachments.length > 0) {
+      setIsUploading(true);
       setOptionsSheetOpen(true);
       setIsLoadingAttachment(true);
+      cancelRef.current = false;
       try {
         const totalParts = attachments.reduce((acc, attachment) => {
           return acc + Math.ceil(attachment.fileMetadata.size! / MULTIPART_FILE_UPLOAD_SIZE);
@@ -315,6 +322,10 @@ const ReportIssue = () => {
         // Upload each attachment
         setUploadProgress(`${t("upload.starting")}`);
         for (const [, attachment] of attachments.entries()) {
+          if (cancelRef.current) {
+            throw new Error('Upload aborted');
+          }
+
           const payload: AddAttachmentQuickReportStartAPIPayload = {
             id: attachment.id,
             fileName: attachment.fileMetadata.name,
@@ -341,6 +352,7 @@ const ReportIssue = () => {
           optimisticAttachments.push(payload);
         }
         setUploadProgress(t("upload.completed"));
+        setIsUploading(false);
       } catch (err) {
         Sentry.captureException(err);
       }
@@ -350,11 +362,11 @@ const ReportIssue = () => {
       {
         id: uuid,
         electionRoundId: activeElectionRound?.id,
-        incidentCategory: formData.incident_category as IncidentCategory,
         description: formData.issue_description,
         title: formData.issue_title,
         quickReportLocationType,
         pollingStationDetails: formData.polling_station_details,
+        incidentCategory: formData.incident_category,
         ...(pollingStationId ? { pollingStationId } : {}),
         attachments: optimisticAttachments,
       },
@@ -382,7 +394,7 @@ const ReportIssue = () => {
     setAttachments((attachments) => attachments.filter((attachment) => attachment.id !== id));
   };
 
-  const handleOnShowAttachmentSheet = () => {
+  const handleOnShowAttachementSheet = () => {
     if (isOnline) {
       setOptionsSheetOpen(true);
     } else {
@@ -393,6 +405,15 @@ const ReportIssue = () => {
         text2Style: { textAlign: "center" },
       });
     }
+  };
+
+  const onAbortUpload = () => {
+    cancelRef.current = true;
+    setOptionsSheetOpen(false);
+    setIsPreparingFile(false);
+    setIsUploading(false);
+    setUploadProgress("");
+    removeMutationByScopeId(MUTATION_SCOPE_DO_NOT_HYDRATE);
   };
 
   return (
@@ -474,7 +495,7 @@ const ReportIssue = () => {
                 />
               )}
 
-              <Controller
+               <Controller
                 key="incident_category"
                 name="incident_category"
                 control={control}
@@ -592,7 +613,7 @@ const ReportIssue = () => {
                 label={t("media.add")}
                 onPress={() => {
                   Keyboard.dismiss();
-                  handleOnShowAttachmentSheet();
+                  handleOnShowAttachementSheet();
                 }}
               />
             </YStack>
@@ -602,10 +623,14 @@ const ReportIssue = () => {
         <OptionsSheet
           open={optionsSheetOpen}
           setOpen={setOptionsSheetOpen}
-          isLoading={isLoadingAttachment || isPreparingFile}
+          isLoading={isLoadingAttachment || isPreparingFile || isUploading}
         >
-          {isLoadingAttachment || isPreparingFile ? (
-            <MediaLoading progress={uploadProgress} />
+          {isLoadingAttachment || isPreparingFile || isUploading ? (
+            <MediaLoading
+              progress={uploadProgress}
+              isUploading={isUploading}
+              onAbortUpload={onAbortUpload}
+            />
           ) : (
             <YStack paddingHorizontal="$sm">
               <Typography
