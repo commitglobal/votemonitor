37e586dd6c8cb647f9984ad8d0ef9386683bda46
luciatugui <lucia.tugui@wearetribus.com>
2024-10-18 10:29:38 +0300

    fix: ui/ol lists in app guides & scrollview height

--- mobile/app/(observer)/(app)/guide/[guideId].tsx
+++ mobile/app/(observer)/(app)/guide/[guideId].tsx
@@ -18,7 +18,7 @@ const GuideScreen = () => {
   };
 
   return (
-    <Screen preset="fixed" safeAreaEdges={["bottom"]}>
+    <Screen preset="fixed" safeAreaEdges={["bottom"]} contentContainerStyle={{ flexGrow: 1 }}>
       <Header
         title={guide?.title || ""}
         leftIcon={<Icon icon="chevronLeft" color="white" />}
