7e21d56435fccc09cf72e59b88da147cc3c216eb
luciatugui <lucia.tugui@wearetribus.com>
2024-10-22 12:09:09 +0300

    fix: don't display psi form card unless we have questions

--- mobile/components/PollingStationGeneral.tsx
+++ mobile/components/PollingStationGeneral.tsx
@@ -1,5 +1,5 @@
 import { router } from "expo-router";
-import { XStack, YStack } from "tamagui";
+import { YStack } from "tamagui";
 import PollingStationInfo from "./PollingStationInfo";
 import PollingStationInfoDefault from "./PollingStationInfoDefault";
 import {
@@ -11,7 +11,6 @@ import Card from "./Card";
 import { useTranslation } from "react-i18next";
 import { Typography } from "./Typography";
 import { PSITime } from "./PSITime";
-import { Icon } from "./Icon";
 
 interface PollingStationGeneralProps {
   psiData: PollingStationInformationAPIResponse | null | undefined;
@@ -24,25 +23,6 @@ export const PollingStationGeneral: React.FC<PollingStationGeneralProps> = ({
 }) => {
   const { t } = useTranslation("observation");
 
-  // PSI form not configured
-  if (!psiFormQuestions || !psiFormQuestions.questions) {
-    return (
-      <YStack gap="$xxs">
-        <Card>
-          <XStack gap="$md" alignItems="center">
-            <Icon icon="warning" color="$purple5" />
-            <Typography preset="body2" color="$purple5" flex={1}>
-              {t("no_form")}
-            </Typography>
-          </XStack>
-        </Card>
-        <Typography preset="helper" color="$gray5" fontWeight="400" flex={1}>
-          {t("refresh_page")}
-        </Typography>
-      </YStack>
-    );
-  }
-
   return (
     <YStack gap="$xxs">
       <Typography preset="body2" fontWeight="700" color="$gray7">
@@ -51,22 +31,25 @@ export const PollingStationGeneral: React.FC<PollingStationGeneralProps> = ({
 
       <PSITime psiData={psi} />
 
-      <Card gap="$md" onPress={router.push.bind(null, "/polling-station-questionnaire")}>
-        {!psi?.answers?.length && !psi?.isCompleted ? (
-          <PollingStationInfoDefault
-            onPress={router.push.bind(null, "/polling-station-questionnaire")}
-          />
-        ) : (
-          <PollingStationInfo
-            nrOfAnswers={psi?.answers?.length}
-            nrOfQuestions={psiFormQuestions?.questions?.length}
-            isMarkedAsCompleted={psi?.isCompleted}
-          />
-        )}
-        <CardFooter
-          text={t("polling_stations_information.polling_station_form.form_details_button_label")}
-        ></CardFooter>
-      </Card>
+      {/* only display the PSI card for polling stations that have a configured PSI form */}
+      {psiFormQuestions && psiFormQuestions.questions && psiFormQuestions.questions.length > 0 && (
+        <Card gap="$md" onPress={router.push.bind(null, "/polling-station-questionnaire")}>
+          {!psi?.answers?.length && !psi?.isCompleted ? (
+            <PollingStationInfoDefault
+              onPress={router.push.bind(null, "/polling-station-questionnaire")}
+            />
+          ) : (
+            <PollingStationInfo
+              nrOfAnswers={psi?.answers?.length}
+              nrOfQuestions={psiFormQuestions?.questions?.length}
+              isMarkedAsCompleted={psi?.isCompleted}
+            />
+          )}
+          <CardFooter
+            text={t("polling_stations_information.polling_station_form.form_details_button_label")}
+          ></CardFooter>
+        </Card>
+      )}
     </YStack>
   );
 };
