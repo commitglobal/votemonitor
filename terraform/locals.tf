7acfe012821c635ce7f746c928709092e223ac8c
Ion Dormenco <idormenco@gmail.com>
2024-10-23 17:46:13 +0300

    Update locals.tf

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -5,12 +5,12 @@ locals {
   images = {
     api = {
       image = "commitglobal/votemonitor"
-      tag   = "0.2.29"
+      tag   = "0.2.30"
     }
 
     hangfire = {
       image = "commitglobal/votemonitor-hangfire"
-      tag   = "0.2.29"
+      tag   = "0.2.30"
     }
   }
 
