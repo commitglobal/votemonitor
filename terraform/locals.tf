1c5830cc03bf6a7dba01f6154b43793bc66c8824
Ion Dormenco <idormenco@gmail.com>
2024-11-29 11:19:55 +0200

    Release v0.2.42

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -5,12 +5,12 @@ locals {
   images = {
     api = {
       image = "commitglobal/votemonitor"
-      tag   = "0.2.41"
+      tag   = "0.2.42"
     }
 
     hangfire = {
       image = "commitglobal/votemonitor-hangfire"
-      tag   = "0.2.41"
+      tag   = "0.2.42"
     }
   }
 
