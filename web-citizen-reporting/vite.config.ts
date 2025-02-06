d136c6f0057b5bfcb9ba9bac0b84a2cabeb56e9f
imdeaconu <imdeaconu@gmail.com>
2025-02-06 09:46:11 +0200

    WIP: add env vars and packages

--- web-citizen-reporting/vite.config.ts
+++ web-citizen-reporting/vite.config.ts
@@ -1,10 +1,11 @@
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [TanStackRouterVite({ autoCodeSplitting: true }), react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
