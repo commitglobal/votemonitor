cfcbde98e46dc7dd5c7ffd794977aa0017bf1fe3
imdeaconu <imdeaconu@gmail.com>
2025-02-06 17:45:09 +0200

    WIP: fix fallthrough switch

--- web-citizen-reporting/src/location-reducer.ts
+++ web-citizen-reporting/src/location-reducer.ts
@@ -17,46 +17,45 @@ export const locationReducer = (
 ): LocationState => {
   switch (action.type) {
     case "SET_FILTER":
-      if (action.level === "level1Filter")
-        return {
-          ...state,
-          level1Filter: action.value,
-          level2Filter: undefined,
-          level3Filter: undefined,
-          level4Filter: undefined,
-          level5Filter: undefined,
-        };
-
-      if (action.level === "level2Filter")
-        return {
-          ...state,
-          level2Filter: action.value,
-          level3Filter: undefined,
-          level4Filter: undefined,
-          level5Filter: undefined,
-        };
-
-      if (action.level === "level3Filter")
-        return {
-          ...state,
-          level3Filter: action.value,
-          level4Filter: undefined,
-          level5Filter: undefined,
-        };
-
-      if (action.level === "level4Filter")
-        return {
-          ...state,
-          level4Filter: action.value,
-          level5Filter: undefined,
-        };
-
-      if (action.level === "level5Filter")
-        return {
-          ...state,
-          level5Filter: action.value,
-        };
-
+      switch (action.level) {
+        case "level1Filter":
+          return {
+            ...state,
+            level1Filter: action.value,
+            level2Filter: undefined,
+            level3Filter: undefined,
+            level4Filter: undefined,
+            level5Filter: undefined,
+          };
+        case "level2Filter":
+          return {
+            ...state,
+            level2Filter: action.value,
+            level3Filter: undefined,
+            level4Filter: undefined,
+            level5Filter: undefined,
+          };
+        case "level3Filter":
+          return {
+            ...state,
+            level3Filter: action.value,
+            level4Filter: undefined,
+            level5Filter: undefined,
+          };
+        case "level4Filter":
+          return {
+            ...state,
+            level4Filter: action.value,
+            level5Filter: undefined,
+          };
+        case "level5Filter":
+          return {
+            ...state,
+            level5Filter: action.value,
+          };
+        default:
+          return state;
+      }
     case "CLEAR_FILTER":
       return { ...state, [action.level]: undefined };
     case "RESET_FILTERS":
