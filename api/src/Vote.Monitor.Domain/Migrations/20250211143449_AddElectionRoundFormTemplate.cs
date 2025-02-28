081da77046da4acc626423587c3ba6e39d2696ed
Friptu Teodor <74249114+FriptuT@users.noreply.github.com>
2025-02-28 15:21:22 +0200

    Feat/ElectionRoundFormTemplatesAssignment  (#870)
    * adding ElectionRoundFormTemplate #1
    
    Creating entity and starting endpoint
    
    * Update .gitignore
    
    ignoring /api/src/Vote.Monitor.Api/Uploads
    
    * assign templates to election round - endpoint#1
    
    assign templates to election round - endpoint with tests #1
    
    * adding AssignTemplates tests for DeleteRangeAsync and Validator  #2
    
    * adjust for ElectionRoundFormTemplateAssignment #3
    
    updates on assigning templates on an ElectionRound consisting of tests, endpoint and configuration files
    
    * adjust testValidators  ElectionRoundFormTemplateAssignment #4
    
    ---------
    
    Co-authored-by: Ion Dormenco <idormenco@gmail.com>

---
+++ api/src/Vote.Monitor.Domain/Migrations/20250211143449_AddElectionRoundFormTemplate.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Vote.Monitor.Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddElectionRoundFormTemplate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ElectionRoundFormTemplates",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    ElectionRoundId = table.Column<Guid>(type: "uuid", nullable: false),
+                    FormTemplateId = table.Column<Guid>(type: "uuid", nullable: false),
+                    CreatedOn = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    CreatedBy = table.Column<Guid>(type: "uuid", nullable: false),
+                    LastModifiedOn = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    LastModifiedBy = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ElectionRoundFormTemplates", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ElectionRoundFormTemplates_ElectionRounds_ElectionRoundId",
+                        column: x => x.ElectionRoundId,
+                        principalTable: "ElectionRounds",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ElectionRoundFormTemplates_FormTemplates_FormTemplateId",
+                        column: x => x.FormTemplateId,
+                        principalTable: "FormTemplates",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ElectionRoundFormTemplates_ElectionRoundId_FormTemplateId",
+                table: "ElectionRoundFormTemplates",
+                columns: new[] { "ElectionRoundId", "FormTemplateId" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ElectionRoundFormTemplates_FormTemplateId",
+                table: "ElectionRoundFormTemplates",
+                column: "FormTemplateId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ElectionRoundFormTemplates");
+        }
+    }
+}
---
+++ api/tests/Feature.FormTemplates.UnitTests/Endpoints/AssignEndpointTests.cs
@@ -0,0 +1,133 @@
+﻿using Feature.FormTemplates.AssignTemplates;
+using Feature.FormTemplates.Specifications;
+using NSubstitute.ReturnsExtensions;
+using Vote.Monitor.Domain.Entities.ElectionRoundAggregate;
+using Vote.Monitor.Domain.Entities.ElectionRoundFormTemplateAggregate;
+
+namespace Feature.FormTemplates.UnitTests.Endpoints;
+
+public class AssignEndpointTests
+{
+    private readonly IReadRepository<ElectionRound> _electionRoundRepository;
+    private readonly IReadRepository<FormTemplateAggregate> _formTemplateRepository;
+    private readonly IRepository<ElectionRoundFormTemplate> _electionRoundFormTemplateRepository;
+    private readonly AssignTemplates.Endpoint _endpoint;
+
+    public AssignEndpointTests()
+    {
+        _electionRoundRepository = Substitute.For<IReadRepository<ElectionRound>>();
+        _formTemplateRepository = Substitute.For<IReadRepository<FormTemplateAggregate>>();
+        _electionRoundFormTemplateRepository = Substitute.For<IRepository<ElectionRoundFormTemplate>>();
+        _endpoint = Factory.Create<Endpoint>(
+            _electionRoundRepository,
+            _formTemplateRepository,
+            _electionRoundFormTemplateRepository);
+    }
+
+    [Fact]
+    public async Task ShouldUnassignFormTemplates_WhenRequestDoesNotContainAssignedTemplates()
+    {
+        // Arrange
+        _electionRoundRepository.AnyAsync(Arg.Any<GetElectionRoundByIdSpecification>()).Returns(true);
+        _formTemplateRepository.CountAsync(Arg.Any<ListFormTemplatesByIds>()).Returns(1);
+
+        var electionRoundId = Guid.NewGuid();
+        var templateIdToKeep = Guid.NewGuid();
+        var templateIdToDelete = Guid.NewGuid();
+
+        _electionRoundFormTemplateRepository
+            .ListAsync(Arg.Any<ListElectionRoundFormTemplateSpecification>())
+            .Returns(new List<ElectionRoundFormTemplate>
+            {
+                new ElectionRoundFormTemplateAggregateFaker(electionRoundId, templateIdToKeep),
+                new ElectionRoundFormTemplateAggregateFaker(electionRoundId, templateIdToDelete),
+            });
+
+
+        // Act
+        var request = new Request { ElectionRoundId = electionRoundId, FormTemplateIds = [templateIdToKeep] };
+
+        var response = await _endpoint.ExecuteAsync(request, default);
+
+        // Assert
+        response.Should().BeOfType<Results<NoContent, NotFound>>()!
+            .Which!
+            .Result.Should().BeOfType<NoContent>();
+
+        await _electionRoundFormTemplateRepository.Received(1)
+            .DeleteRangeAsync(Arg.Is<IEnumerable<ElectionRoundFormTemplate>>(
+                x => x.Count() == 1
+                     && x.Any(y => y.FormTemplateId == templateIdToDelete)), Arg.Any<CancellationToken>());
+    }
+
+
+    [Fact]
+    public async Task ShouldAssignFormTemplates_WhenRequestContainUnassignedTemplates()
+    {
+        // Arrange
+        _electionRoundRepository.AnyAsync(Arg.Any<GetElectionRoundByIdSpecification>()).Returns(true);
+        _formTemplateRepository.CountAsync(Arg.Any<ListFormTemplatesByIds>()).Returns(2);
+        _electionRoundFormTemplateRepository
+            .ListAsync(Arg.Any<ListElectionRoundFormTemplateSpecification>()).Returns([]);
+
+        var formTemplateId1 = Guid.NewGuid();
+        var formTemplateId2 = Guid.NewGuid();
+
+
+        // Act
+        var request = new Request { FormTemplateIds = [formTemplateId1, formTemplateId2] };
+
+        var response = await _endpoint.ExecuteAsync(request, default);
+
+        // Assert
+        response.Should().BeOfType<Results<NoContent, NotFound>>()!
+            .Which!
+            .Result.Should().BeOfType<NoContent>();
+
+        await _electionRoundFormTemplateRepository.Received(1)
+            .AddRangeAsync(Arg.Is<IEnumerable<ElectionRoundFormTemplate>>(x => x.Count() == 2
+                                                                               && x.Any(y =>
+                                                                                   y.FormTemplateId == formTemplateId1)
+                                                                               && x.Any(y =>
+                                                                                   y.FormTemplateId ==
+                                                                                   formTemplateId2)),
+                Arg.Any<CancellationToken>());
+    }
+
+
+    [Fact]
+    public async Task ShouldReturnNotFound_WhenElectionRoundDoesNotExist()
+    {
+        // Arange
+        _electionRoundRepository.AnyAsync(Arg.Any<GetElectionRoundByIdSpecification>()).Returns(false);
+        
+        // Act
+        var response = await _endpoint.ExecuteAsync(new Request(), default);
+
+        // assert
+        response.Should().BeOfType<Results<NoContent, NotFound>>()!
+            .Which!
+            .Result.Should().BeOfType<NotFound>();
+    }
+
+    [Fact]
+    public async Task ShouldReturnNotFound_WhenFormTemplatesNotFound()
+    {
+        // Arrange
+        _electionRoundRepository.AnyAsync(Arg.Any<GetElectionRoundByIdSpecification>()).Returns(true);
+        _formTemplateRepository.CountAsync(Arg.Any<ListFormTemplatesByIds>()).Returns(1);
+
+        var formTemplateId1 = Guid.NewGuid(); 
+        var formTemplateId2 = Guid.NewGuid(); 
+            
+        // Act
+        var request = new Request { FormTemplateIds = [formTemplateId1, formTemplateId2] };
+
+        var response = await _endpoint.ExecuteAsync(request, default);
+
+        // Assert
+        response.Should().BeOfType<Results<NoContent, NotFound>>()!
+            .Which!
+            .Result.Should().BeOfType<NotFound>();
+    }
+}
