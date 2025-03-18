fbb4ec816f7b288531dc064712f606cacc6292a2
Ion Dormenco <idormenco@gmail.com>
2025-03-18 16:23:51 +0200

    Add available ngos for monitoring

---
+++ api/src/Feature.Monitoring/ListAvailableNgos/Validator.cs
@@ -0,0 +1,9 @@
+﻿namespace Feature.Monitoring.ListAvailableNgos;
+
+public class Validator : Validator<Request>
+{
+    public Validator()
+    {
+        RuleFor(x => x.ElectionRoundId).NotEmpty();
+    }
+}
