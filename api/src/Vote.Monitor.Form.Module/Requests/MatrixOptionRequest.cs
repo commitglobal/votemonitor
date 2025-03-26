73aba243eff0bed233322bea1d6535ef121aef83
Friptu Teodor <friptut@gmail.com>
2025-03-26 22:43:47 +0200

    Add MatrixQuestion feature

---
+++ api/src/Vote.Monitor.Form.Module/Requests/MatrixOptionRequest.cs
@@ -0,0 +1,10 @@
+﻿using Vote.Monitor.Core.Models;
+
+namespace Vote.Monitor.Form.Module.Requests;
+
+public class MatrixOptionRequest
+{
+    public Guid Id { get; set; }
+    public TranslatedString Text { get; set; }
+    public bool IsFlagged { get; set; }
+}
---
+++ api/tests/Vote.Monitor.TestUtils/Fakes/Aggregates/Questions/MatrixOptionFaker.cs
@@ -0,0 +1,13 @@
+﻿using Bogus;
+using Vote.Monitor.Domain.Entities.FormBase.Questions;
+
+namespace Vote.Monitor.TestUtils.Fakes.Aggregates.Questions;
+
+public sealed class MatrixOptionFaker : Faker<MatrixOption>
+{
+    public MatrixOptionFaker(Guid? id = null, string[]? languageList = null, bool isFlagged=false)
+    {
+        CustomInstantiator(f => MatrixOption.Create(id ?? f.Random.Guid(), new TranslatedStringFaker(languageList),
+            isFlagged));
+    }
+}
