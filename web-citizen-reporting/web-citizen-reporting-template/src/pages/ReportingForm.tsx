2e02dd15204b8676e5ee73ba00cb99e69c9b0cf8
Ion Dormenco <idormenco@gmail.com>
2025-03-28 11:30:04 +0200

    Add form with and display questions

--- web-citizen-reporting/web-citizen-reporting-template/src/pages/ReportingForm.tsx
+++ web-citizen-reporting/web-citizen-reporting-template/src/pages/ReportingForm.tsx
@@ -1,37 +1,344 @@
+import type { BaseQuestion } from "@/common/types";
 import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
 import {
   Card,
   CardContent,
-  CardFooter,
+  CardDescription,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { useForms } from "@/queries/use-forms";
+import { Checkbox } from "@/components/ui/checkbox";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import { Textarea } from "@/components/ui/textarea";
+import {
+  cn,
+  isDateQuestion,
+  isMultiSelectQuestion,
+  isNumberQuestion,
+  isSingleSelectQuestion,
+  isTextQuestion,
+} from "@/lib/utils";
+import { formsOptions } from "@/queries/use-forms";
 import { Route } from "@/routes/forms/$formId";
+import { DevTool } from "@hookform/devtools";
+import { useSuspenseQuery } from "@tanstack/react-query";
 import { notFound } from "@tanstack/react-router";
+import { format } from "date-fns";
+import { CalendarIcon } from "lucide-react";
+import { useForm } from "react-hook-form";
+
+function QuestionText({
+  languageCode,
+  question,
+}: {
+  languageCode: string;
+  question: BaseQuestion;
+}) {
+  return (
+    <FormLabel className="flex gap-1">
+      <span> {question.code}</span>
+      <span>-</span>
+      <span>{question.text[languageCode]}</span>
+    </FormLabel>
+  );
+}
+
+function QuestionDescription({
+  languageCode,
+  question,
+}: {
+  languageCode: string;
+  question: BaseQuestion;
+}) {
+  return (
+    <FormDescription>{question?.helptext?.[languageCode]}</FormDescription>
+  );
+}
 
 function ReportingForm() {
   const { formId } = Route.useParams();
-  const { data: form } = useForms((forms) =>
-    forms.find((f) => f.id === formId)
-  );
+  const { data: citizenReportFoms } = useSuspenseQuery(formsOptions());
 
-  if (form === undefined) {
+  const citizenReportFom = citizenReportFoms.find((f) => f.id === formId);
+
+  if (citizenReportFom === undefined) {
     throw notFound({ throw: false });
+  }
+
+  const form = useForm();
+
+  function onSubmit() {
+    console.log("submit");
   }
 
   return (
     <Card>
       <CardHeader>
-        <CardTitle>{form.name[form.defaultLanguage]}</CardTitle>
+        <CardTitle>
+          {citizenReportFom.name[citizenReportFom.defaultLanguage]}
+        </CardTitle>
+        <CardDescription>
+          {citizenReportFom.description[citizenReportFom.defaultLanguage]}
+        </CardDescription>
       </CardHeader>
-      <CardContent></CardContent>
-      <CardFooter className="w-full flex justify-end">
-        <div className="flex gap-4">
-          <Button variant="outline">Cancel</Button>
-          <Button>Submit</Button>
+
+      <CardContent className="flex flex-col gap-4">
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            {citizenReportFom.questions.map((question) => (
+              <div className="w-full" key={question.id}>
+                {isTextQuestion(question) && (
+                  <FormField
+                    control={form.control}
+                    name="username"
+                    render={({ field }) => (
+                      <FormItem>
+                        <QuestionText
+                          question={question}
+                          languageCode={citizenReportFom.defaultLanguage}
+                        />
+                        <QuestionDescription
+                          question={question}
+                          languageCode={citizenReportFom.defaultLanguage}
+                        />
+
+                        <FormControl>
+                          <Textarea
+                            placeholder={
+                              question?.inputPlaceholder?.[
+                                citizenReportFom.defaultLanguage
+                              ]
+                            }
+                            {...field}
+                          />
+                        </FormControl>
+
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                )}
+                {isNumberQuestion(question) && (
+                  <FormField
+                    control={form.control}
+                    name="username"
+                    render={({ field }) => (
+                      <FormItem>
+                        <QuestionText
+                          question={question}
+                          languageCode={citizenReportFom.defaultLanguage}
+                        />
+                        <QuestionDescription
+                          question={question}
+                          languageCode={citizenReportFom.defaultLanguage}
+                        />
+                        <FormControl>
+                          <Input
+                            type="number"
+                            placeholder={
+                              question?.inputPlaceholder?.[
+                                citizenReportFom.defaultLanguage
+                              ]
+                            }
+                            {...field}
+                          />
+                        </FormControl>
+
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                )}
+
+                {isDateQuestion(question) && (
+                  <FormField
+                    control={form.control}
+                    name="dob"
+                    render={({ field }) => (
+                      <FormItem className="flex flex-col">
+                        <QuestionText
+                          question={question}
+                          languageCode={citizenReportFom.defaultLanguage}
+                        />
+                        <QuestionDescription
+                          question={question}
+                          languageCode={citizenReportFom.defaultLanguage}
+                        />
+                        <Popover>
+                          <PopoverTrigger asChild>
+                            <FormControl>
+                              <Button
+                                variant={"outline"}
+                                className={cn(
+                                  "w-[240px] pl-3 text-left font-normal",
+                                  !field.value && "text-muted-foreground"
+                                )}
+                              >
+                                {field.value ? (
+                                  format(field.value, "PPP")
+                                ) : (
+                                  <span>Pick a date</span>
+                                )}
+                                <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                              </Button>
+                            </FormControl>
+                          </PopoverTrigger>
+                          <PopoverContent className="w-auto p-0" align="start">
+                            <Calendar
+                              mode="single"
+                              selected={field.value}
+                              onSelect={field.onChange}
+                              disabled={(date) =>
+                                date > new Date() ||
+                                date < new Date("1900-01-01")
+                              }
+                              initialFocus
+                            />
+                          </PopoverContent>
+                        </Popover>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                )}
+
+                {isSingleSelectQuestion(question) && (
+                  <FormField
+                    control={form.control}
+                    name="type"
+                    render={({ field }) => (
+                      <FormItem className="space-y-3">
+                        <QuestionText
+                          question={question}
+                          languageCode={citizenReportFom.defaultLanguage}
+                        />
+                        <QuestionDescription
+                          question={question}
+                          languageCode={citizenReportFom.defaultLanguage}
+                        />
+                        <FormControl>
+                          <RadioGroup
+                            onValueChange={field.onChange}
+                            defaultValue={field.value}
+                            className="flex flex-col space-y-1"
+                          >
+                            {question.options.map((option) => (
+                              <FormItem
+                                className="flex items-center space-x-3 space-y-0"
+                                key={`${question.id}-${option.id}`}
+                              >
+                                <FormControl>
+                                  <RadioGroupItem value={option.id} />
+                                </FormControl>
+                                <FormLabel className="font-normal">
+                                  {
+                                    option.text[
+                                      citizenReportFom.defaultLanguage
+                                    ]
+                                  }
+                                </FormLabel>
+                              </FormItem>
+                            ))}
+                          </RadioGroup>
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                )}
+
+                {isMultiSelectQuestion(question) && (
+                  <FormField
+                    control={form.control}
+                    name="items"
+                    render={() => (
+                      <FormItem>
+                        <div className="mb-4">
+                          <QuestionText
+                            question={question}
+                            languageCode={citizenReportFom.defaultLanguage}
+                          />
+                          <QuestionDescription
+                            question={question}
+                            languageCode={citizenReportFom.defaultLanguage}
+                          />
+                        </div>
+                        {question.options.map((option) => (
+                          <FormField
+                            key={option.id}
+                            control={form.control}
+                            name="items"
+                            render={({ field }) => {
+                              return (
+                                <FormItem
+                                  key={option.id}
+                                  className="flex flex-row items-start space-x-3 space-y-0"
+                                >
+                                  <FormControl>
+                                    <Checkbox
+                                      checked={field.value?.includes(option.id)}
+                                      onCheckedChange={(checked) => {
+                                        return checked
+                                          ? field.onChange([
+                                              ...field.value,
+                                              option.id,
+                                            ])
+                                          : field.onChange(
+                                              field.value?.filter(
+                                                (value: string) =>
+                                                  value !== option.id
+                                              )
+                                            );
+                                      }}
+                                    />
+                                  </FormControl>
+                                  <FormLabel className="text-sm font-normal">
+                                    {
+                                      option.text[
+                                        citizenReportFom.defaultLanguage
+                                      ]
+                                    }
+                                  </FormLabel>
+                                </FormItem>
+                              );
+                            }}
+                          />
+                        ))}
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                )}
+              </div>
+            ))}
+          </form>
+          <DevTool control={form.control} /> {/* set up the dev tool */}
+        </Form>
+        <div className="w-full flex justify-end">
+          <div className="flex gap-4">
+            <Button variant="outline">Cancel</Button>
+            <Button type="submit" className="cursor-pointer">
+              Submit
+            </Button>
+          </div>
         </div>
-      </CardFooter>
+      </CardContent>
     </Card>
   );
 }
--- web-citizen-reporting/web-citizen-reporting-template/src/queries/use-forms.tsx
+++ web-citizen-reporting/web-citizen-reporting-template/src/queries/use-forms.tsx
@@ -1,17 +1,16 @@
 import { getForms } from "@/api/get-forms";
-import type { FormModel } from "@/common/types";
-import { electionRoundId } from "@/lib/utils";
-import { skipToken, useQuery } from "@tanstack/react-query";
+import { queryOptions, useQuery } from "@tanstack/react-query";
 const STALE_TIME = 1000 * 60 * 15; // 15 minutes
 
-export const useForms = <TResult = Array<FormModel>,>(
-  select?: (elections: Array<FormModel>) => TResult
-) => {
-  return useQuery({
+export const formsOptions = () => {
+  return queryOptions({
     queryKey: ["forms"],
     placeholderData: [],
-    queryFn: electionRoundId ? () => getForms() : skipToken,
-    select,
+    queryFn: () => getForms(),
     staleTime: STALE_TIME,
   });
+};
+
+export const useForms = () => {
+  return useQuery(formsOptions());
 };
