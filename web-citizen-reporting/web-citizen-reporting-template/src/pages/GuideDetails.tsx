6650d23bfd3a24581be3a868c71c0c068163c4a5
Rafael Ciobotariu <61947947+RafaelCiobotariu@users.noreply.github.com>
2025-03-31 13:40:49 +0300

    Feature/guide-details-page (#950)
    * added guide details component for text guideType
    
    * finished guide details page
    
    * Update NotificationDetails.tsx
    
    ---------
    
    Co-authored-by: Ion Dormenco <idormenco@gmail.com>

--- web-citizen-reporting/web-citizen-reporting-template/src/pages/GuideDetails.tsx
+++ web-citizen-reporting/web-citizen-reporting-template/src/pages/GuideDetails.tsx
@@ -1,14 +1,52 @@
+import { Spinner } from "@/components/Spinner";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { downloadFile } from "@/lib/utils";
 import { useGuides } from "@/queries/use-guides";
 import { Route } from "@/routes/guides/$guideId";
+import { notFound } from "@tanstack/react-router";
+import { Download, ExternalLink } from "lucide-react";
 
 function GuideDetails() {
   const { guideId } = Route.useParams();
 
-  const { data: guide } = useGuides((guides) =>
+  const { data: guide, isLoading } = useGuides((guides) =>
     guides.find((g) => g.id === guideId)
   );
 
-  return <>{JSON.stringify(guide)}</>;
+  if (isLoading) return <Spinner show={isLoading} />;
+
+  if (!guide) throw notFound();
+  return (
+    <>
+      <Card>
+        <CardHeader>
+          <CardTitle>{guide.title}</CardTitle>
+        </CardHeader>
+        {guide.guideType === "Text" && (
+          <CardContent className="h-full w-full">
+            <div dangerouslySetInnerHTML={{ __html: guide.text }} />
+          </CardContent>
+        )}
+        {guide.guideType === "Website" && (
+          <Button variant="link" className="cursor-pointer">
+            <a href={guide.websiteUrl}>Visit Website</a>
+            <ExternalLink className="h-4 w-4" />
+          </Button>
+        )}
+        {guide.guideType === "Document" && (
+          <Button
+            variant="link"
+            className="cursor-pointer"
+            onClick={() => downloadFile(guide.presignedUrl)}
+          >
+            Download
+            <Download className="h-4 w-4" />
+          </Button>
+        )}
+      </Card>
+    </>
+  );
 }
 
 export default GuideDetails;
