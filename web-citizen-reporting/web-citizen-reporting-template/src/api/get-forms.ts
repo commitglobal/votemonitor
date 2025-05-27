e9fb9a082f08fe8f73f729af510ad63bf5a5f257
Iulian Deaconu <imdeaconu@gmail.com>
2025-05-27 13:59:38 +0300

    [Citizen Reporting Web] Bugfixes and improvements for the reporting form component (#960)
    * WIP: fix number inputs not validating and text, number and selection input losing the value when going back
    
    * fix form validation
    
    * add number input component
    
    * fetch formData inside route
    
    * call useLoaderData from the Route obj
    
    * WIP: add display logic for form questions
    
    * Wip/refactor inputs (#17)
    
    * start refactoring inputs
    
    * fix multiselect and add free text responses
    
    * refactor display logic check
    
    * refactor and set required only if field is displayed
    
    * refactor logic
    
    * add freeform text to single select input
    
    * add rating input

--- web-citizen-reporting/web-citizen-reporting-template/src/api/get-forms.ts
+++ web-citizen-reporting/web-citizen-reporting-template/src/api/get-forms.ts
@@ -7,3 +7,13 @@ export const getForms = (): Promise<Array<FormModel>> => {
     `/api/election-rounds/${electionRoundId}/citizen-reporting-forms`
   ).then((res) => res.data.forms);
 };
+
+export const getFormById = async (
+  formId: string
+): Promise<FormModel | undefined> => {
+  const formData = getForms().then((forms) =>
+    forms.find((form) => form.id === formId)
+  );
+
+  return formData;
+};
--- web-citizen-reporting/web-citizen-reporting-template/src/routes/forms/$formId.tsx
+++ web-citizen-reporting/web-citizen-reporting-template/src/routes/forms/$formId.tsx
@@ -1,10 +1,33 @@
+import { currentFormLanguageAtom } from "@/features/forms/atoms";
 import NotFound from "@/pages/NotFound";
 import SubmitCitizenReport from "@/pages/ReportingForm";
-import { formsOptions } from "@/queries/use-forms";
-import { createFileRoute } from "@tanstack/react-router";
+import { formQueryOptions } from "@/queries/use-forms";
+import { useSuspenseQuery } from "@tanstack/react-query";
+import { createFileRoute, notFound } from "@tanstack/react-router";
+import { useSetAtom } from "jotai";
+import { useEffect } from "react";
 
 export const Route = createFileRoute("/forms/$formId")({
-  loader: (opts) => opts.context.queryClient.ensureQueryData(formsOptions()),
-  component: SubmitCitizenReport,
+  loader: async (opts) => {
+    const { formId } = opts.params;
+    const formData = await opts.context.queryClient.ensureQueryData(
+      formQueryOptions(formId)
+    );
+
+    if (!formData) throw notFound();
+    return formData;
+  },
+  component: () => {
+    const { formId } = Route.useParams();
+    const { data } = useSuspenseQuery(formQueryOptions(formId));
+    const setCurrentLanguage = useSetAtom(currentFormLanguageAtom);
+
+    useEffect(() => {
+      if (!data?.defaultLanguage) return;
+      setCurrentLanguage(data.defaultLanguage);
+    }, [data]);
+
+    return <SubmitCitizenReport />;
+  },
   notFoundComponent: NotFound,
 });
