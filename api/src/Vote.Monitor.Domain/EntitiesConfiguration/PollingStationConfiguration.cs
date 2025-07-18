f232062af76682bac663e06924778dcd921c31df
Paul Antal <paul.antal@hotmail.com>
2025-07-18 16:59:18 +0300

    Add migration for SMS form submission fields

--- api/src/Vote.Monitor.Domain/EntitiesConfiguration/PollingStationConfiguration.cs
+++ api/src/Vote.Monitor.Domain/EntitiesConfiguration/PollingStationConfiguration.cs
@@ -9,6 +9,7 @@ public class PollingStationConfiguration : IEntityTypeConfiguration<PollingStati
         builder.HasKey(p => p.Id);
 
         builder.Property(p => p.Id).IsRequired();
+        builder.Property(p => p.SecondaryId).IsRequired().HasDefaultValue("").HasMaxLength(4);
         builder.Property(p => p.Address).HasMaxLength(2024).IsRequired();
         builder.Property(p => p.DisplayOrder).IsRequired();
         builder.Property(p => p.Tags).IsRequired(false);
---
+++ api/src/Vote.Monitor.Domain/Migrations/20250718135435_AddSmsFormSubmissionRelatedFields.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Vote.Monitor.Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSmsFormSubmissionRelatedFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "SecondaryId",
+                table: "PollingStations",
+                type: "character varying(4)",
+                maxLength: 4,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PhoneNumber",
+                table: "MonitoringObservers",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SecondaryId",
+                table: "PollingStations");
+
+            migrationBuilder.DropColumn(
+                name: "PhoneNumber",
+                table: "MonitoringObservers");
+        }
+    }
+}
