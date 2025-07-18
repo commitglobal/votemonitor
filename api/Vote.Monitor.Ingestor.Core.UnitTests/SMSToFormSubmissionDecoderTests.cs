312ee5d25b087bf11faa78cb8e35cb280356bb01
Paul Antal <paul.antal@hotmail.com>
2025-07-18 14:03:58 +0300

    Add core project and base SMS form submission decoder

---
+++ api/Vote.Monitor.Ingestor.Core.UnitTests/SMSToFormSubmissionDecoderTests.cs
@@ -0,0 +1,130 @@
+﻿using FluentAssertions;
+using NSubstitute.ExceptionExtensions;
+using Vote.Monitor.Ingestor.Core.Converters;
+
+namespace Vote.Monitor.Ingestor.Core.UnitTests;
+
+public class SMSToFormSubmissionDecoderTests
+{
+    private static SMSToFormSubmissionDecoder _decoder = new();
+
+    private const string VALID_FORM_CODE = "A3";
+    private const string FORM_CODE_WITH_INVALID_CHARACTERS = "1#";
+
+    private const string VALID_CORRELATION_ID = "12AB";
+    private const string CORRELATION_ID_WITH_INVALID_CHARACTERS = "12A!";
+
+    private const string VALID_POLLING_STATION_CODE = "ABC3";
+    private const string POLLING_STATION_CODE_WITH_INVALID_CHARACTERS = "C!#.";
+
+    private const string VALID_QUESTION_CODE = "QA";
+    private const string QUESTION_CODE_WITH_INVALID_CHARACTERS = "!2";
+
+    private const string VALID_ANSWER = "132";
+    private const string ANSWER_WITH_INVALID_CHARACTERS = "!24$";
+
+    private static (string questionCode, string questionAnswer)[] _validQuestions = [(VALID_QUESTION_CODE, VALID_ANSWER)];
+
+    [Fact]
+    public void DecodingEmptyString_ThrowsException()
+    {
+        _decoder.Invoking(d => d.Decode(string.Empty)).Should().Throw<ArgumentException>().WithMessage("smsMessage must not be null or empty");
+    }
+
+    [Fact]
+    public void DecodingStringWithoutValidFormCode_ThrowsException()
+    {
+        _decoder.Invoking(d => d.Decode(GetEncodedString(formCode: FORM_CODE_WITH_INVALID_CHARACTERS))).Should()
+            .Throw<ArgumentException>()
+            .WithMessage($"Form code '{FORM_CODE_WITH_INVALID_CHARACTERS}' contains invalid characters. Only alphanumeric characters are valid");
+    }
+
+    [Fact]
+    public void DecodingStringWithValidFormCode_ReturnsItInASMSFormSubmission()
+    {
+        SMSFormSubmission smsFormSubmission = _decoder.Decode(GetEncodedString(formCode: VALID_FORM_CODE));
+        smsFormSubmission.FormCode.Should().Be(VALID_FORM_CODE);
+    }
+
+    [Fact]
+    public void DecodingStringWithoutValidCorrelationId_ThrowsException()
+    {
+        _decoder.Invoking(d => d.Decode(GetEncodedString(correlationId: CORRELATION_ID_WITH_INVALID_CHARACTERS))).Should()
+            .Throw<ArgumentException>()
+            .WithMessage($"Correlation ID '{CORRELATION_ID_WITH_INVALID_CHARACTERS}' contains invalid characters. Only alphanumeric characters are valid");
+    }
+
+    [Fact]
+    public void DecodingStringWithValidCorrelationId_ReturnsItInASMSFormSubmission()
+    {
+        SMSFormSubmission smsFormSubmission = _decoder.Decode(GetEncodedString(correlationId: VALID_CORRELATION_ID));
+        smsFormSubmission.CorrelationId.Should().Be(VALID_CORRELATION_ID);
+    }
+
+
+    [Fact]
+    public void DecodingStringWithoutValidPollingStationCode_ThrowsException()
+    {
+        _decoder.Invoking(d => d.Decode(GetEncodedString(pollingStationCode: POLLING_STATION_CODE_WITH_INVALID_CHARACTERS))).Should()
+            .Throw<ArgumentException>()
+            .WithMessage($"Polling station code '{POLLING_STATION_CODE_WITH_INVALID_CHARACTERS}' contains invalid characters. Only alphanumeric characters are valid");
+    }
+
+    [Fact]
+    public void DecodingStringWithValidPollingStationCode_ReturnsItInASMSFormSubmission()
+    {
+        SMSFormSubmission smsFormSubmission = _decoder.Decode(GetEncodedString(pollingStationCode: VALID_POLLING_STATION_CODE));
+        smsFormSubmission.PollingStationCode.Should().Be(VALID_POLLING_STATION_CODE);
+    }
+
+    [Fact]
+    public void DecodingStringWithoutQuestions_ThrowsException()
+    {
+        _decoder.Invoking(d => d.Decode(GetEncodedString(questions: []))).Should()
+            .Throw<ArgumentException>()
+            .WithMessage("The message must contain question answers");
+    }
+
+    [Fact]
+    public void DecodingStringWithInvalidQuestionCode_ThrowsException()
+    {
+        _decoder.Invoking(d => d.Decode(GetEncodedString(questions: [(QUESTION_CODE_WITH_INVALID_CHARACTERS, VALID_ANSWER)]))).Should()
+            .Throw<ArgumentException>()
+            .WithMessage($"Question code '{QUESTION_CODE_WITH_INVALID_CHARACTERS}' contains invalid characters. Only letters are valid");
+    }
+
+    [Fact]
+    public void DecodingStringWithInvalidQuestionAnswer_ThrowsException()
+    {
+        _decoder.Invoking(d => d.Decode(GetEncodedString(questions: [(VALID_QUESTION_CODE, ANSWER_WITH_INVALID_CHARACTERS)]))).Should()
+            .Throw<ArgumentException>()
+            .WithMessage($"The answer '{ANSWER_WITH_INVALID_CHARACTERS}' for question '{VALID_QUESTION_CODE}' contains invalid characters. Only digits are valid");
+    }
+
+    [Fact]
+    public void DecodingStringWithValidQuestionAnswer_ReturnsThemInASMSFormSubmission()
+    {
+        SMSFormSubmission smsFormSubmission = _decoder.Decode(GetEncodedString(questions: [(VALID_QUESTION_CODE, VALID_ANSWER), (VALID_QUESTION_CODE, VALID_ANSWER)]));
+        smsFormSubmission.Questions.Length.Should().Be(2);
+        smsFormSubmission.Questions.Should().AllSatisfy(q => q.Code.Should().Be(VALID_QUESTION_CODE));
+        smsFormSubmission.Questions.Should().AllSatisfy(q => q.Answer.Should().Be(VALID_ANSWER));
+    }
+
+    private static string GetEncodedString(
+        string formCode = VALID_FORM_CODE,
+        string correlationId = VALID_CORRELATION_ID,
+        string pollingStationCode = VALID_POLLING_STATION_CODE,
+        (string questionCode, string questionAnswer)[]? questions = null)
+    {
+        questions ??= _validQuestions;
+
+        var encodedString = $"{formCode}{correlationId}{pollingStationCode}";
+
+        foreach (var (questionCode, questionAnswer) in questions)
+        {
+            encodedString += $"{questionCode}{questionAnswer}";
+        }
+
+        return encodedString;
+    }
+}
---
+++ api/src/Vote.Monitor.Ingestor.Core/Converters/SMSToFormSubmissionDecoder.cs
@@ -0,0 +1,153 @@
+﻿using AngleSharp.Text;
+using Vote.Monitor.Domain.Entities.FormSubmissionAggregate;
+
+namespace Vote.Monitor.Ingestor.Core.Converters;
+
+public class SMSToFormSubmissionDecoder
+{
+    private const int FORM_CODE_START_INDEX = 0;
+    private const int FORM_CODE_LENGTH = 2;
+    private const int CORRELATION_ID_START_INDEX = 2;
+    private const int CORRELATION_ID_LENGTH = 4;
+    private const int POLLING_STATION_CODE_START_INDEX = 6;
+    private const int POLLING_STATION_CODE_LENGTH = 4;
+    private const int QUESTIONS_START_INDEX = 10;
+    private const int QUESTION_CODE_LENGTH = 2;
+
+    public SMSFormSubmission Decode(string smsMessage)
+    {
+        ValidateSmsMessage(smsMessage);
+
+        var formCode = ExtractFormCode(smsMessage);
+        ValidateFormCode(formCode);
+
+        var correlationId = ExtractCorrelationId(smsMessage);
+        ValidateCorrelationId(correlationId);
+
+        var pollingStationCode = ExtractPollingStationCode(smsMessage);
+        ValidatePollingStationCode(pollingStationCode);
+
+        var questions = ExtractQuestions(smsMessage);
+        ValidateQuestions(questions);
+
+        return new SMSFormSubmission() { 
+            FormCode = formCode, 
+            CorrelationId = correlationId, 
+            PollingStationCode = pollingStationCode,
+            Questions = questions
+        };
+
+    }
+
+    private void ValidateSmsMessage(string smsMessage)
+    {
+        if (string.IsNullOrWhiteSpace(smsMessage)) throw new ArgumentException($"{nameof(smsMessage)} must not be null or empty");
+    }
+
+    private string ExtractFormCode(string smsMessage)
+    {
+        return smsMessage.Substring(FORM_CODE_START_INDEX, FORM_CODE_LENGTH);
+    }
+
+    private void ValidateFormCode(string formCode)
+    {
+        if (formCode.Any(c => !c.IsAlphanumericAscii()))
+            throw new ArgumentException($"Form code '{formCode}' contains invalid characters. Only alphanumeric characters are valid");
+    }
+
+    private string ExtractCorrelationId(string smsMessage)
+    {
+        return smsMessage.Substring(CORRELATION_ID_START_INDEX, CORRELATION_ID_LENGTH);
+    }
+
+    private void ValidateCorrelationId(string correlationId)
+    {
+        if (correlationId.Any(c => !c.IsAlphanumericAscii()))
+            throw new ArgumentException($"Correlation ID '{correlationId}' contains invalid characters. Only alphanumeric characters are valid");
+    }
+
+    private string ExtractPollingStationCode(string smsMessage)
+    {
+        return smsMessage.Substring(POLLING_STATION_CODE_START_INDEX, POLLING_STATION_CODE_LENGTH);
+    }
+
+    private void ValidatePollingStationCode(string pollingStationCode)
+    {
+        if (pollingStationCode.Any(c => !c.IsAlphanumericAscii()))
+            throw new ArgumentException($"Polling station code '{pollingStationCode}' contains invalid characters. Only alphanumeric characters are valid");
+    }
+
+    private SMSFormSubmission.Question[] ExtractQuestions(string smsMessage)
+    {
+        var questionsText = smsMessage.Substring(QUESTIONS_START_INDEX);
+
+        List<SMSFormSubmission.Question> questions = new();
+
+        bool readingCode = true;
+
+        string currentCode = "";
+        int answerStartIndex = -1;
+
+        for(var i = 0; i < questionsText.Length;)
+        {
+            if (readingCode)
+            {
+                currentCode = questionsText.Substring(i, QUESTION_CODE_LENGTH);
+                i += QUESTION_CODE_LENGTH;
+                answerStartIndex = i;
+                readingCode = false;
+            }
+            else
+            {
+                if (!questionsText[i].IsLetter())
+                {
+                    i++;
+                    continue;
+                } 
+                else
+                {
+                    var answer = questionsText.Substring(answerStartIndex, i - answerStartIndex);
+                    questions.Add(new() { Code = currentCode, Answer = answer });
+                    answerStartIndex = -1;
+                    readingCode = true;
+                }
+            }
+        }
+
+        if (!string.IsNullOrEmpty(currentCode))
+        {
+            var answer = questionsText.Substring(answerStartIndex);
+            questions.Add(new() { Code = currentCode, Answer = answer});
+        }
+
+        return questions.ToArray();
+    }
+
+    private void ValidateQuestions(SMSFormSubmission.Question[] questions)
+    {
+        if (!questions.Any())
+            throw new ArgumentException("The message must contain question answers");
+        foreach(var question in questions)
+        {
+            if(question.Code.Any(c => !c.IsLetter()))
+                throw new ArgumentException($"Question code '{question.Code}' contains invalid characters. Only letters are valid");
+            if (question.Answer.Any(c => !c.IsDigit()))
+                throw new ArgumentException($"The answer '{question.Answer}' for question '{question.Code}' contains invalid characters. Only digits are valid");
+        }
+    }
+}
+
+public class SMSFormSubmission
+{
+    public required string FormCode;
+    public required string CorrelationId;
+    public required string PollingStationCode;
+
+    public required Question[] Questions;
+
+    public class Question
+    {
+        public required string Code;
+        public required string Answer;
+    }
+}
