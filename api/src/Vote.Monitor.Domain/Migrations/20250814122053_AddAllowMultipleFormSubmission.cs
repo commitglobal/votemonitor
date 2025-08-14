6ea703987b86502821344952021a3d47d0a3622c
Ion Dormenco <idormenco@gmail.com>
2025-08-14 15:21:56 +0300

    Add AllowMultipleFormSubmission flag

---
+++ api/src/Vote.Monitor.Domain/Migrations/20250814122053_AddAllowMultipleFormSubmission.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Vote.Monitor.Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAllowMultipleFormSubmission : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "AllowMultipleFormSubmission",
+                table: "MonitoringNgos",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AllowMultipleFormSubmission",
+                table: "MonitoringNgos");
+        }
+    }
+}
