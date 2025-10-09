79dd7351a00cdf01ffe9969ad0532beeee8a6122
Ion Dormenco <idormenco@gmail.com>
2025-10-09 12:04:03 +0300

    add knip
    closes #1016

--- web2.0/src/routes/(app)/elections/$electionRoundId/observers/index.tsx
+++ web2.0/src/routes/(app)/elections/$electionRoundId/observers/index.tsx
@@ -1,11 +1,10 @@
 import Page from "@/pages/NgoAdmin/MonitoringObservers/Page";
 import { monitoringObserversSearchSchema } from "@/types/monitoring-observer";
 import { createFileRoute } from "@tanstack/react-router";
-import { zodValidator } from "@tanstack/zod-adapter";
 
 export const Route = createFileRoute(
   "/(app)/elections/$electionRoundId/observers/"
 )({
   component: Page,
-  validateSearch: zodValidator(monitoringObserversSearchSchema),
+  validateSearch: monitoringObserversSearchSchema,
 });
