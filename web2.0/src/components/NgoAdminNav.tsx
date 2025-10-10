9ec3044ac7a3390cf5957613111b667a901fbfa4
Iulian Deaconu <imdeaconu@gmail.com>
2025-10-10 20:24:52 +0300

    enhancement: create a custov NavLink component (#1024)
    Lgtm

--- web2.0/src/components/NgoAdminNav.tsx
+++ web2.0/src/components/NgoAdminNav.tsx
@@ -1,93 +1,58 @@
-import { useCurrentElectionRound } from "@/contexts/election-round.context";
-import { cn } from "@/lib/utils";
-import { Link } from "@tanstack/react-router";
+import { Link, LinkProps } from '@tanstack/react-router'
+import { useCurrentElectionRound } from '@/contexts/election-round.context'
+import { cn } from '@/lib/utils'
 
-export default function NgoAdminNav() {
-  const { electionRoundId } = useCurrentElectionRound();
+export function AdminNavLink(props: LinkProps & { title: string }) {
+  const { electionRoundId } = useCurrentElectionRound()
+  const { title } = props
 
   return (
-    <div className="mr-4 hidden md:flex">
-      <nav className="flex items-center gap-4 text-sm xl:gap-6">
-        <Link
-          to="/elections/$electionRoundId"
-          params={{ electionRoundId: electionRoundId! }}
-          className={cn("transition-colors hover:text-foreground/80")}
-        >
-          Dashboard
-        </Link>
-        <Link
-          to="/elections/$electionRoundId/forms"
-          params={{ electionRoundId: electionRoundId! }}
-          className={cn(
-            "transition-colors hover:text-foreground/80 text-foreground/80"
-          )}
-          activeProps={{
-            className: "text-foreground",
-          }}
-        >
-          Forms
-        </Link>
-        <Link
-          to="/elections/$electionRoundId/observers"
-          params={{ electionRoundId: electionRoundId! }}
-          className={cn(
-            "transition-colors hover:text-foreground/80 text-foreground/80"
-          )}
-          activeProps={{
-            className: "text-foreground",
-          }}
-        >
-          Observers
-        </Link>
-        <Link
-          to="/elections/$electionRoundId/guides"
-          params={{ electionRoundId: electionRoundId! }}
-          className={cn(
-            "transition-colors hover:text-foreground/80 text-foreground/80"
-          )}
-          activeProps={{
-            className: "text-foreground",
-          }}
-        >
-          Guides
-        </Link>
-        <Link
-          to="/elections/$electionRoundId/submissions"
-          params={{ electionRoundId: electionRoundId! }}
-          className={cn(
-            "transition-colors hover:text-foreground/80 text-foreground/80"
-          )}
-          activeProps={{
-            className: "text-foreground",
-          }}
-        >
-          Submissions
-        </Link>
-        <Link
-          to="/elections/$electionRoundId/quick-reports"
-          params={{ electionRoundId: electionRoundId! }}
-          className={cn(
-            "transition-colors hover:text-foreground/80 text-foreground/80"
-          )}
-          activeProps={{
-            className: "text-foreground",
-          }}
-        >
-          Quick reports
-        </Link>
-        <Link
-          to="/elections/$electionRoundId/incidents"
-          params={{ electionRoundId: electionRoundId! }}
-          className={cn(
-            "transition-colors hover:text-foreground/80 text-foreground/80"
-          )}
-          activeProps={{
-            className: "text-foreground",
-          }}
-        >
-          Incident reports
-        </Link>
+    <Link
+      params={{ electionRoundId: electionRoundId! }}
+      className={cn('hover:text-foreground/80 transition-colors')}
+      activeProps={{
+        className: cn(
+          'underline underline-offset-12 decoration-2 decoration-black dark:decoration-white text-foreground'
+        ),
+      }}
+      {...props}
+    >
+      {title}
+    </Link>
+  )
+}
+
+export default function NgoAdminNav() {
+  const { electionRoundId } = useCurrentElectionRound()
+
+  return (
+    <div className='mr-4 hidden md:flex'>
+      <nav className='flex items-center gap-4 text-sm xl:gap-6'>
+        <AdminNavLink
+          to='/elections/$electionRoundId'
+          title='Dashboard'
+          activeOptions={{ exact: true }}
+        />
+        <AdminNavLink to='/elections/$electionRoundId/forms' title='Forms' />
+        <AdminNavLink
+          to='/elections/$electionRoundId/observers'
+          title='Observers'
+        />
+
+        <AdminNavLink to='/elections/$electionRoundId/guides' title='Guides' />
+        <AdminNavLink
+          to='/elections/$electionRoundId/submissions'
+          title='Submissions'
+        />
+        <AdminNavLink
+          to='/elections/$electionRoundId/quick-reports'
+          title='Quick reports'
+        />
+        <AdminNavLink
+          to='/elections/$electionRoundId/incidents'
+          title='Incident reports'
+        />
       </nav>
     </div>
-  );
+  )
 }
