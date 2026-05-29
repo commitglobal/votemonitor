7adc6e7a5ddf836e067dffb471b00bd6210017ee
Ion Dormenco <idormenco@gmail.com>
2026-05-29 17:42:34 +0200

    Add list entries detailed (#1057)
    Co-authored-by: ion.dormenco <ion.dormenco@wearetribus.com>

---
+++ api/src/Feature.Form.Submissions/ListEntriesDetailed/Validator.cs
@@ -0,0 +1,16 @@
+namespace Feature.Form.Submissions.ListEntriesDetailed;
+
+public class Validator : Validator<Request>
+{
+    public Validator()
+    {
+        RuleFor(x => x.ElectionRoundId).NotEmpty();
+        RuleFor(x => x.NgoId).NotEmpty();
+        RuleFor(x => x.DataSource).NotEmpty();
+        
+        RuleFor(x => x.PageNumber)
+            .GreaterThanOrEqualTo(1);
+
+        RuleFor(x => x.PageSize).InclusiveBetween(1, 100);
+    }
+}
