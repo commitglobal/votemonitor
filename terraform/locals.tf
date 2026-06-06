387393b6a7ee9b63f1af33d3dbe3a253f001458e
Ion Dormenco <idormenco@gmail.com>
2026-06-06 21:16:05 +0300

    Update image tags to version 0.2.76

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -5,12 +5,12 @@ locals {
   images = {
     api = {
       image = "commitglobal/votemonitor"
-      tag   = "0.2.75"
+      tag   = "0.2.76"
     }
 
     hangfire = {
       image = "commitglobal/votemonitor-hangfire"
-      tag   = "0.2.75"
+      tag   = "0.2.76"
     }
   }
 
